ba32aa0ccbf9fdbedf0bb2e2da5c3347251104af
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-13 15:40:05 -0400

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -1,1 +1,17 @@
+import discord
+from discord.ext import commands
 
+intents = discord.Intents.default()
+intents.reactions = True
+intents.messages = True
+intents.message_content = True
+
+bot2 = commands.Bot(command_prefix='!', intents=intents)
+
+@bot2.event
+async def on_ready():
+    print(f"Bot2 is ready. Connected as {bot2.user.name}")
+
+# Rest of your bot2 code goes here
+
+bot2.run(TOKEN2)
