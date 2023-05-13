6cbeb21378954e091bc56d6acce17376913f124e
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-13 13:45:08 -0400

    Update index.py

--- index.py
+++ index.py
@@ -1,12 +1,12 @@
-import re
 import discord
 import json
 from discord.ext import commands
 import logging
+import re
 
 logging.basicConfig(level=logging.INFO)
 
-TOKEN = "MTEwNTU5ODczNjU1MTM4NzI0Nw.GqLK7T.uA0l5cq1hPB7JcgvBU7OsqdzZkaNpg54cEWAak"
+TOKEN = "MTEwNTU5ODczNjU1MTM4NzI0Nw.Gtxm_1.AUKZIq_xdfHhuG7LTvJKuimE-bhPqWJEJ8slq4"
 
 intents = discord.Intents.default()
 intents.reactions = True
@@ -46,7 +46,7 @@ def load_data():
 def save_data(data):
     with open('counting_bot_data.json', 'w') as f:
         json.dump(data, f, indent=4)
-       
+
 data = load_data()
 
 def get_server_data(guild_id):
@@ -58,31 +58,48 @@ def get_server_data(guild_id):
             'counting_channel_id': None,
             'increment': 1
         }
+    else:
+        server_data = data[guild_id]
+        if 'next_increment' not in server_data:
+            server_data['next_increment'] = server_data.get('increment', 1)
+        else:
+            server_data['increment'] = server_data['next_increment']
+            del server_data['next_increment']
+        if 'increment' not in server_data:
+            server_data['increment'] = 1  # Set default value for 'increment' if not present
     return data[guild_id]
 
-locked_increment = {}
-
-async def reset_channel(channel, error_message, increment_message=None):
+async def reset_channel(channel, error_message, increment_message=None, typed_message=None):
     guild_id = str(channel.guild.id)
     server_data = get_server_data(guild_id)
 
     new_channel = await channel.clone()
     await channel.delete()
 
-    server_data['counter'] = 1
-    server_data['counting_channel_id'] = new_channel.id
-    last_user[guild_id] = None
+    if guild_id in last_user:
+        del last_user[guild_id]
 
-    if guild_id in locked_increment:
-        server_data['increment'] = locked_increment[guild_id]
-        del locked_increment[guild_id]
+    if 'next_increment' in server_data:
+        server_data['increment'] = server_data['next_increment']
+        del server_data['next_increment']
+    elif 'increment' not in server_data:
+        server_data['increment'] = 1  # Set default increment value if 'increment' is not set
+
+    server_data['counter'] = server_data['increment']  # Start the counter at the current increment value
+    server_data['counting_channel_id'] = new_channel.id
 
     save_data(data)
 
-    await new_channel.send(error_message)
+    messages = [error_message]
     if increment_message:
-        await new_channel.send(increment_message)
+        messages.append(increment_message)
+    if typed_message:
+        messages.append(typed_message)
+    combined_message = '\n\n'.join(messages)
+
+    await new_channel.send(combined_message)
     return new_channel
+
 
 @bot.event
 async def on_ready():
@@ -94,14 +111,21 @@ async def on_ready():
     activity_name = f'{server_count} Servers'
     activity = discord.Activity(type=discord.ActivityType.watching, name=activity_name)
     await bot.change_presence(activity=activity)
-    
+
 @bot.command(name='increment')
-async def increment(ctx, increment_value: int = 1):
+async def increment(ctx, increment_value: int = None):
     guild_id = str(ctx.guild.id)
     server_data = get_server_data(guild_id)
 
-    locked_increment[guild_id] = increment_value
-    await ctx.send(f"The increment value will be set to {locked_increment[guild_id]} at the start of the next game.")
+    if increment_value is not None:
+        if guild_id not in last_user:  # Check if no one has counted yet
+            server_data['next_increment'] = increment_value
+            save_data(data)
+            await ctx.send(f"The increment value will be set to {increment_value} at the start of the next game.")
+        else:
+            await ctx.send("The increment value cannot be changed until the current game is completed.")
+
+    save_data(data)
 
 @bot.command(name='set_channel')
 @commands.has_permissions(manage_channels=True)
@@ -130,35 +154,53 @@ async def on_message(message):
 
     if guild_id in last_user and message.author.id == last_user[guild_id]:
         error_message = f"Error: {message.author.mention}, you cannot count twice in a row. Wait for someone else to count."
-        increment_message = f"The current increment value is {server_data['increment']}." if server_data['increment'] != 1 else None
-        combined_message = f"{error_message}\n{increment_message}"
-        channel = await reset_channel(message.channel, combined_message)
-        await channel.send(combined_message)
-        return
-
-    try:
-        int_message = int(message.content)
-    except ValueError:
-        return
-
-    if int_message != server_data['counter']:
-        error_message = f"Error: {message.author.mention}, the next number should be {server_data['counter']}. You typed: '{message.content}'."
-        increment_message = f"The current increment value is {server_data['increment']}." if server_data['increment'] != 1 else None
-        combined_message = f"{error_message}\n{increment_message}"
-        channel = await reset_channel(message.channel, combined_message)
-        await channel.send(combined_message)
-        return
-
-    if server_data['counter'] > server_data['high_score']:
-        await message.add_reaction("🏆")
-        server_data['high_score'] = server_data['counter']
+    elif not re.fullmatch(r'\d+', message.content):
+        error_message = f"Error: {message.author.mention}, you typed an invalid expression or a non-integer."
     else:
-        await message.add_reaction("✅")
+        int_message = int(message.content)
+        expected_value = server_data['counter']
 
-    server_data['counter'] += server_data['increment']
-    last_user[guild_id] = message.author.id
+        if int_message != expected_value:
+            error_message = f"Error: {message.author.mention}, the next number should be {expected_value}."
+        else:
+            if server_data['counter'] > server_data['high_score']:
+                await message.add_reaction("🏆")
+                server_data['high_score'] = server_data['counter']
+            else:
+                await message.add_reaction("✅")
+            server_data['counter'] += server_data['increment']
+            last_user[guild_id] = message.author.id
+            save_data(data)
+            return
+
+    increment_message = f"The current increment value is {server_data['increment']}."
+    typed_message = f"You typed: '{message.content}'."
+    combined_message = f"{error_message}\n\n{increment_message}\n\n{typed_message}"
+    await reset_channel(message.channel, error_message, increment_message, typed_message)
+
 
     save_data(data)
+
+
+@bot.event
+async def on_guild_join(guild):
+    get_server_data(guild.id)
+    server_count = len(bot.guilds)
+    activity_name = f'{server_count} Servers'
+    activity = discord.Activity(type=discord.ActivityType.watching, name=activity_name)
+    await bot.change_presence(activity=activity)
+
+@bot.event
+async def on_guild_remove(guild):
+    guild_id = str(guild.id)
+    if guild_id in data:
+        del data[guild_id]
+        save_data(data)
+
+    server_count = len(bot.guilds)
+    activity_name = f'{server_count} Servers'
+    activity = discord.Activity(type=discord.ActivityType.watching, name=activity_name)
+    await bot.change_presence(activity=activity)
 
 bot.run(TOKEN)
 
