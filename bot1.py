e0f56d9d9735aaf07fef64fc0df883698e28ddf3
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-14 10:50:55 -0400

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -144,6 +144,7 @@ async def reset_channel(channel, error_message, increment_message, typed_message
 
     return new_channel
 
+
 @bot1.event
 async def on_message(message):
     if message.author == bot1.user:
