15c3d55df356d016dc4a87c9e569f9672799ec28
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-14 15:58:28 -0400

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,44 +1,8 @@
 import discord
-import json
 from discord.ext import commands
-import logging
-import re
+import json
 
-TOKEN1 = "MTEwNTU5ODczNjU1MTM4NzI0Nw.Gc2MCb.LXE8ptGi_uQqn0FBzvF461pMBAZUCzyP4nMRtY"
-
-logging.basicConfig(level=logging.INFO)
-
-intents = discord.Intents.default()
-intents.reactions = True
-intents.messages = True
-intents.message_content = True
-intents.guilds = True
-intents.members = True
-
-bot1 = commands.Bot(command_prefix='!', intents=intents)
-bot1.remove_command('help')
-
-class CustomHelpCommand(commands.HelpCommand):
-    def get_command_signature(self, command):
-        return f"{self.context.prefix}{command.qualified_name} {command.signature}"
-
-    async def send_bot_help(self, mapping):
-        embed = discord.Embed(title="Help", color=discord.Color.blue())
-        embed.set_thumbnail(url=self.context.bot.user.avatar.url)
-        for cog, commands in mapping.items():
-            filtered_commands = await self.filter_commands(commands, sort=True)
-            if filtered_commands:
-                command_signatures = [self.get_command_signature(c) for c in filtered_commands]
-                cog_name = getattr(cog, "qualified_name", "Other")
-                embed.add_field(name=cog_name, value="\n".join(command_signatures), inline=False)
-
-        await self.get_destination().send(embed=embed)
-        return
-
-bot1.help_command = CustomHelpCommand()
-
-last_user = {}
-data = {}
+bot1 = commands.Bot(command_prefix='!')
 
 def load_data():
     try:
@@ -48,29 +12,15 @@ def load_data():
     except FileNotFoundError:
         return {}, {}
 
-def save_data(data, last_user):
+def save_data(data):
     with open('counting_bot_data.json', 'w') as f:
-        json.dump({"data": data, "last_user": last_user}, f, indent=4)
+        json.dump(data, f, indent=4)
 
-data, last_user = load_data()
+@bot1.event
+async def on_ready():
+    print(f'{bot1.user} has connected to Discord!')
 
-def get_server_data(guild_id):
-    guild_id = str(guild_id)
-    if guild_id not in data:
-        data[guild_id] = {
-            'counter': 1,
-            'previous_count': 0,
-            'high_score': 0,
-            'counting_channel_id': None,
-            'increment': 1
-        }
-    else:
-        server_data = data[guild_id]
-        if 'next_increment' not in server_data:
-            server_data['next_increment'] = server_data.get('increment', 1)
-        else:
-            server_data['increment'] = server_data['next_increment']
-            del server_data['next_increment']@bot1.event
+@bot1.event
 async def on_message(message):
     if message.author == bot1.user:
         return
@@ -105,134 +55,53 @@ async def on_message(message):
         await message.add_reaction("✅")
         data[str(message.guild.id)]["last_number"] = typed_number
         save_data(data)
-        if 'increment' not in server_data:
-            server_data['increment'] = 1  # Set default value for 'increment' if not present
-    return data[guild_id]
 
-@bot1.event
-async def on_ready():
-    print(f'{bot1.user.name} is ready. Connected as {bot1.user.name}')
+# Commands
 
-@bot1.command(name='increment')
-async def increment(ctx, increment_value: int = None):
-    guild_id = str(ctx.guild.id)
-    server_data = get_server_data(guild_id)
-
-    if increment_value is not None:
-        server_data['next_increment'] = increment_value
-        save_data(data, last_user)
-        await ctx.send(f"The increment value will be set to {increment_value} at the start of the next game.")
-    else:
-        await ctx.send(f"The current increment value is {server_data['increment']}.")
-
-    save_data(data, last_user)
-
-@bot1.command(name='set_channel')
-@commands.has_permissions(manage_channels=True)
-async def set_counting_channel(ctx, channel: discord.TextChannel):
-    guild_id = str(ctx.guild.id)
-    server_data = get_server_data(guild_id)
-
-    if server_data['counting_channel_id'] != channel.id:
-        server_data['counting_channel_id'] = channel.id
-        save_data(data, last_user)
-        await ctx.send(f"Counting channel has been set to {channel.mention}.")
-    else:
-        await ctx.send("The counting channel is already set to that channel.")
+@bot1.command(name='set_increment')
+async def set_increment(ctx, value: int):
+    if value <= 0:
+        await ctx.send('Increment value must be greater than 0.')
         return
 
-async def reset_channel(channel):
+    data, _ = load_data()
+    server_data = data[str(ctx.guild.id)]
+    server_data['increment'] = value
+    save_data(data)
+    await ctx.send(f'Increment has been set to {value}.')
+
+@bot1.command(name='reset_channel')
+async def reset_channel(ctx):
+    error_message = "The counting channel has been reset."
+    increment_message = ""
+    typed_message = ""
+    new_channel = await reset_channel(ctx.channel, ctx.author.mention, error_message, increment_message, typed_message)
+    data, _ = load_data()
+    data[str(ctx.guild.id)]["counting_channel"] = new_channel.id
+    save_data(data)
+
+# Helper functions
+
+async def reset_channel(channel, mention, error_message, increment_message, typed_message):
     guild = channel.guild
-    if guild is None:
-        return
-
-    try:
-        await channel.delete(reason="Counting error")
-    except discord.errors.NotFound:
-        return
 
     overwrites = channel.overwrites
     category = channel.category
 
-    try:
-        new_channel = await guild.create_text_channel(name=channel.name, overwrites=overwrites, category=category)
-    except discord.errors.HTTPException:
-        return
+    await channel.delete(reason="Counting channel reset")
 
-    return new_channel
+    new_channel = await guild.create_text_channel(name=channel.name, overwrites=overwrites, category=category)
 
+    error_embed = discord.Embed(title="Counting Error", color=discord.Color.red(), description=f"{mention}, {error_message}")
+    if increment_message:
+        error_embed.add_field(name="Increment", value=increment_message, inline=False)
+    if typed_message:
+        error_embed.add_field(name="Typed Number", value=typed_message, inline=False)
 
-@bot1.event
-async def on_message(message):
-    if message.author == bot1.user:
-        return
-    
-    if message.content.startswith('!'):
-        await bot1.process_commands(message)
-        return
+    await new_channel.send(embed=error_embed)
 
-    data, last_user = load_data()
-    server_data = data[str(message.guild.id)]
-    counting_channel_id = server_data["counting_channel"]
+if __name__ == "__main__":
+    bot_token = "MTEwNTU5ODczNjU1MTM4NzI0Nw.G2ppAC.yAchMMCTjDWx7kcuVEMrO4LpeqSn6OuAVvsZkA"
+    bot1.run(bot_token)
 
-    if message.channel.id != counting_channel_id:
-        return
-
-    if not message.content.isdigit():
-        return
-
-    typed_number = int(message.content)
-    last_number = server_data["last_number"]
-    increment = server_data["increment"]
-    next_number = last_number + increment
-
-    if typed_number != next_number:
-        error_message = f"The next number should be {next_number}."
-        increment_message = f"The increment is currently set to {increment}."
-        typed_message = f"You typed: {typed_number}"
-        new_channel = await reset_channel(message.channel, message.author.mention, error_message, increment_message, typed_message)
-        data[str(message.guild.id)]["counting_channel"] = new_channel.id
-        save_data(data)
-    else:
-        await message.add_reaction("✅")
-        data[str(message.guild.id)]["last_number"] = typed_number
-        save_data(data)
-
-
-
-@bot1.event
-async def on_message_edit(before, after):
-    await bot1.process_commands(after)
-
-
-@bot1.event
-async def on_message_delete(message):
-    await bot1.process_commands(message)
-
-
-@bot1.event
-async def on_guild_join(guild):
-    server_count = len(bot1.guilds)
-    activity_name = f'{server_count} Servers'
-    activity = discord.Activity(type=discord.ActivityType.watching, name=activity_name)
-    await bot1.change_presence(activity=activity)
-
-
-@bot1.event
-async def on_guild_remove(guild):
-    server_count = len(bot1.guilds)
-    activity_name = f'{server_count} Servers'
-    activity = discord.Activity(type=discord.ActivityType.watching, name=activity_name)
-    await bot1.change_presence(activity=activity)
-
-    guild_id = str(guild.id)
-    if guild_id in data:
-        del data[guild_id]
-        save_data(data, last_user)
-    if guild_id in last_user:
-        del last_user[guild_id]
-        save_data(data, last_user)
-
-
-bot1.run(TOKEN1)
-
+  
