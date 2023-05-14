e1e302edd025a0598db21c06ae2233e713e9c76f
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-14 18:41:08 -0400

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,246 +1,97 @@
-import json
 import discord
 from discord.ext import commands
-import logging
-import re
-
-TOKEN1 = "MTEwNTU5ODczNjU1MTM4NzI0Nw.Gc2MCb.LXE8ptGi_uQqn0FBzvF461pMBAZUCzyP4nMRtY"
-
-logging.basicConfig(level=logging.DEBUG)
 
 intents = discord.Intents.default()
-intents.reactions = True
 intents.messages = True
-intents.message_content = True
-intents.guilds = True
-intents.members = True
+intents.reactions = True
+bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
 
-bot1 = commands.Bot(command_prefix='!', intents=intents)
-bot1.remove_command('help')
+counting_channels = {}
+increments = {}
+last_counters = {}
+high_scores = {}
 
-class CustomHelpCommand(commands.HelpCommand):
-    def get_command_signature(self, command):
-        return f"{self.context.prefix}{command.qualified_name} {command.signature}"
-
-    async def send_bot_help(self, mapping):
-        embed = discord.Embed(title="Help", color=discord.Color.blue())
-        embed.set_thumbnail(url=self.context.bot.user.avatar.url)
-        for cog, commands in mapping.items():
-            filtered_commands = await self.filter_commands(commands, sort=True)
-            if filtered_commands:
-                command_signatures = [self.get_command_signature(c) for c in filtered_commands]
-                cog_name = getattr(cog, "qualified_name", "Other")
-                embed.add_field(name=cog_name, value="\n".join(command_signatures), inline=False)
-
-        await self.get_destination().send(embed=embed)
-        return
-
-bot1.help_command = CustomHelpCommand()
-
-last_user = {}
-data = {}
-
-def load_data():
+async def check_counting_message(ctx, count, increment, last_counter):
+    if ctx.author.id == last_counter:
+        await ctx.channel.send(f"{ctx.author.mention}, you can't count twice in a row!")
+        return False
     try:
-        with open('counting_bot_data.json', 'r') as f:
-            loaded_data = json.load(f)
-            return loaded_data.get("data", {}), loaded_data.get("last_user", {})
-    except FileNotFoundError:
-        return {}, {}
+        count = int(count)
+    except ValueError:
+        await ctx.channel.send(f"{ctx.author.mention}, only numbers are allowed!")
+        return False
+    return count == increment
 
-def save_data(data, last_user):
-    with open('counting_bot_data.json', 'w') as f:
-        json.dump({"data": data, "last_user": last_user}, f, indent=4)
-
-data, last_user = load_data()
-
-def get_server_data(guild_id):
-    guild_id = str(guild_id)
-    if guild_id not in data:
-        data[guild_id] = {
-            'counter': 1,
-            'previous_count': 0,
-            'high_score': 0,
-            'counting_channel_id': None,
-            'increment': 1
-        }
-    else:
-        server_data = data[guild_id]
-        if 'next_increment' not in server_data:
-            server_data['next_increment'] = server_data.get('increment', 1)
-        else:
-            server_data['increment'] = server_data['next_increment']
-            del server_data['next_increment']
-        if 'increment' not in server_data:
-            server_data['increment'] = 1  # Set default value for 'increment' if not present
-    return data[guild_id]
-
-@bot1.event
+@bot.event
 async def on_ready():
-    print(f'{bot1.user.name} is ready. Connected as {bot1.user.name}')
+    print(f'Logged in as {bot.user}')
 
-@bot1.command(name='increment')
-async def increment(ctx, increment_value: int = None):
-    guild_id = str(ctx.guild.id)
-    server_data = get_server_data(guild_id)
+@bot.command()
+async def set_channel(ctx, channel: discord.TextChannel):
+    counting_channels[ctx.guild.id] = channel.id
+    increments[ctx.guild.id] = 1
+    last_counters[ctx.guild.id] = None
+    high_scores[ctx.guild.id] = 0
+    await ctx.send(f"Counting channel set to {channel.mention}")
 
-    if increment_value is not None:
-        server_data['next_increment'] = increment_value
-        save_data(data, last_user)
-        await ctx.send(f"The increment value will be set to {increment_value} at the start of the next game.")
-    else:
-        await ctx.send(f"The current increment value is {server_data['increment']}.")
+@bot.command()
+async def increment(ctx, value: int):
+    increments[ctx.guild.id] = value
+    await ctx.send(f"Increment value set to {value}")
 
-    save_data(data, last_user)
+@bot.command()
+async def help(ctx):
+    embed = discord.Embed(
+        title="Counting Bot Help",
+        description="List of commands for the Counting Bot",
+        color=discord.Color.blue()
+    )
 
-@bot1.command(name='set_channel')
-@commands.has_permissions(manage_channels=True)
-async def set_counting_channel(ctx, channel: discord.TextChannel):
-    guild_id = str(ctx.guild.id)
-    server_data = get_server_data(guild_id)
+    embed.add_field(
+        name="!set_channel [channel]",
+        value="Set the counting channel to [channel]",
+        inline=False
+    )
 
-    if server_data['counting_channel_id'] != channel.id:
-        server_data['counting_channel_id'] = channel.id
-        save_data(data, last_user)
-        await ctx.send(f"Counting channel has been set to {channel.mention}.")
-    else:
-        await ctx.send("The counting channel is already set to that channel.")
-        return
+    embed.add_field(
+        name="!increment [value]",
+        value="Set the increment value for counting",
+        inline=False
+    )
 
-async def reset_channel(channel, error_message, increment_message, typed_message):
-    logging.info("Resetting channel...")
-    logging.info(f"Channel name: {channel.name}")
-    logging.info(f"Error message: {error_message}")
-    logging.info(f"Increment message: {increment_message}")
-    logging.info(f"Typed message: {typed_message}")
-    
-    guild = channel.guild
-    overwrites = channel.overwrites
-    category = channel.category
+    embed.add_field(
+        name="!help",
+        value="Display this help message",
+        inline=False
+    )
 
-    try:
-        await channel.delete(reason="Counting error")
-        logging.info("Channel deleted.")
-    except discord.Forbidden:
-        logging.error(f"Unable to delete channel: {channel.name}")
+    await ctx.send(embed=embed)
 
-    try:
-        new_channel = await guild.create_text_channel(name=channel.name, overwrites=overwrites, category=category)
-        error_embed = discord.Embed(title="Counting Error", color=discord.Color.red())
-        error_embed.add_field(name="Error Message", value=error_message, inline=False)
-        error_embed.add_field(name="Increment", value=increment_message, inline=False)
-        error_embed.add_field(name="Typed Message", value=typed_message, inline=False)
-        await new_channel.send(embed=error_embed)
-        logging.info("New channel created.")
-    except discord.Forbidden:
-        logging.error("Unable to create new channel.")
-
-    return new_channel
-
-
-
-
-
-
-
-
-@bot1.event
+@bot.event
 async def on_message(message):
-    if message.author == bot1.user:
+    if message.author.bot:
         return
 
-    guild_id = str(message.guild.id)
-    server_data = get_server_data(guild_id)
+    if message.guild and message.channel.id == counting_channels.get(message.guild.id):
+        guild_id = message.guild.id
+        increment = increments[guild_id]
+        last_counter = last_counters[guild_id]
 
-    if server_data.get('counting_channel_id') is None or message.channel.id != server_data.get('counting_channel_id'):
-        await bot1.process_commands(message)
-        return
+        if await check_counting_message(message, message.content, increment, last_counter):
+            last_counters[guild_id] = message.author.id
+            await message.add_reaction("✅")
 
-    if message.content.startswith(bot1.command_prefix):
-        await bot1.process_commands(message)
-        return
-
-    logging.debug(f"Received message: {message.content}")
-
-    if guild_id in last_user and message.author.id == last_user[guild_id]:
-        error_message = f"Error: {message.author.mention}, you cannot count twice in a row. Wait for someone else to count."
-        ping_message = await message.channel.send(error_message)
-        await ping_message.delete()  # Deletes the message immediately
-        logging.debug("Counting Error: User counted twice in a row")
-        return
-    else:
-        try:
-            int_message = int(eval("".join(re.findall(r'\d+|\+|\-|\*|x|\/|\(|\)', message.content.replace('x', '*')))))
-        except (ValueError, TypeError, NameError, ZeroDivisionError, SyntaxError):
-            error_message = f"Error: {message.author.mention}, you typed an invalid expression or a non-integer."
-            logging.debug("Counting Error: Invalid input")
+            score = int(message.content)
+            if score > high_scores[guild_id]:
+                high_scores[guild_id] = score
+                await message.add_reaction("🏆")
         else:
-            expected_value = server_data['counter']
+            await message.channel.delete()
+            new_channel = await message.guild.create_text_channel("counting")
+            counting_channels[guild_id] = new_channel.id
+            embed = discord.Embed(title="Counting Failure", description=f"Reason: Invalid count\nIncrement: {increment}\nFailed message: {message.content}", color=0xFF0000)
+            await new_channel.send(embed=embed)
+    else:
+        await bot.process_commands(message)
 
-            if int_message != expected_value:
-                error_message = f"Error: {message.author.mention}, the next number should be {expected_value}."
-                # Reset the counter after the game fails
-                server_data['counter'] = 1
-                logging.debug("Counting Error: Incorrect count")
-            else:
-                if server_data['counter'] > server_data['high_score']:
-                    await message.add_reaction("🏆")
-                    server_data['high_score'] = server_data['counter']
-                else:
-                    await message.add_reaction("✅")
-                server_data['counter'] += server_data['increment']
-                last_user[guild_id] = message.author.id
-                save_data(data, last_user)
-                await reset_channel(message.channel, error_message, increment_message, typed_message)
-                await bot1.process_commands(message)
-                return
-
-    increment_message = f"The increment is currently set to {server_data['increment']}."
-    typed_message = f"You typed: {message.content}"
-    new_channel = await reset_channel(message.channel, error_message, increment_message, typed_message)
-    server_data['counting_channel_id'] = new_channel.id
-    save_data(data, last_user)
-
-    logging.debug("Resetting the channel...")
-    logging.debug(f"Channel name: {message.channel.name}")
-    logging.debug(f"Error message: {error_message}")
-    logging.debug(f"Increment message: {increment_message}")
-    logging.debug(f"Typed message: {typed_message}")
-
-    await bot1.process_commands(message)
-
-
-
-
-
-
-
-
-
-
-
-
-
-@bot1.event
-async def on_guild_join(guild):
-    server_count = len(bot1.guilds)
-    activity_name = f'{server_count} Servers'
-    activity = discord.Activity(type=discord.ActivityType.watching, name=activity_name)
-    await bot1.change_presence(activity=activity)
-
-@bot1.event
-async def on_guild_remove(guild):
-    server_count = len(bot1.guilds)
-    activity_name = f'{server_count} Servers'
-    activity = discord.Activity(type=discord.ActivityType.watching, name=activity_name)
-    await bot1.change_presence(activity=activity)
-
-    guild_id = str(guild.id)
-    if guild_id in data:
-        del data[guild_id]
-        save_data(data, last_user)
-    if guild_id in last_user:
-        del last_user[guild_id]
-        save_data(data, last_user)
-
-bot1.run(TOKEN1)
+bot.run('MTEwNTU5ODczNjU1MTM4NzI0Nw.Gc2MCb.LXE8ptGi_uQqn0FBzvF461pMBAZUCzyP4nMRtY')
