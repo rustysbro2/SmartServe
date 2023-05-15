fe726f2c57e07daff119bda96bcfc4a06067d3f8
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-15 09:24:05 -0400

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -125,7 +125,7 @@ async def on_message(message):
 
         print(f"[DEBUG] Checking count message ({message.content}) in guild ({message.guild.id})")  # Debug message
 
-        is_valid, result = await check_counting_message(message, message.content, increment, last_counter)
+        is_valid, result = await check_counting_message(message, message.content, increment, last_counters)
         if is_valid:
             await message.add_reaction("✅")
             last_counters[message.guild.id] = result
