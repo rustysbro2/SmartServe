14e5e040688348c9da5fad273b82e44da38b7284
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-14 21:36:23 -0400

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,11 +1,10 @@
 import discord
 from discord.ext import commands
-import asyncio
 import ast
-import operator
 
-intents = discord.Intents.all()
-bot1 = commands.Bot(command_prefix='!', intents=intents, help_command=None)
+intents = discord.Intents.default()
+intents.message_content = True
+bot1 = commands.Bot(command_prefix="!", intents=intents)
 
 counting_channels = {}
 increments = {}
@@ -17,8 +16,12 @@ allowed_operators = {
     ast.Sub: operator.sub,
     ast.Mult: operator.mul,
     ast.Div: operator.truediv,
-    ast.Pow: operator.pow,
     ast.FloorDiv: operator.floordiv,
+    ast.Mod: operator.mod,
+    ast.Pow: operator.pow,
+    ast.BitXor: operator.xor,
+    ast.USub: operator.neg,
+    ast.UAdd: operator.pos
 }
 
 async def check_counting_message(message, count, increment, last_counter):
@@ -44,11 +47,6 @@ async def check_counting_message(message, count, increment, last_counter):
 
     return result == increment, result
 
-
-@bot1.event
-async def on_ready():
-    print(f'Logged in as {bot1.user}')
-
 @bot1.command()
 async def set_channel(ctx, channel: discord.TextChannel):
     counting_channels[ctx.guild.id] = channel.id
@@ -58,37 +56,9 @@ async def set_channel(ctx, channel: discord.TextChannel):
     await ctx.send(f"Counting channel set to {channel.mention}")
 
 @bot1.command()
-async def increment(ctx, value: int):
-    increments[ctx.guild.id] = value
-    await ctx.send(f"Increment value set to {value}")
-
-@bot1.command()
-async def help(ctx):
-    embed = discord.Embed(
-        title="Counting Bot Help",
-        description="List of commands for the Counting Bot",
-        color=discord.Color.blue()
-    )
-
-    embed.add_field(
-        name="!set_channel [channel]",
-        value="Set the counting channel to [channel]",
-        inline=False
-    )
-
-    embed.add_field(
-        name="!increment [value]",
-        value="Set the increment value for counting",
-        inline=False
-    )
-
-    embed.add_field(
-        name="!help",
-        value="Display this help message",
-        inline=False
-    )
-
-    await ctx.send(embed=embed)
+async def increment(ctx, num: int):
+    increments[ctx.guild.id] = num
+    await ctx.send(f"Increment changed to {num}")
 
 @bot1.event
 async def on_message(message):
@@ -103,39 +73,46 @@ async def on_message(message):
         is_valid, result = await check_counting_message(message, message.content, increment, last_counter)
         if is_valid:
             last_counters[guild_id] = message.author.id
-            increments[guild_id] = result + 1  # Update the increment value with the last valid result + 1
+            increments[guild_id] = result + 1
             await message.add_reaction("✅")
 
             if result > high_scores[guild_id]:
                 high_scores[guild_id] = result
                 await message.add_reaction("🏆")
         else:
-            # Store channel properties before deletion
             channel_name = message.channel.name
             channel_position = message.channel.position
             channel_category = message.channel.category
-            channel_permissions = message.channel.permission_overwrites
+            channel_overwrites = message.channel.overwrites
             channel_topic = message.channel.topic
 
-            # Delete the channel
             await message.channel.delete()
 
-            # Create a new channel with the same properties
             new_channel = await message.guild.create_text_channel(
                 name=channel_name,
                 position=channel_position,
                 category=channel_category,
-                overwrites=channel_permissions,
+                overwrites=channel_overwrites,
                 topic=channel_topic
             )
             counting_channels[guild_id] = new_channel.id
-            last_counters[guild_id] = None  # Clear the last user for the specific server
-            increments[guild_id] = 1  # Reset the increment value to 1
+            last_counters[guild_id] = None
+            increments[guild_id] = 1
             embed = discord.Embed(title="Counting Failure", description=f"Reason: Invalid count\nIncrement: {increment}\nFailed message: {message.content}", color=0xFF0000)
             await new_channel.send(embed=embed)
     else:
         await bot1.process_commands(message)
 
+@bot1.command()
+async def help(ctx):
+    embed = discord.Embed(title="Counting Bot Help", description="List of commands for the counting bot:", color=0x00FF00)
+    embed.add_field(name="!set_channel [channel]", value="Sets the channel for counting.", inline=False)
+    embed.add_field(name="!increment [number]", value="Changes the counting increment.", inline=False)
+    await ctx.send(embed=embed)
 
+@bot1.event
+async def on_ready():
+    print(f'{bot1.user} has connected to Discord!')
+
+bot1.remove_command("help")
 bot1.run('MTEwNTU5ODczNjU1MTM4NzI0Nw.Gc2MCb.LXE8ptGi_uQqn0FBzvF461pMBAZUCzyP4nMRtY')
-
