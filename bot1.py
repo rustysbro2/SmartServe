4977b03ea1242d6dc1f86cb2a6fb299f437a3518
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-16 16:03:50 -0400

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -8,7 +8,6 @@ import asyncio
 logging.basicConfig(level=logging.DEBUG)
 
 bot_token = 'MTEwNTU5ODczNjU1MTM4NzI0Nw.G-i9vg.q3zXGRKAvdtozwU0JzSpWCSDH1bfLHvGX801RY'
-intents = discord.Intents.default()
 intents.message_reactions = True
 intents = discord.Intents.all()
 
