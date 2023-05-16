a213b54b89350f77cb54c91e5bef422ac6084611
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-16 16:06:12 -0400

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 bot_token = 'MTEwNTU5ODczNjU1MTM4NzI0Nw.G-i9vg.q3zXGRKAvdtozwU0JzSpWCSDH1bfLHvGX801RY'
 intents = discord.Intents.all()
-intents.message_reactions = True
+intents.reactions = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 mydb = {}
