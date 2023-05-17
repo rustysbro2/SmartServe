6b23f15414ba3a418d67a320ff8218259147b048
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-17 11:07:17 -0400

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import os
 import json
+import ast
+import operator as op
 
 intents = discord.Intents().all()
 bot = commands.Bot(command_prefix='!', intents=intents)
