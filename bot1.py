5c560de2a4b8dd6882b4fc0d896d03ecb23e542d
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-17 09:00:49 -0400

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
-from discord_slash import SlashContext
-from discord_slash import cog_ext
+from discord_slash import SlashContext, cog_ext
 from discord_slash.utils.manage_commands import create_option
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
