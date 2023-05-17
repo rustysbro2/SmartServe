746994d9c033b0eb2a3d25444d352329cc037021
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-17 10:00:09 -0400

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -1,29 +1,27 @@
 import discord
 from discord.ext import commands
+from discord_slash import SlashCommand
 
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
 
 bot = commands.Bot(command_prefix="!", intents=intents)
-
+slash = SlashCommand(bot)
 
 @bot.event
 async def on_ready():
     print(f"Bot is ready. Logged in as {bot.user}")
 
-
-@bot.slash_command()
+@slash.slash(name="hello", description="Say hello to the bot")
 async def hello(ctx: discord.SlashContext):
     await ctx.send("Hello, world!")
 
-
-@bot.slash_command()
+@slash.slash(name="echo", description="Echoes a message")
 async def echo(ctx: discord.SlashContext, message: str):
     await ctx.send(message)
 
-
-@bot.slash_command(name="add")
+@slash.slash(name="add", description="Adds two numbers")
 async def add_numbers(ctx: discord.SlashContext, number1: int, number2: int):
     result = number1 + number2
     await ctx.send(f"The sum of {number1} and {number2} is {result}")
