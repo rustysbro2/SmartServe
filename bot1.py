8ce6297d0ccd388619e905feb94faba0dc165a01
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-17 14:59:05 -0400

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -72,10 +72,13 @@ async def set_channel(ctx, channel: discord.TextChannel):
         all_data = json.load(f)
     data = all_data.get(str(ctx.guild.id), default_data.copy())
     data['channel_id'] = channel.id
+    if 'new_channel' in all_data[str(ctx.guild.id)]:
+        del all_data[str(ctx.guild.id)]['new_channel']  # Remove new_channel flag
     all_data[str(ctx.guild.id)] = data
     with open(data_file, 'w') as f:
         json.dump(all_data, f, indent=4)
     await ctx.send(f'Counting channel has been set to {channel.mention}')
+
 
 
 @bot.command()
