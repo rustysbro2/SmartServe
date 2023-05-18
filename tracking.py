ec8b237991486d60001b4bcc8e61255a3725f532
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-17 22:16:53 -0400

    Update tracking.py

--- tracking.py
+++ tracking.py
@@ -6,38 +6,37 @@ class Tracking(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.Cog.listener()
+    async def on_invite_create(self, invite):
+        guild_id = str(invite.guild.id)
+
+        # Load existing data from the JSON file
+        with open('invite_data.json', 'r') as f:
+            all_data = json.load(f)
+
+        # Update the invite data for the guild
+        all_data[guild_id] = invite.code
+
+        # Save the updated data back to the JSON file
+        with open('invite_data.json', 'w') as f:
+            json.dump(all_data, f, indent=4)
+
     @commands.command()
-    @commands.has_permissions(administrator=True)
-    async def tracking(self, ctx, channel: discord.TextChannel = None):
+    async def tracking(self, ctx):
         guild_id = str(ctx.guild.id)
 
         # Load existing data from the JSON file
-        with open('count_data.json', 'r') as f:
+        with open('invite_data.json', 'r') as f:
             all_data = json.load(f)
 
-        if channel is None:
-            # Retrieve the current tracking channel for the guild
-            data = all_data.get(guild_id)
-            if data and data['tracking_channel_id']:
-                channel_id = data['tracking_channel_id']
-                channel = self.bot.get_channel(int(channel_id))
-                if channel:
-                    await ctx.send(f"The current tracking channel is {channel.mention}.")
-                else:
-                    await ctx.send("The current tracking channel is invalid or no longer accessible.")
-            else:
-                await ctx.send("No tracking channel is currently set.")
+        # Retrieve the current tracking invite for the guild
+        invite_code = all_data.get(guild_id)
+
+        if invite_code:
+            invite_url = f"https://discord.gg/{invite_code}"
+            await ctx.send(f"The current tracking invite is: {invite_url}.")
         else:
-            # Update the tracking channel ID for the guild
-            all_data[guild_id] = {
-                'tracking_channel_id': str(channel.id)
-            }
-
-            # Save the updated data back to the JSON file
-            with open('count_data.json', 'w') as f:
-                json.dump(all_data, f, indent=4)
-
-            await ctx.send(f"The tracking channel has been set to {channel.mention}.")
+            await ctx.send("No tracking invite is currently set.")
 
 def setup(bot):
     bot.add_cog(Tracking(bot))
