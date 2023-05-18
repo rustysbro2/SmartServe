d88681ba47d966284c1fdb28013a6353c31e28ec
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-17 22:29:48 -0400

    Update tracking.py

--- tracking.py
+++ tracking.py
@@ -5,37 +5,53 @@ import json
 class Tracking(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.data_file = 'invite_data.json'
+        self.default_data = {
+            'channel_id': None
+        }
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
         guild = member.guild
 
-        # Load existing invite data from the JSON file
-        with open('invite_data.json', 'r') as f:
-            all_data = json.load(f)
+        # Load existing data from the JSON file
+        all_data = self.load_data()
 
-        # Retrieve the invites for the guild
-        invites = await guild.invites()
+        # Retrieve the designated channel for join messages
+        channel_id = all_data.get(str(guild.id), {}).get('channel_id')
+        if channel_id:
+            channel = self.bot.get_channel(channel_id)
+            if channel:
+                message = f"{member.mention} has joined the server!"
+                await channel.send(message)
 
-        # Find the invite that matches the member's join
-        for invite in invites:
-            if invite.inviter == member:
-                inviter = invite.inviter
-                invite_code = invite.code
-                invite_type = self.get_invite_type(invite)
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def set_join_channel(self, ctx, channel: discord.TextChannel):
+        guild = ctx.guild
 
-                print(f"{member.name}#{member.discriminator} has joined the server {guild.name} and was invited by {inviter.name}#{inviter.discriminator} (Invite Type: {invite_type}, Invite Code: {invite_code})")
-                break
+        # Load existing data from the JSON file
+        all_data = self.load_data()
 
-    def get_invite_type(self, invite):
-        if invite.max_uses == 0 and invite.max_age == 0 and invite.temporary:
-            return "Vanity"
-        elif invite.max_uses == 0 and invite.max_age == 0 and not invite.temporary:
-            return "Permanent Instant Invite"
-        elif invite.max_uses > 0 and invite.max_age == 0 and not invite.temporary:
-            return "Permanent Invite"
-        else:
-            return "Temporary Invite"
+        # Update the join channel ID for the guild
+        all_data[str(guild.id)] = {
+            'channel_id': channel.id
+        }
+        self.save_data(all_data)
+
+        await ctx.send(f"The join channel has been set to {channel.mention}.")
+
+    def load_data(self):
+        try:
+            with open(self.data_file, 'r') as f:
+                all_data = json.load(f)
+        except FileNotFoundError:
+            all_data = {}
+        return all_data
+
+    def save_data(self, all_data):
+        with open(self.data_file, 'w') as f:
+            json.dump(all_data, f, indent=4)
 
 def setup(bot):
     bot.add_cog(Tracking(bot))
