fd11c3c901da1e783e81ecbb8d774133a9276449
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-17 21:44:36 -0400

    Update tracking.py

--- tracking.py
+++ tracking.py
@@ -1,57 +1,43 @@
 import discord
 from discord.ext import commands
+import json
 
 class Tracking(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_invite_create(self, invite):
-        # Get the tracking channel ID from the data file
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def tracking(self, ctx, channel: discord.TextChannel = None):
+        guild_id = str(ctx.guild.id)
+
+        # Load existing data from the JSON file
         with open('count_data.json', 'r') as f:
             all_data = json.load(f)
 
-        guild_id = str(invite.guild.id)
-        data = all_data.get(guild_id)
-        if data and data['tracking_channel_id']:
-            channel_id = data['tracking_channel_id']
+        if channel is None:
+            # Retrieve the current tracking channel for the guild
+            data = all_data.get(guild_id)
+            if data and data['tracking_channel_id']:
+                channel_id = data['tracking_channel_id']
+                channel = self.bot.get_channel(int(channel_id))
+                if channel:
+                    await ctx.send(f"The current tracking channel is {channel.mention}.")
+                else:
+                    await ctx.send("The current tracking channel is invalid or no longer accessible.")
+            else:
+                await ctx.send("No tracking channel is currently set.")
+        else:
+            # Update the tracking channel ID for the guild
+            all_data[guild_id] = {
+                'tracking_channel_id': str(channel.id)
+            }
 
-            # Find the tracking channel in the guild
-            guild = self.bot.get_guild(invite.guild.id)
-            tracking_channel = guild.get_channel(channel_id)
+            # Save the updated data back to the JSON file
+            with open('count_data.json', 'w') as f:
+                json.dump(all_data, f, indent=4)
 
-            if tracking_channel:
-                inviter = invite.inviter
-                tracking_message = f"New invite created by {inviter.mention}: {invite.url}"
-                await tracking_channel.send(tracking_message)
-
-    @commands.Cog.listener()
-    async def on_member_join(self, member):
-        # Get the tracking channel ID from the data file
-        with open('count_data.json', 'r') as f:
-            all_data = json.load(f)
-
-        guild_id = str(member.guild.id)
-        data = all_data.get(guild_id)
-        if data and data['tracking_channel_id']:
-            channel_id = data['tracking_channel_id']
-
-            # Find the tracking channel in the guild
-            guild = self.bot.get_guild(member.guild.id)
-            tracking_channel = guild.get_channel(channel_id)
-
-            if tracking_channel:
-                inviter = await self.get_inviter(member)
-                tracking_message = f"New member joined from {inviter.mention}: {member.mention}"
-                await tracking_channel.send(tracking_message)
-
-    async def get_inviter(self, member):
-        # Fetch the audit logs for member joins
-        logs = await member.guild.audit_logs(limit=10).flatten()
-        for log in logs:
-            if log.action == discord.AuditLogAction.invite_create and log.target.code == member.guild.me.id:
-                return log.user
-        return None
+            await ctx.send(f"The tracking channel has been set to {channel.mention}.")
 
 def setup(bot):
     bot.add_cog(Tracking(bot))
