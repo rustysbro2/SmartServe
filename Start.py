e7f69dfa1dab6c802b9b18c34b5eba7af1accd72
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-19 17:15:24 -0400

    Update Start.py

--- Start.py
+++ Start.py
@@ -1,10 +1,10 @@
 import subprocess
 
 # Start bot1.py
-bot1_process = subprocess.Popen(['python3', 'bot1.py'])
+bot1_process = subprocess.Popen(['python3', 'counting.py'])
 
 # Start bot2.py
-bot2_process = subprocess.Popen(['python3', 'bot2.py'])
+bot2_process = subprocess.Popen(['python3', 'WebBot.py'])
 
 # Wait for both processes to complete
 bot1_process.wait()
