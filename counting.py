e34421ad11743d85c23c3ca6b383ed2dfbeeedf4
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-19 19:04:49 -0400

    Update counting.py

--- counting.py
+++ counting.py
@@ -105,7 +105,7 @@ async def on_ready():
 
     for extension in extensions:
         try:
-            bot.load_extension(f"cogs.{extension}")  # if the extensions are in a folder named cogs
+            bot.load_extension(extension)  # if the extensions are in the same folder as this script
             print(f"Extension '{extension}' loaded successfully.")
         except commands.ExtensionError as e:
             print(f"Failed to load extension '{extension}': {e}")
