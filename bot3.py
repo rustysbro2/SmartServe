09c90bf60f9fbf327b4b612275ee3aead0ab5825
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-19 17:01:30 -0400

    Update bot3.py

--- bot3.py
+++ bot3.py
@@ -26,7 +26,7 @@ default_data = {
 }
 
 # Add your extension names here
-extensions = ['giveaway', 'tracking']
+
 
 # emojis lists
 check_mark_emojis = ['✅', '☑️', '✔️']
@@ -273,4 +273,4 @@ async def on_message(message):
             await ping_msg.delete()
 
 
-bot.run('YOUR_BOT_TOKEN')
+bot.run('')
