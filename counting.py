3e9e81ff6f4000ac66c25ae2225d535e261d124e
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-19 23:26:22 -0400

    Update counting.py

--- counting.py
+++ counting.py
@@ -138,6 +138,10 @@ async def generate_help_data():
                     example = generate_command_example(command)
                     help_data[command.name] = {'usage': usage, 'example': example}
 
+    if not help_data:
+        print("No commands found.")
+        return
+
     try:
         with open('help_data.json', 'w') as f:
             json.dump(help_data, f, indent=4)
@@ -145,6 +149,7 @@ async def generate_help_data():
         print("Help data generated successfully.")
     except Exception as e:
         print(f"Error generating help data: {e}")
+
 
 
 
