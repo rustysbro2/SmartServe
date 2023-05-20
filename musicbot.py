920fe85fdfd6e3b9c93a4cf6812697bd319a402d
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-20 12:49:08 -0400

    Update musicbot.py

--- musicbot.py
+++ musicbot.py
@@ -20,12 +20,23 @@ class MusicBot(commands.Cog):
     async def play(self, ctx, url):
         if not ctx.voice_client:
             await ctx.invoke(self.bot.get_command("join"))
-        
-        with youtube_dl.YoutubeDL({'format': 'bestaudio', 'noplaylist':'True'}) as ydl:
+
+        # Specify the `format` parameter to download a lower quality audio format.
+        with youtube_dl.YoutubeDL({'format': 'bestaudio[ext=m4a]', 'noplaylist':'True'}) as ydl:
             info = ydl.extract_info(url, download=False)
             url2 = info['formats'][0]['url']
             ctx.voice_client.play(discord.FFmpegPCMAudio(url2))
 
+        # Handle errors gracefully.
+        try:
+            info = ydl.extract_info(url, download=False)
+            url2 = info['formats'][0]['url']
+            ctx.voice_client.play(discord.FFmpegPCMAudio(url2))
+        except Exception as e:
+            print(e)
+            return
+
 
 def setup(bot):
     bot.add_cog(MusicBot(bot))
+
