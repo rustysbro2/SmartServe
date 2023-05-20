10e33541cc78b35485c669a5636cba2d2612c0fc
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-19 22:14:39 -0400

    Update counting.py

--- counting.py
+++ counting.py
@@ -3,13 +3,6 @@ from discord.ext import commands
 import os
 import json
 import inspect
-import tracemalloc
-import random
-from giveaway import Giveaway
-from tracking import Tracking  # Import the Tracking class
-from musicbot import MusicBot
-
-tracemalloc.start()
 
 intents = discord.Intents().all()
 bot = commands.Bot(command_prefix='!', intents=intents)
@@ -26,35 +19,64 @@ default_data = {
     'increment': 1,
     'pending_increment': None,
     'old_increment': 1,
-    'successful_counts': 0  # add this line
+    'successful_counts': 0
 }
 
 # Add your extension names here
 extensions = ['musicbot', 'giveaway', 'tracking']
 
-# emojis lists
-check_mark_emojis = ['✅', '☑️', '✔️']
-trophy_emojis = ['🏆', '🥇', '🥈', '🥉']
-
 def ensure_data_file_exists():
     if not os.path.exists(data_file):
         with open(data_file, 'w') as f:
-            json.dump({}, f, indent=4)
+            json.dump(default_data, f, indent=4)
 
-def generate_command_example(command):
-    params = inspect.signature(command.callback).parameters.values()
-    args = []
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send('Invalid command.')
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send('You missed some required arguments.')
+    else:
+        raise error
 
-    for param in params:
-        if param.name not in ['self', 'ctx']:
-            if param.default is param.empty:
-                args.append(f"<{param.name}>")
-            else:
-                args.append(f"[{param.name}]")
+@bot.event
+async def on_ready():
+    print(f"We have logged in as {bot.user}")
 
-    example = f"!{command.name} {' '.join(args)}"
-    return example
+    if not os.path.exists(data_file):
+        with open(data_file, 'w') as f:
+            json.dump(default_data, f, indent=4)
 
+    for extension in extensions:
+        try:
+            bot.load_extension(extension)  # Load the extension
+            print(f"Extension '{extension}' loaded successfully.")
+        except commands.ExtensionError as e:
+            print(f"Failed to load extension '{extension}': {e}")
+
+@bot.command()
+async def help(ctx, command_name: str = None):
+    embed = discord.Embed(title="Bot Help", color=discord.Color.blue())
+    embed.set_thumbnail(url=bot.user.avatar.url)
+    embed.description = "Welcome to the Bot Help!\nHere are the available commands:"
+
+    if command_name is None:
+        for command in bot.commands:
+            if not command.hidden:
+                usage = get_command_usage(command)
+                example = generate_command_example(command)
+                embed.add_field(name=f"**!{command.name}**", value=f"{usage}\nExample: {example}", inline=False)
+    else:
+        command = bot.get_command(command_name)
+        if command and not command.hidden:
+            usage = get_command_usage(command)
+            example = generate_command_example(command)
+            embed.add_field(name=f"**!{command.name}**", value=f"{usage}\nExample: {example}", inline=False)
+        else:
+            embed.description = f"No information found for command: `!{command_name}`"
+
+    embed.set_footer(text="For more information, contact the bot owner.")
+    await ctx.send(embed=embed)
 
 def get_command_usage(command):
     signature = f"!{command.name}"
@@ -69,127 +91,8 @@ def get_command_usage(command):
                 params_str.append(f"<{param.name}>")
 
     usage = " ".join(params_str)
-    return f"{signature} {usage}"
-
-async def generate_help_data():
-    help_data = {}
-
-    for extension in extensions:
-        ext = bot.get_cog(extension)
-        print(f"Extension: {extension}, Cog: {ext}")
-        if ext:
-            for command in ext.get_commands():
-                if not command.hidden:
-                    usage = get_command_usage(command)
-                    example = generate_command_example(command)
-                    help_data[command.name] = {'usage': usage, 'example': example}
-
-    try:
-        with open('help_data.json', 'w') as f:
-            json.dump(help_data, f, indent=4)
-
-        print("Help data generated successfully.")
-    except Exception as e:
-        print(f"Error generating help data: {e}")
-
-
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        await ctx.send('Invalid command.')
-    elif isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send('You missed some required arguments.')
-    else:
-        raise error
-
-@bot.event
-async def on_ready():
-    print(f"We have logged in as {bot.user}")
-    ensure_data_file_exists()
-
-    with open(data_file, 'r') as f:
-        all_data = json.load(f)
-
-    for guild in bot.guilds:
-        guild_id = str(guild.id)
-        if guild_id not in all_data:
-            all_data[guild_id] = default_data.copy()
-        else:
-            existing_data = all_data[guild_id]
-            for key, value in default_data.items():
-                if key not in existing_data:
-                    existing_data[key] = value
-
-    with open(data_file, 'w') as f:
-        json.dump(all_data, f, indent=4)
-
-    await generate_help_data()  # Generate help data
-
-    for extension in extensions:
-        try:
-            bot.load_extension(extension)  # Load the extension
-            print(f"Extension '{extension}' loaded successfully.")
-        except commands.ExtensionError as e:
-            print(f"Failed to load extension '{extension}': {e}")
-
-    await bot.add_cog(Giveaway(bot))  # Add the Giveaway cog
-    await bot.add_cog(Tracking(bot))  # Add the Tracking cog
-    await bot.add_cog(MusicBot(bot))  # Add the MusicBot cog
-
-    print("Extensions and cogs loaded successfully.")
-
-
-
-...
-
-# Rest of your code
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-bot.remove_command('help')
-@bot.command()
-async def help(ctx, command_name: str = None):
-    try:
-        with open('help_data.json', 'r') as f:
-            help_data = json.load(f)
-    except FileNotFoundError:
-        help_data = {}
-
-    embed = discord.Embed(title="Bot Help", color=discord.Color.blue())
-    embed.set_thumbnail(url=bot.user.avatar.url)
-    embed.description = "Welcome to the Bot Help!\nHere are the available commands:"
-
-    if command_name is None:
-        for cmd, usage in help_data.items():
-            example = usage['example']
-            value = f"`{usage['usage']}`\nExample: {example}" if example else f"`{usage['usage']}`"
-            embed.add_field(name=f"**{cmd}**", value=value, inline=False)
-    else:
-        usage = help_data.get(command_name)
-        if usage:
-            example = usage['example']
-            value = f"`{usage['usage']}`\nExample: {example}" if example else f"`{usage['usage']}`"
-            embed.add_field(name=f"**{command_name}**", value=value, inline=False)
-        else:
-            embed.description = f"No information found for command: `{command_name}`"
-
-    embed.set_footer(text="For more information, contact the bot owner.")
-    await ctx.send(embed=embed)
+    signature_with_params = f"{signature} {usage}"
+    return f"```\n{signature_with_params}\n```"
 
 
 
