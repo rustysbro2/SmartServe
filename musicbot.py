2ce48dd150cf6a3e3ae144249dae1e7c99c5f198
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-20 01:31:30 -0400

    Update musicbot.py

--- musicbot.py
+++ musicbot.py
@@ -7,6 +7,13 @@ class MusicBot(commands.Cog):
         self.bot = bot
         self.voice_client = None
 
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
+        if self.voice_client is not None and self.voice_client.is_connected() and len(self.voice_client.channel.members) == 1:
+            # Only bot remains in the voice channel
+            await self.voice_client.disconnect()
+            self.voice_client = None
+
     @commands.command()
     async def play(self, ctx, url):
         if ctx.author.voice is None:
@@ -29,12 +36,6 @@ class MusicBot(commands.Cog):
             await ctx.send("Now playing: " + video.title)
         except pytube.exceptions.PytubeError:
             await ctx.send("Failed to load the video. Please provide a valid YouTube URL.")
-
-    @commands.command()
-    async def leave(self, ctx):
-        if self.voice_client is not None:
-            await self.voice_client.disconnect()
-            self.voice_client = None
 
     @commands.command()
     async def pause(self, ctx):
