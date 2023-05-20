12b7329dc44503f8f054d6394288daea57bae3ec
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-20 01:53:22 -0400

    Update giveaway.py

--- giveaway.py
+++ giveaway.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 import random
+import re
+import datetime
 
 class Giveaway(commands.Cog):
     def __init__(self, bot):
@@ -8,20 +10,24 @@ class Giveaway(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(administrator=True)
-    async def giveaway(self, ctx, duration: int, winners: int, *, prize: str):
+    async def giveaway(self, ctx, duration: str, winners: int, *, prize: str):
         print("Giveaway command invoked")
-        print("Giveaway command invoked")
-        print(f"Duration: {duration} minutes")
+        print(f"Duration: {duration}")
         print(f"Winners: {winners}")
         print(f"Prize: {prize}")
 
-        giveaway_message = await ctx.send(f"🎉 **GIVEAWAY** 🎉\n\nPrize: {prize}\nDuration: {duration} minutes\nWinners: {winners}\n\nReact with 🎉 to enter!")
+        duration_seconds = parse_duration(duration)
+        if duration_seconds is None:
+            await ctx.send("Invalid duration format. Please use a valid format such as '10d' or '3h'.")
+            return
+
+        giveaway_message = await ctx.send(f"🎉 **GIVEAWAY** 🎉\n\nPrize: {prize}\nDuration: {duration}\nWinners: {winners}\n\nReact with 🎉 to enter!")
         print("Giveaway message sent")
 
         await giveaway_message.add_reaction("🎉")
         print("Giveaway reaction added")
 
-        await discord.utils.sleep_until(giveaway_message.created_at + discord.timedelta(minutes=duration))
+        await discord.utils.sleep_until(giveaway_message.created_at + datetime.timedelta(seconds=duration_seconds))
         print("Giveaway duration elapsed")
 
         giveaway_message = await ctx.channel.fetch_message(giveaway_message.id)
@@ -43,6 +49,28 @@ class Giveaway(commands.Cog):
         winner_mentions = " ".join([winner.mention for winner in giveaway_winners])
         await ctx.send(f"🎉 **GIVEAWAY WINNERS** 🎉\n\nPrize: {prize}\nWinners: {winner_mentions}")
         print("Giveaway winners announced")
+
+def parse_duration(duration):
+    duration_regex = r"(\d+)([smhdw])"
+    match = re.findall(duration_regex, duration)
+    if not match:
+        return None
+
+    seconds = 0
+    for value, unit in match:
+        value = int(value)
+        if unit == 's':
+            seconds += value
+        elif unit == 'm':
+            seconds += value * 60
+        elif unit == 'h':
+            seconds += value * 3600
+        elif unit == 'd':
+            seconds += value * 86400
+        elif unit == 'w':
+            seconds += value * 604800
+
+    return seconds
 
 def setup(bot):
     bot.add_cog(Giveaway(bot))
