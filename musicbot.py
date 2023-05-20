8d756c719cbb4ec3a52c67a9c589970154401d97
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-20 15:17:30 -0400

    Update musicbot.py

--- musicbot.py
+++ musicbot.py
@@ -59,65 +59,65 @@ class MusicBot(commands.Cog):
             print(e)
 
     async def play_queue(self, voice_channel):
-    queue = self.voice_queues[voice_channel]
-    if queue.empty():
-        return
+        queue = self.voice_queues[voice_channel]
+        if queue.empty():
+            return
 
-    filename, requester, text_channel = await queue.get()
-    voice_client = voice_channel.guild.voice_client
-    try:
-        voice_client.play(discord.FFmpegPCMAudio(filename), after=lambda e: self.bot.loop.create_task(self.check_queue(voice_channel, text_channel)))
+        filename, requester, text_channel = await queue.get()
+        voice_client = voice_channel.guild.voice_client
+        try:
+            voice_client.play(discord.FFmpegPCMAudio(filename), after=lambda e: self.bot.loop.create_task(self.check_queue(voice_channel, text_channel)))
 
-        queue_size = queue.qsize() + 1  # Add 1 for the current song
-        await text_channel.send(f"Now playing: {filename} (requested by {requester})\nQueue size: {queue_size} song(s)")
+            queue_size = queue.qsize() + 1  # Add 1 for the current song
+            await text_channel.send(f"Now playing: {filename} (requested by {requester})\nQueue size: {queue_size} song(s)")
 
-        while voice_client.is_playing() or voice_client.is_paused():
-            if voice_channel in self.vote_skip and len(self.vote_skip[voice_channel]) >= (len(voice_channel.members) - 1) // 2 + 1:
-                voice_client.stop()
-                break
+            while voice_client.is_playing() or voice_client.is_paused():
+                if voice_channel in self.vote_skip and len(self.vote_skip[voice_channel]) >= (len(voice_channel.members) - 1) // 2 + 1:
+                    voice_client.stop()
+                    break
 
-            await asyncio.sleep(1)  # Check vote skip status every second
+                await asyncio.sleep(1)  # Check vote skip status every second
 
-        await self.check_queue(voice_channel, text_channel)  # Play the next song in the queue
+            await self.check_queue(voice_channel, text_channel)  # Play the next song in the queue
 
-    except Exception as e:
-        print(e)
+        except Exception as e:
+            print(e)
 
-async def check_queue(self, voice_channel, text_channel):
-    queue = self.voice_queues[voice_channel]
-    if queue.empty():
-        await self.leave(voice_channel.guild.voice_client)
-        return
+    async def check_queue(self, voice_channel, text_channel):
+        queue = self.voice_queues[voice_channel]
+        if queue.empty():
+            await self.leave(voice_channel.guild.voice_client)
+            return
 
-    await self.play_queue(voice_channel)
+        await self.play_queue(voice_channel)
 
-@commands.slash_command(name="vote_skip", description="Votes to skip the current song.")
-async def vote_skip(self, interaction: discord.SlashCommandInteraction):
-    if not interaction.voice_client or interaction.voice_client.channel not in self.voice_queues:
-        await interaction.response.send("No song is currently playing.")
-        return
+    @commands.slash_command(name="vote_skip", description="Votes to skip the current song.")
+    async def vote_skip(self, interaction: discord.SlashCommandInteraction):
+        if not interaction.voice_client or interaction.voice_client.channel not in self.voice_queues:
+            await interaction.response.send("No song is currently playing.")
+            return
 
-    voice_channel = interaction.voice_client.channel
-    member = interaction.author
-    vote_skip_set = self.vote_skip[voice_channel]
+        voice_channel = interaction.voice_client.channel
+        member = interaction.author
+        vote_skip_set = self.vote_skip[voice_channel]
 
-    # Calculate votes needed (excluding bot users)
-    votes_needed = (len(voice_channel.members) - 1) // 2 + 1
+        # Calculate votes needed (excluding bot users)
+        votes_needed = (len(voice_channel.members) - 1) // 2 + 1
 
-    if member == self.bot.user:
-        await interaction.response.send("Bots cannot vote to skip.")
-    elif member in vote_skip_set:
-        await interaction.response.send("You have already voted to skip.")
-    else:
-        vote_skip_set.add(member)
-        await interaction.response.send(f"{member.mention} has voted to skip the current song.")
-
-        if len(vote_skip_set) >= votes_needed:
-            await interaction.response.send("Vote skip successful. Skipping current song.")
-            ctx.voice_client.stop()
+        if member == self.bot.user:
+            await interaction.response.send("Bots cannot vote to skip.")
+        elif member in vote_skip_set:
+            await interaction.response.send("You have already voted to skip.")
         else:
-            votes_remaining = votes_needed - len(vote_skip_set)
-            await interaction.response.send(f"{votes_remaining} more vote(s) needed to skip the current song.")
+            vote_skip_set.add(member)
+            await interaction.response.send(f"{member.mention} has voted to skip the current song.")
+
+            if len(vote_skip_set) >= votes_needed:
+                await interaction.response.send("Vote skip successful. Skipping current song.")
+                ctx.voice_client.stop()
+            else:
+                votes_remaining = votes_needed - len(vote_skip_set)
+                await interaction.response.send(f"{votes_remaining} more vote(s) needed to skip the current song.")
 
 def setup(bot):
     bot.add_cog(MusicBot(bot))
