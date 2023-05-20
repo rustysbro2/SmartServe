b851858f8ce817775be567fade4fbe4e62781f32
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-20 13:37:25 -0400

    Update musicbot.py

--- musicbot.py
+++ musicbot.py
@@ -3,13 +3,12 @@ from discord.ext import commands
 import yt_dlp
 import os
 import asyncio
-from collections import deque
 
 class MusicBot(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.queue = deque()
-        self.is_playing = False
+        self.voice_queues = {}
+        self.vote_skip = {}
 
     @commands.command()
     async def join(self, ctx):
@@ -23,46 +22,78 @@ class MusicBot(commands.Cog):
     async def leave(self, ctx):
         if ctx.voice_client:
             await ctx.voice_client.disconnect()
+            if ctx.voice_client.channel in self.voice_queues:
+                del self.voice_queues[ctx.voice_client.channel]
+                del self.vote_skip[ctx.voice_client.channel]
 
     @commands.command()
     async def play(self, ctx, url):
         if not ctx.voice_client:
-            await ctx.invoke(self.bot.get_command("join"))
+            await self.join(ctx)
         
         if not ctx.voice_client:
             return
 
+        if ctx.voice_client.channel not in self.voice_queues:
+            self.voice_queues[ctx.voice_client.channel] = asyncio.Queue()
+            self.vote_skip[ctx.voice_client.channel] = set()
+
+        queue = self.voice_queues[ctx.voice_client.channel]
         ydl_opts = {'format': 'bestaudio/best', 'outtmpl': 'downloads/%(id)s.%(ext)s'}
         try:
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 info = ydl.extract_info(url, download=True)
                 filename = ydl.prepare_filename(info)
-                self.queue.append((filename, ctx.author))
-                await self.play_queue(ctx)
+                await queue.put((filename, ctx.author))
+                if queue.qsize() == 1:
+                    await self.play_queue(ctx.voice_client.channel)
                 await ctx.send(f"Added to the queue: {info['title']}")
         except Exception as e:
             print(e)
 
-    async def play_queue(self, ctx):
-        if not self.is_playing and self.queue:
-            self.is_playing = True
-            filename, requester = self.queue.popleft()
-            voice_channel = ctx.voice_client
-            try:
-                voice_channel.play(discord.FFmpegPCMAudio(filename), after=lambda e: self.check_queue(ctx))
-                await ctx.send(f"Now playing: {filename} (requested by {requester})")
-            except Exception as e:
-                print(e)
-                self.is_playing = False
+    async def play_queue(self, voice_channel):
+        queue = self.voice_queues[voice_channel]
+        if queue.empty():
+            return
 
-    def check_queue(self, ctx):
-        if not self.queue:
-            self.is_playing = False
-            asyncio.run_coroutine_threadsafe(ctx.send("Queue is empty. Leaving voice channel."), self.bot.loop)
+        filename, requester = await queue.get()
+        voice_client = voice_channel.guild.voice_client
+        try:
+            voice_client.play(discord.FFmpegPCMAudio(filename), after=lambda e: self.bot.loop.create_task(self.check_queue(voice_channel)))
+            await voice_channel.send(f"Now playing: {filename} (requested by {requester})")
+        except Exception as e:
+            print(e)
+
+    async def check_queue(self, voice_channel):
+        queue = self.voice_queues[voice_channel]
+        if queue.empty():
+            del self.voice_queues[voice_channel]
+            del self.vote_skip[voice_channel]
+            await voice_channel.send("Queue is empty. Leaving voice channel.")
+            await self.leave(voice_channel)
         else:
-            self.play_queue(ctx)
-        
-    # Delete the file after it finishes playing
+            await self.play_queue(voice_channel)
+
+    @commands.command()
+    async def vote_skip(self, ctx):
+        if not ctx.voice_client or ctx.voice_client.channel not in self.voice_queues:
+            await ctx.send("No song is currently playing.")
+            return
+
+        voice_channel = ctx.voice_client.channel
+        member = ctx.author
+        vote_skip_set = self.vote_skip[voice_channel]
+        vote_skip_set.add(member)
+
+        members_in_vc = len(voice_channel.members)
+        votes_needed = members_in_vc // 2 + 1
+
+        if len(vote_skip_set) >= votes_needed:
+            await ctx.send("Vote skip successful. Skipping current song.")
+            voice_channel.stop()
+        else:
+            await ctx.send(f"{member.mention} has voted to skip the current song. {votes_needed - len(vote_skip_set)} more vote(s) needed.")
+
     def delete_file(self, filename):
         try:
             os.remove(filename)
