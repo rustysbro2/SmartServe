16df5ca969dcb732d58bc6d6c027c5ed535055ef
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-21 09:37:27 -0400

    Update tracking.py

--- tracking.py
+++ tracking.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord_slash import cog_ext, SlashContext
 import json
 import random
+import interactions
 
 class Tracking(commands.Cog):
     def __init__(self, bot):
