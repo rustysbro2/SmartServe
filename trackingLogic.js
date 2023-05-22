adfbe514da7ed82f0ec51844a0b8d8a559289c90
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-22 13:20:40 -0400

    Update trackingLogic.js

--- trackingLogic.js
+++ trackingLogic.js
@@ -47,24 +47,32 @@ async function trackUserJoin(guildId, member) {
       if (trackingChannelId) {
         const trackingChannel = member.guild.channels.cache.get(trackingChannelId);
         if (trackingChannel && trackingChannel.isText()) {
-          console.log(`Sending message to tracking channel: ${trackingChannel.name}`);
-          try {
-            await trackingChannel.send(`User ${member.user.tag} joined using invite code ${usedInvite.code}`);
-            console.log(`Message sent successfully.`);
-          } catch (error) {
-            console.error(`Error sending message:`, error);
-          }
-        } else {
-          console.error(`Invalid tracking channel:`, trackingChannel);
+          trackingChannel.send(`User ${member.user.tag} joined using invite code ${usedInvite.code}`);
         }
-      } else {
-        console.error(`Tracking channel ID not set for guild ${guildId}`);
       }
-    } else {
-      console.error(`No used invite found for member ${member.user.tag}`);
     }
   }
 
   trackingData[guildId] = guildData;
   saveTrackingData(trackingData);
 }
+
+function setTrackingChannel(guildId, channelId) {
+  const trackingData = loadTrackingData();
+  let guildData = trackingData[guildId];
+
+  if (!guildData) {
+    guildData = {
+      inviteMap: {},
+      trackingChannelId: null,
+    };
+  }
+
+  guildData.trackingChannelId = channelId;
+  saveTrackingData(trackingData);
+}
+
+module.exports = {
+  trackUserJoin,
+  setTrackingChannel,
+};
