bc9325e98ca62beac231fe2ea08ea84e7bee9a55
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-22 13:18:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { token, clientId } = require('./config');
-const { trackUserJoin } = require('./trackingLogic');
+const { trackUserJoin, setTrackingChannel } = require('./trackingLogic');
 
 const client = new Client({
   intents: [
