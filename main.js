066d3698defd37c1d983bb992c8976001e63684b
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-22 12:29:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,15 +5,20 @@ const { Routes } = require('discord-api-types/v9');
 const { token, clientId } = require('./config');
 const { trackUserJoin } = require('./trackingLogic');
 
-const client = new Client({ 
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_PRESENCES]
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_INVITES,
+    Intents.FLAGS.GUILD_PRESENCES,
+  ],
 });
 
 // Create a collection to store the commands
 client.commands = new Collection();
 
 // Read the command files from the 'commands' folder
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
 
 // Load each command dynamically and add it to the collection
 for (const file of commandFiles) {
@@ -42,7 +47,7 @@ client.once('ready', async () => {
     console.log('Started refreshing application (/) commands.');
 
     // Convert the command collection to an array of command data
-    const commands = client.commands.map(command => command.data.toJSON());
+    const commands = client.commands.map((command) => command.data.toJSON());
 
     // Register the commands globally
     await rest.put(Routes.applicationCommands(clientId), { body: commands });
@@ -54,13 +59,13 @@ client.once('ready', async () => {
 });
 
 // Event triggered when a user joins a guild
-client.on('guildMemberAdd', async member => {
+client.on('guildMemberAdd', async (member) => {
   console.log('Tracking user join:', member.user.tag);
-  await trackUserJoin(member.guild.id, member);
+  await trackUserJoin(member);
 });
 
 // Event triggered when an interaction (slash command) is created
-client.on('interactionCreate', async interaction => {
+client.on('interactionCreate', async (interaction) => {
   if (!interaction.isCommand()) return;
 
   const commandName = interaction.commandName;
