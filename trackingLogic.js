5d056ac4c339970b7f29ac8a72d148654321d85a
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-22 01:06:48 -0400

    Update trackingLogic.js

--- trackingLogic.js
+++ trackingLogic.js
@@ -1,3 +1,23 @@
+const fs = require('fs');
+
+function loadTrackingData() {
+  try {
+    const data = fs.readFileSync('trackingData.json', 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      fs.writeFileSync('trackingData.json', '{}', 'utf8');
+      return {};
+    }
+    throw error;
+  }
+}
+
+async function saveTrackingData(data) {
+  const jsonData = JSON.stringify(data, null, 2);
+  fs.writeFileSync('trackingData.json', jsonData, { encoding: 'utf8', flag: 'w' });
+}
+
 async function trackUserJoin(guildId, member) {
   console.log(`Tracking user join: ${member.user.tag}`);
 
@@ -76,3 +96,28 @@ async function trackUserJoin(guildId, member) {
   trackingData[guildId] = guildData;
   await saveTrackingData(trackingData);
 }
+
+function setTrackingChannel(guildId, channelId) {
+  const trackingData = loadTrackingData();
+  let guildData = trackingData[guildId];
+
+  if (!guildData) {
+    guildData = {
+      inviteMap: {},
+      trackingChannelId: null,
+    };
+    trackingData[guildId] = guildData;
+  }
+
+  if (guildData.trackingChannelId === channelId) {
+    return;
+  }
+
+  guildData.trackingChannelId = channelId;
+  saveTrackingData(trackingData);
+}
+
+module.exports = {
+  trackUserJoin,
+  setTrackingChannel
+};
