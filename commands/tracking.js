cba3736d139d8bf3d57774ee3cc558692c5f0cc2
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-22 10:43:18 -0400

    Update tracking.js

--- commands/tracking.js
+++ commands/tracking.js
@@ -3,7 +3,7 @@ const { setTrackingChannel } = require('../trackingLogic');
 
 const trackCommand = {
   data: new SlashCommandBuilder()
-    .setName('track')  // Changed the command name here
+    .setName('track')
     .setDescription('Set the tracking channel')
     .addChannelOption(option =>
       option.setName('channel')
@@ -13,12 +13,12 @@ const trackCommand = {
   execute: async (interaction) => {
     const channel = interaction.options.getChannel('channel');
     if (channel) {
-      setTrackingChannel(interaction.guildId, channel.id);
+      setTrackingChannel(interaction.guild.id, channel.id);
       await interaction.reply(`Tracking channel set to <#${channel.id}>.`);
     } else {
       await interaction.reply('Please provide a valid channel.');
     }
-  }
+  },
 };
 
 module.exports = trackCommand;
