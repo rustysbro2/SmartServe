02e4b910a97bc272fa3c159db87fbb0e18c6f9d3
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-22 01:02:13 -0400

    Update trackingLogic.js

--- trackingLogic.js
+++ trackingLogic.js
@@ -1,23 +1,3 @@
-const fs = require('fs');
-
-function loadTrackingData() {
-  try {
-    const data = fs.readFileSync('trackingData.json', 'utf8');
-    return JSON.parse(data);
-  } catch (error) {
-    if (error.code === 'ENOENT') {
-      fs.writeFileSync('trackingData.json', '{}', 'utf8');
-      return {};
-    }
-    throw error;
-  }
-}
-
-async function saveTrackingData(data) {
-  const jsonData = JSON.stringify(data, null, 2);
-  fs.writeFileSync('trackingData.json', jsonData, { encoding: 'utf8', flag: 'w' });
-}
-
 async function trackUserJoin(guildId, member) {
   console.log(`Tracking user join: ${member.user.tag}`);
 
@@ -63,10 +43,22 @@ async function trackUserJoin(guildId, member) {
           const inviter = await member.guild.members.fetch(usedInvite.inviter.id);
           if (inviter) {
             console.log(`Sending message in tracking channel: User ${member.user.tag} joined the server. Invited by ${inviter.user.tag}`);
-            trackingChannel.send(`User ${member.user.tag} joined the server. Invited by ${inviter.user.tag}`);
+            trackingChannel.send(`User ${member.user.tag} joined the server. Invited by ${inviter.user.tag}`)
+              .then(() => {
+                console.log('Message sent successfully.');
+              })
+              .catch((error) => {
+                console.error('Error sending message:', error);
+              });
           } else {
             console.log(`Sending message in tracking channel: User ${member.user.tag} joined the server.`);
-            trackingChannel.send(`User ${member.user.tag} joined the server.`);
+            trackingChannel.send(`User ${member.user.tag} joined the server.`)
+              .then(() => {
+                console.log('Message sent successfully.');
+              })
+              .catch((error) => {
+                console.error('Error sending message:', error);
+              });
           }
         } else {
           console.log('Tracking channel does not exist or is not text-based.');
@@ -84,28 +76,3 @@ async function trackUserJoin(guildId, member) {
   trackingData[guildId] = guildData;
   await saveTrackingData(trackingData);
 }
-
-function setTrackingChannel(guildId, channelId) {
-  const trackingData = loadTrackingData();
-  let guildData = trackingData[guildId];
-
-  if (!guildData) {
-    guildData = {
-      inviteMap: {},
-      trackingChannelId: null,
-    };
-    trackingData[guildId] = guildData;
-  }
-
-  if (guildData.trackingChannelId === channelId) {
-    return;
-  }
-
-  guildData.trackingChannelId = channelId;
-  saveTrackingData(trackingData);
-}
-
-module.exports = {
-  trackUserJoin,
-  setTrackingChannel
-};
