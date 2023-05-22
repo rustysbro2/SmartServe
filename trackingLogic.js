4d75d79c729e619c6e9c4088e724fc36ff55118f
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-22 14:53:16 -0400

    Update trackingLogic.js

--- trackingLogic.js
+++ trackingLogic.js
@@ -1,73 +1,135 @@
+const fs = require('fs');
 const mysql = require('mysql');
 
-const pool = mysql.createPool({
+const connectionConfig = {
   host: 'localhost',
   user: 'Rustysbro',
   password: '1234',
-  database: 'counting',
-});
+  database: 'counting'
+};
 
-function trackUserJoin(guildId, member) {
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.error('Error getting MySQL connection:', err);
-      return;
-    }
+function loadTrackingData() {
+  const connection = mysql.createConnection(connectionConfig);
 
-    const invitesQuery = 'SELECT * FROM invites WHERE guildId = ?';
-    connection.query(invitesQuery, [guildId], (err, inviteResults) => {
+  return new Promise((resolve, reject) => {
+    connection.connect((err) => {
       if (err) {
-        console.error('Error querying invites:', err);
-        connection.release();
+        console.error('Error connecting to MySQL:', err);
+        reject(err);
         return;
       }
 
-      const invites = {};
-      for (const inviteResult of inviteResults) {
-        invites[inviteResult.code] = {
-          uses: inviteResult.uses,
-          inviter: inviteResult.inviterId,
-        };
+      const query = 'SELECT * FROM tracking_data';
+      connection.query(query, (error, results) => {
+        if (error) {
+          console.error('Error loading tracking data:', error);
+          reject(error);
+        } else {
+          const trackingData = {};
+          for (const row of results) {
+            trackingData[row.guild_id] = {
+              inviteMap: JSON.parse(row.invite_map),
+              trackingChannelId: row.tracking_channel_id
+            };
+          }
+          console.log('Tracking data loaded successfully');
+          resolve(trackingData);
+        }
+        connection.end();
+      });
+    });
+  });
+}
+
+function saveTrackingData(data) {
+  const connection = mysql.createConnection(connectionConfig);
+
+  connection.connect((err) => {
+    if (err) {
+      console.error('Error connecting to MySQL:', err);
+      return;
+    }
+
+    const values = Object.entries(data).map(([guildId, { inviteMap, trackingChannelId }]) => {
+      return `('${guildId}', '${JSON.stringify(inviteMap)}', '${trackingChannelId}')`;
+    });
+
+    const query = `INSERT INTO tracking_data (guild_id, invite_map, tracking_channel_id) VALUES ${values} ON DUPLICATE KEY UPDATE invite_map = VALUES(invite_map), tracking_channel_id = VALUES(tracking_channel_id)`;
+
+    connection.query(query, (error, results) => {
+      if (error) {
+        console.error('Error saving tracking data:', error);
+      } else {
+        console.log('Tracking data saved successfully');
       }
+      connection.end();
+    });
+  });
+}
 
-      const usedInvite = member.guild.invites.cache.find(
-        (invite) => invites[invite.code] && invites[invite.code].uses < invite.uses
-      );
+async function trackUserJoin(guildId, member) {
+  const trackingData = await loadTrackingData();
+  let guildData = trackingData[guildId];
 
-      if (usedInvite) {
-        const trackingChannelId = guildData.trackingChannelId;
+  if (!guildData) {
+    guildData = {
+      inviteMap: {},
+      trackingChannelId: null
+    };
+  }
+
+  if (member instanceof GuildMember) {
+    const invites = await member.guild.invites.fetch();
+
+    const usedInvite = invites.find((invite) => {
+      const inviteData = guildData.inviteMap[invite.code];
+      return inviteData && inviteData.uses < invite.uses;
+    });
+
+    if (usedInvite) {
+      guildData.inviteMap[usedInvite.code] = {
+        uses: usedInvite.uses,
+        inviter: member.id
+      };
+
+      const trackingChannelId = guildData.trackingChannelId;
+      if (trackingChannelId) {
         const trackingChannel = member.guild.channels.cache.get(trackingChannelId);
         if (trackingChannel && trackingChannel.isText()) {
           trackingChannel.send(`User ${member.user.tag} joined using invite code ${usedInvite.code}`);
         }
       }
+    }
+  }
 
-      connection.release();
-    });
-  });
+  trackingData[guildId] = guildData;
+  saveTrackingData(trackingData);
 }
 
 function setTrackingChannel(guildId, channelId) {
-  pool.getConnection((err, connection) => {
+  const connection = mysql.createConnection(connectionConfig);
+
+  connection.connect((err) => {
     if (err) {
-      console.error('Error getting MySQL connection:', err);
+      console.error('Error connecting to MySQL:', err);
       return;
     }
 
-    const updateQuery = 'UPDATE guilds SET trackingChannelId = ? WHERE guildId = ?';
-    connection.query(updateQuery, [channelId, guildId], (err) => {
-      if (err) {
-        console.error('Error updating tracking channel:', err);
-        connection.release();
-        return;
+    const query = `UPDATE tracking_data SET tracking_channel_id = '${channelId}' WHERE guild_id = '${guildId}'`;
+    connection.query(query, (error, results) => {
+      if (error) {
+        console.error('Error updating tracking channel:', error);
+      } else {
+        console.log('Tracking channel updated successfully');
       }
-
-      connection.release();
+      connection.end();
     });
   });
 }
 
 module.exports = {
+  loadTrackingData,
+  saveTrackingData,
   trackUserJoin,
-  setTrackingChannel,
+  setTrackingChannel
 };
