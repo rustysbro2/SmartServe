5c1cfe74bcbc475dcd19e6acf5f0216c988bda78
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-21 23:06:52 -0400

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+const helpCommand = {
+  data: new SlashCommandBuilder()
+    .setName('help')
+    .setDescription('Show available commands'),
+  execute: async (interaction) => {
+    const { commands } = interaction.client;
+
+    const helpMessage = commands.map(command => {
+      const commandName = command.name;
+      const commandDescription = command.description;
+      const commandOptions = command.options.map(option => option.name).join(', ');
+      const usage = `/${commandName} ${commandOptions}`;
+
+      return `**${commandName}**: ${commandDescription}\nUsage: \`${usage}\``;
+    }).join('\n\n');
+
+    interaction.reply(`**Available Commands**:\n\n${helpMessage}`);
+  }
+};
+
+module.exports = {
+  helpCommand
+};
