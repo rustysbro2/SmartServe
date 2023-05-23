d3f4391de60350b22077ff90d91ab92ed1fe0627
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-22 21:32:13 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,13 @@
+// bot.js
+const { Client } = require('discord.js');
+const { token } = require('./config.js');
+
+const client = new Client({ shards: "auto" });
+
+client.on('ready', () => {
+    console.log(`Shard ${client.shard.ids} logged in as ${client.user.tag}!`);
+});
+
+// Add your event handlers and other code here
+
+client.login(token);
