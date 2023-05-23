279b67bc51a44eba3701d2dc8f595f214ec073ef
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-23 19:23:06 -0400

    Update musicPlayer.js

--- features/musicPlayer.js
+++ features/musicPlayer.js
@@ -1,10 +1,6 @@
-const { createAudioPlayer, createAudioResource, joinVoiceChannel, StreamType, AudioPlayerStatus } = require('@discordjs/voice');
-const ytdl = require('discord-ytdl-core');
-const fs = require('fs');
-const path = require('path');
-const util = require('util');
-const stream = require('stream');
-const pipeline = util.promisify(stream.pipeline);
+const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
+const ytdl = require('ytdl-core');
+const { MessageEmbed } = require('discord.js');
 
 class MusicPlayer {
     constructor(client, guildId) {
@@ -13,9 +9,9 @@ class MusicPlayer {
         this.queue = [];
         this.player = createAudioPlayer();
         this.connection = null;
-        this.player.on(AudioPlayerStatus.Idle, () => {
+        this.player.on('idle', () => {
             if(this.queue.length > 0) {
-                this.playSong(this.queue.shift());
+                this.play(this.queue.shift());
             }
         });
     }
@@ -31,38 +27,10 @@ class MusicPlayer {
     }
 
     async play(url) {
-        this.queue.push(url);
-        if (this.player.state.status === AudioPlayerStatus.Idle) {
-            this.playSong(this.queue.shift());
-        }
-    }
-
-async playSong(url) {
-    try {
-        const info = await ytdl.getBasicInfo(url);
-        const formats = info.formats.filter((format) => format.audioBitrate);
-        const bestFormat = formats.sort((a, b) => b.audioBitrate - a.audioBitrate)[0];
-
-        const stream = ytdl(url, { format: bestFormat });
-        const filename = path.join(__dirname, `${Date.now()}.opus`);
-        await pipeline(stream, fs.createWriteStream(filename));
-
-        const resource = createAudioResource(filename, { inputType: StreamType.OggOpus });
+        const stream = ytdl(url, { filter: 'audioonly' });
+        const resource = createAudioResource(stream);
         this.player.play(resource);
-
-        this.player.once(AudioPlayerStatus.Idle, () => {
-            fs.unlink(filename, (err) => {
-                if (err) {
-                    console.error('Failed to delete file:', filename);
-                }
-            });
-        });
-    } catch (err) {
-        console.error('Failed to play song:', err);
-        // You might want to retry the operation, handle the error in some other way, or ignore it.
     }
-}
-
 
     enqueue(url) {
         this.queue.push(url);
