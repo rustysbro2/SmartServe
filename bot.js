5053d22b0063c3d90eb302ae57d0dfc46faf4231
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-23 11:13:12 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,27 +1,33 @@
 // bot.js
-const { Client, Intents, Collection } = require('discord.js');
-const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.js');
+const fs = require('fs');
 
 // List intents that the bot needs access to
 const intents = new Intents([
-    Intents.FLAGS.GUILDS, 
+    Intents.FLAGS.GUILDS,
     Intents.FLAGS.GUILD_MESSAGES,
-    // Add other intents here based on the needs of your bot
 ]);
 
 const client = new Client({ shards: "auto", intents });
-client.commands = new Collection();
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+// Create a new Collection for commands
+client.commands = new Collection();
+
+// Dynamically retrieve commands
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.data.name, command);
 }
 
-client.once('ready', () => {
+client.once('ready', async () => {
     console.log(`Shard ${client.shard.ids} logged in as ${client.user.tag}!`);
-    client.user.setActivity(`${client.guilds.cache.size} servers | Shard: ${client.shard.ids}`, { type: 'WATCHING' });
+    client.user.setActivity(`Watching ${client.guilds.cache.size} servers | Shard ${client.shard.ids[0]}`, { type: 'WATCHING' });
+
+    // Start the command registration process after bot is ready
+    const slashCommands = require('./slashCommands.js');
+    await slashCommands(client);
 });
 
 client.on('interactionCreate', async interaction => {
