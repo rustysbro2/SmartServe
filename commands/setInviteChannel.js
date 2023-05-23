261fa8b0bac2f3e36a10bc2a46ba5ac3fd431ed8
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-23 13:07:21 -0400

    Update setInviteChannel.js

--- commands/setInviteChannel.js
+++ commands/setInviteChannel.js
@@ -1,15 +1,11 @@
-// commands/setInviteChannel.js
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const inviteTracker = require('../features/inviteTracker.js');
-
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('setInviteChannel')
-        .setDescription('Set the channel where invite notifications should be sent.')
-        .addChannelOption(option => option.setName('channel').setDescription('The channel').setRequired(true)),
+        .setName('setinvitechannel')
+        .setDescription('Set the channel for invite tracking messages')
+        .addChannelOption(option => option.setName('channel').setDescription('The channel to send messages in').setRequired(true)),
     async execute(interaction) {
         const channel = interaction.options.getChannel('channel');
-        inviteTracker.setInviteChannel(interaction.guild.id, channel.id);
-        await interaction.reply(`Invite notifications will be sent in ${channel.name}.`);
+        inviteTracker.setInviteChannel(interaction.guildId, channel.id);
+        await interaction.reply({ content: `Invite tracking messages will be sent in ${channel.name}`, ephemeral: true });
     },
 };
