d9fc38af9bf646e6436419d29569724bef4b55f6
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-23 11:13:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 const { ShardingManager } = require('discord.js');
 const { token } = require('./config.js');
 
-
-
 const manager = new ShardingManager('./bot.js', { token: token });
 
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
