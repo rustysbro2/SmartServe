c9a7f656a2415eb5896658e851f7e835c7bc6352
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-23 16:46:43 -0400

    Update hello.js

--- commands/hello.js
+++ commands/hello.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-    data: new SlashCommandBuilder().setName('hello').setDescription('Replies with Hello!'),
+    data: new SlashCommandBuilder().setName('hellos').setDescription('Replies with Hello!'),
     async execute(interaction) {
         await interaction.reply('Hello!');
     },
