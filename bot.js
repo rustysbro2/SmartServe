44e1e79b2c979a00514e4dd0b0e904ca063f3adf
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-23 10:29:40 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 // bot.js
-const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.js');
 
 // List intents that the bot needs access to
@@ -10,6 +11,29 @@ const intents = new Intents([
 ]);
 
 const client = new Client({ shards: "auto", intents });
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+});
 
 client.on('ready', () => {
     console.log(`Shard ${client.shard.ids} logged in as ${client.user.tag}!`);
