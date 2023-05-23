5449fb0711f0503fe13c1159bcde63cbe046997c
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-23 15:47:30 -0400

    Update inviteTracker.js

--- features/inviteTracker.js
+++ features/inviteTracker.js
@@ -7,6 +7,20 @@ let invites = {};
 async function fetchInvites(guild) {
     const guildInvites = await guild.invites.fetch();
     invites[guild.id] = guildInvites;
+    guildInvites.forEach(invite => {
+        updateInviteInDb(guild.id, invite.code, invite.uses);
+    });
+}
+
+function updateInviteInDb(guildId, inviteCode, uses) {
+    db.query(`
+        INSERT INTO invites (guildId, inviteCode, uses)
+        VALUES (?, ?, ?)
+        ON DUPLICATE KEY UPDATE
+        uses = VALUES(uses)
+    `, [guildId, inviteCode, uses], function (error) {
+        if (error) throw error;
+    });
 }
 
 db.query(`
@@ -22,7 +36,6 @@ db.query(`
     CREATE TABLE IF NOT EXISTS invites (
         guildId VARCHAR(255),
         inviteCode VARCHAR(255),
-        inviterId VARCHAR(255),
         uses INT,
         PRIMARY KEY(guildId, inviteCode)
     )
@@ -44,39 +57,54 @@ module.exports = {
 
         client.on('guildMemberAdd', async member => {
             console.log(`New member added: ${member.user.tag}`);
-            
-            const embed = new MessageEmbed()
-                .setTitle("New Member Joined!")
-                .setColor("#32CD32");
-            
-            if (member.user.bot) {
-                embed.setDescription(`${member.user.tag} joined the server through OAuth.`);
-            } else {
-                const newInvite = invites[member.guild.id].find(invite => invite.uses !== dbInvites[invite.code]);
-                if (newInvite) {
-                    embed.setDescription(`${member.user.tag} was invited by ${newInvite.inviter.tag}.`);
-                } else {
-                    embed.setDescription(`${member.user.tag} joined, but we couldn't determine the invite used.`);
-                }
-            }
-            
+            let dbInvites;
             db.query(`
-                SELECT channelId
-                FROM inviteChannels
+                SELECT *
+                FROM invites
                 WHERE guildId = ?
             `, [member.guild.id], async function (error, results) {
                 if (error) throw error;
-                if (results.length > 0) {
-                    const channelId = results[0].channelId;
-                    const channel = member.guild.channels.cache.get(channelId);
-                    if (!channel) return;
-                    
-                    console.log(`Sending message to channel: ${channel.name}`);
-                    channel.send({ embeds: [embed] });
+                dbInvites = results.reduce((obj, row) => {
+                    obj[row.inviteCode] = row.uses;
+                    return obj;
+                }, {});
+
+                const newInvite = invites[member.guild.id].find(invite => invite.uses !== dbInvites[invite.code]);
+                if (!newInvite) {
+                    let msg;
+                    if (member.user.bot) {
+                        msg = `New member joined: ${member.user.tag}, but we couldn't determine the invite used. (likely OAuth)`;
+                    } else {
+                        msg = `New member joined: ${member.user.tag}, but we couldn't determine the invite used. (likely Vanity URL or other special invite)`;
+                    }
+                    console.log(msg);
+                    return;
                 }
+                console.log(`New member: ${member.user.tag} used invite: ${newInvite.code}`);
+                updateInviteInDb(member.guild.id, newInvite.code, newInvite.uses);
+
+                const inviter = client.users.cache.get(newInvite.inviter.id);
+                db.query(`
+                    SELECT channelId
+                    FROM inviteChannels
+                    WHERE guildId = ?
+                `, [member.guild.id], function (error, results) {
+                    if (error) throw error;
+                    if (results.length === 0) return;
+
+                    const channelId = results[0].channelId;
+                    const channel = client.channels.cache.get(channelId);
+                    if (!channel) return;
+                    const embed = new MessageEmbed()
+                        .setTitle('New Member Joined!')
+                        .setDescription(`${member.user.tag} joined using invite code ${newInvite.code} from ${inviter.tag}. Invite was used ${newInvite.uses} times.`)
+                        .setTimestamp()
+                        .setColor('GREEN');
+
+                    channel.send({ embeds: [embed] });
+                });
             });
         });
-
 
         client.on('inviteCreate', async invite => {
             console.log(`Invite created: ${invite.code}`);
@@ -84,24 +112,19 @@ module.exports = {
                 await fetchInvites(invite.guild);
             } else {
                 invites[invite.guild.id].set(invite.code, invite);
+                updateInviteInDb(invite.guild.id, invite.code, invite.uses);
             }
         });
-        
+
         client.on('inviteDelete', async invite => {
             console.log(`Invite deleted: ${invite.code}`);
-            const cachedInvites = invites[invite.guild.id];
-            cachedInvites.delete(invite.code);
+            invites[invite.guild.id].delete(invite.code);
+            db.query(`
+                DELETE FROM invites
+                WHERE guildId = ? AND inviteCode = ?
+            `, [invite.guild.id, invite.code], function (error) {
+                if (error) throw error;
+            });
         });
     },
-    setInviteChannel(guildId, channelId) {
-        console.log(`Setting invite channel: ${channelId} for guild: ${guildId}`);
-        db.query(`
-            INSERT INTO inviteChannels (guildId, channelId)
-            VALUES (?, ?)
-            ON DUPLICATE KEY UPDATE
-            channelId = VALUES(channelId)
-        `, [guildId, channelId], function (error) {
-            if (error) throw error;
-        });
-    }
 };
