3eb63a2f5bd400303864ddaa18eb3dec1c37f028
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-23 11:01:03 -0400

    Update slashCommands.js

--- slashCommands.js
+++ slashCommands.js
@@ -1,29 +1,22 @@
-const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, token } = require('./config.js');
+const { clientId, guildId, token } = require('./config.json');
 
-const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    commands.push(command.data.toJSON());
-}
+const commandsInCode = new Set(fs.readdirSync('./commands').map(file => file.slice(0, -3)));
 
 const rest = new REST({ version: '9' }).setToken(token);
 
 (async () => {
-    try {
-        console.log('Started refreshing application (/) commands.');
+    const registeredCommands = await rest.get(
+        Routes.applicationGuildCommands(clientId, guildId),
+    );
 
-        await rest.put(
-            Routes.applicationCommands(clientId),
-            { body: commands },
-        );
-
-        console.log('Successfully reloaded application (/) commands.');
-    } catch (error) {
-        console.error(error);
+    for (const command of registeredCommands) {
+        if (!commandsInCode.has(command.name)) {
+            console.log(`Deleting command: ${command.name}`);
+            await rest.delete(
+                Routes.applicationGuildCommand(clientId, guildId, command.id),
+            );
+        }
     }
 })();
