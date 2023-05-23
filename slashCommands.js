a98a4d8fa8f9b37d8c0bbfbc9bcb4eb0873c899b
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-23 11:16:16 -0400

    Update slashCommands.js

--- slashCommands.js
+++ slashCommands.js
@@ -1,10 +1,10 @@
 // slashCommands.js
-const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, token } = require('./config.js');
+const { clientId, token } = require('./config.json');
+const fs = require('fs');
 
-module.exports = async (client) => {
+module.exports = async function(client) {
     const commands = [];
     const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
@@ -15,28 +15,31 @@ module.exports = async (client) => {
 
     const rest = new REST({ version: '9' }).setToken(token);
 
-    // Loop through all guilds the bot is connected to
-    for (const guild of client.guilds.cache.values()) {
+    // Loop through each guild the bot is in and register the slash commands
+    client.guilds.cache.each(async (guild) => {
         try {
-            console.log(`Started refreshing application (/) commands in guild ${guild.id}`);
+            console.log(`Started refreshing application (/) commands for guild ${guild.id}.`);
 
-            const existingCommands = await rest.get(Routes.applicationGuildCommands(clientId, guild.id));
+            // Remove all slash commands in the guild
+            const existingCommands = await rest.get(
+                Routes.applicationGuildCommands(clientId, guild.id)
+            );
 
             for (const command of existingCommands) {
-                const commandExists = commands.find(cmd => cmd.name === command.name);
-                if (!commandExists) {
-                    await rest.delete(Routes.applicationGuildCommand(clientId, guild.id, command.id));
-                }
+                await rest.delete(
+                    Routes.applicationGuildCommand(clientId, guild.id, command.id)
+                );
             }
 
+            // Register the new slash commands
             await rest.put(
                 Routes.applicationGuildCommands(clientId, guild.id),
                 { body: commands },
             );
 
-            console.log(`Successfully reloaded application (/) commands in guild ${guild.id}`);
+            console.log(`Successfully reloaded application (/) commands for guild ${guild.id}.`);
         } catch (error) {
-            console.error(error);
+            console.error(`Error while refreshing application (/) commands for guild ${guild.id}.`, error);
         }
-    }
+    });
 };
