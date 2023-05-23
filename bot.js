7853b6f87618755b279e7cdaee18a8aabc7be5e6
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-23 10:14:30 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,15 @@
 // bot.js
-const { Client } = require('discord.js');
+const { Client, Intents } = require('discord.js');
 const { token } = require('./config.js');
 
-const client = new Client({ shards: "auto" });
+// List intents that the bot needs access to
+const intents = new Intents([
+    Intents.FLAGS.GUILDS, 
+    Intents.FLAGS.GUILD_MESSAGES,
+    // Add other intents here based on the needs of your bot
+]);
+
+const client = new Client({ shards: "auto", intents });
 
 client.on('ready', () => {
     console.log(`Shard ${client.shard.ids} logged in as ${client.user.tag}!`);
