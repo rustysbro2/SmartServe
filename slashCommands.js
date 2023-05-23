000ae2bf6759caefaa746a5bd6472544203f64c4
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-23 11:13:28 -0400

    Update slashCommands.js

--- slashCommands.js
+++ slashCommands.js
@@ -1,22 +1,42 @@
+// slashCommands.js
+const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, token } = require('./config.js');
-const fs = require('fs');
+const { clientId, token } = require('./config.json');
 
-const rest = new REST({ version: '9' }).setToken(token);
+module.exports = async (client) => {
+    const commands = [];
+    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-(async () => {
-    try {
-        const commands = await rest.get(Routes.applicationCommands(clientId));
-        const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-        const commandsInCode = commandFiles.map(file => require(`./commands/${file}`).data.name);
-
-        for (const command of commands) {
-            if (!commandsInCode.includes(command.name)) {
-                await rest.delete(Routes.applicationCommand(clientId, command.id));
-            }
-        }
-    } catch (error) {
-        console.error(error);
+    for (const file of commandFiles) {
+        const command = require(`./commands/${file}`);
+        commands.push(command.data.toJSON());
     }
-})();
+
+    const rest = new REST({ version: '9' }).setToken(token);
+
+    // Loop through all guilds the bot is connected to
+    for (const guild of client.guilds.cache.values()) {
+        try {
+            console.log(`Started refreshing application (/) commands in guild ${guild.id}`);
+
+            const existingCommands = await rest.get(Routes.applicationGuildCommands(clientId, guild.id));
+
+            for (const command of existingCommands) {
+                const commandExists = commands.find(cmd => cmd.name === command.name);
+                if (!commandExists) {
+                    await rest.delete(Routes.applicationGuildCommand(clientId, guild.id, command.id));
+                }
+            }
+
+            await rest.put(
+                Routes.applicationGuildCommands(clientId, guild.id),
+                { body: commands },
+            );
+
+            console.log(`Successfully reloaded application (/) commands in guild ${guild.id}`);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+};
