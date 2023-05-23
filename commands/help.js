f86ddef95c62d6063c0e77b227a7643f06846a5b
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-23 08:10:21 -0400

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,22 @@
+// commands/help.js
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('help')
+        .setDescription('List all commands or info about a specific command.'),
+    async execute(interaction) {
+        const commands = interaction.client.commands;
+        const helpEmbed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Help')
+            .setDescription('Here are the available commands:');
+        
+        commands.forEach((value, key) => {
+            helpEmbed.addField(key, value.data.description);
+        });
+
+        return interaction.reply({ embeds: [helpEmbed] });
+    },
+};
