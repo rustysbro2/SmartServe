f64c7964dc2608fd4b578a1033ed930e7d48c200
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-24 08:23:39 -0400

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,4 +1,3 @@
-// play.js
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { AudioPlayerStatus } = require('@discordjs/voice');
 const MusicPlayer = require('../features/musicPlayer');
@@ -10,20 +9,23 @@ module.exports = {
         .addStringOption(option => option.setName('url').setDescription('The URL of the song to play').setRequired(true)),
     async execute(interaction, client) {
         const url = interaction.options.getString('url');
-        const voiceChannel = interaction.member.voice.channelId;
-        if (!voiceChannel) {
-            return interaction.reply('You need to join a voice channel first!');
-        }
-        let musicPlayer = client.musicPlayers.get(voiceChannel);
+        let musicPlayer = client.musicPlayers.get(interaction.guildId);
         if (!musicPlayer) {
-            musicPlayer = new MusicPlayer(client, voiceChannel);
-            client.musicPlayers.set(voiceChannel, musicPlayer);
-            await musicPlayer.join(voiceChannel);
+            musicPlayer = new MusicPlayer(client, interaction.guildId);
+            client.musicPlayers.set(interaction.guildId, musicPlayer);
+        }
+        if (!musicPlayer.connection) {
+            const channel = interaction.member.voice.channelId;
+            if (!channel) {
+                return interaction.reply('You need to join a voice channel first!');
+            }
+            await musicPlayer.join(channel);
         }
         musicPlayer.enqueue(url);
         if (musicPlayer.player.state.status === AudioPlayerStatus.Idle) {
+            await interaction.channel.send(`Now playing: ${url}`);
             musicPlayer.play(musicPlayer.queue.shift());
         }
-        await interaction.reply(`Queued your song! Queue size: ${musicPlayer.queue.length}`);
+        await interaction.reply(`Queued your song! The queue now has ${musicPlayer.queue.length} song(s).`);
     },
 };
