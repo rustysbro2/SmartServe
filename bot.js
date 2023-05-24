8051a3e8e376aa1591dbabdad96bc3f8ca955ae7
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-24 15:16:56 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,29 +49,30 @@ client.on('interactionCreate', async (interaction) => {
   }
 });
 
-client.on('voiceStateUpdate', async (oldState, newState) => {
+client.on('voiceChannelUpdate', async (oldChannel, newChannel) => {
   const botId = client.user.id;
-  const guildId = oldState.guild.id;
+  const guildId = oldChannel.guild.id;
   const musicPlayer = client.musicPlayers.get(guildId);
 
   if (musicPlayer && musicPlayer.connection) {
-    const botInChannel = oldState.channelId && oldState.member.id === botId;
-    const botAlone = newState.channelId && newState.channel.members.size === 1 && newState.member.id === botId;
+    const botInChannel = oldChannel.members.has(botId);
+    const botAlone = newChannel.members.size === 1 && newChannel.members.has(botId);
 
     if (botInChannel && !botAlone) {
-      console.log(`Other users joined the voice channel: ${newState.channel.name}`);
-      console.log(`Channel Members: ${newState.channel.members.size}`);
+      console.log(`Other users joined the voice channel: ${newChannel.name}`);
+      console.log(`Channel Members: ${newChannel.members.size}`);
     }
 
     if (botInChannel && botAlone) {
-      console.log(`Bot is the only member in the voice channel: ${newState.channel.name}`);
-      console.log(`Channel Members: ${newState.channel.members.size}`);
+      console.log(`Bot is the only member in the voice channel: ${newChannel.name}`);
+      console.log(`Channel Members: ${newChannel.members.size}`);
       console.log("Destroying connection and leaving voice channel.");
       musicPlayer.connection.destroy();
       client.musicPlayers.delete(guildId);
     }
   }
 });
+
 
 
 
