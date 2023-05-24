caf60fff3cacc928545cec8e699dc430b52f6923
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-24 16:40:02 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,20 +3,26 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('help')
-        .setDescription('List all commands or info about a specific command.'),
-    async execute(interaction) {
-        const commands = interaction.client.commands;
-        const helpEmbed = new MessageEmbed()
-            .setColor('#0099ff')
-            .setTitle('Help')
-            .setDescription('Here are the available commands:');
-        
-        commands.forEach((value, key) => {
-            helpEmbed.addField(key, value.data.description);
-        });
+  data: new SlashCommandBuilder()
+    .setName('help')
+    .setDescription('List all commands or info about a specific command.'),
 
-        return interaction.reply({ embeds: [helpEmbed] });
-    },
+  async execute(interaction) {
+    const commands = interaction.client.commands;
+    
+    // Create the help embed
+    const helpEmbed = new MessageEmbed()
+      .setColor('#0099ff')
+      .setTitle('Help')
+      .setDescription('Here are the available commands:');
+    
+    // Add a field for each command
+    commands.forEach((command) => {
+      const commandData = command.data.toJSON();
+      helpEmbed.addField(commandData.name, commandData.description);
+    });
+    
+    // Reply with the help embed
+    await interaction.reply({ embeds: [helpEmbed] });
+  },
 };
