ee08043edb96d1346ba4b926ead89dfef1e52068
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-24 13:37:16 -0400

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,59 +1,62 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const MusicPlayer = require('../features/musicPlayer.js');
-const { AudioPlayerStatus } = require('@discordjs/voice');
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./config.js');
+const inviteTracker = require('./features/inviteTracker.js');
+const fs = require('fs');
 
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('play')
-    .setDescription('Play a song from YouTube')
-    .addStringOption(option =>
-      option
-        .setName('url')
-        .setDescription('The YouTube URL of the song to play')
-        .setRequired(true)),
-async execute(interaction, client) {
+const intents = new Intents([
+  Intents.FLAGS.GUILDS,
+  Intents.FLAGS.GUILD_MESSAGES,
+  Intents.FLAGS.GUILD_MEMBERS,
+  Intents.FLAGS.GUILD_VOICE_STATES,
+]);
+
+const client = new Client({ shards: "auto", intents });
+
+client.commands = new Collection();
+client.musicPlayers = new Map();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
+
+client.once('ready', async () => {
+  console.log(`Shard ${client.shard.ids} logged in as ${client.user.tag}!`);
+  client.user.setActivity(`${client.guilds.cache.size} servers | Shard ${client.shard.ids[0]}`, { type: 'WATCHING' });
+
+  inviteTracker.execute(client);
+
+  const slashCommands = require('./slashCommands.js');
+  await slashCommands(client);
+});
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
   try {
-    console.log('Executing /play command...');
-    const url = interaction.options.getString('url');
-    console.log('URL:', url);
-    const guildId = interaction.guildId;
-    console.log('Guild ID:', guildId);
-    const channelId = interaction.member.voice.channelId;
-    console.log('Channel ID:', channelId);
-    const textChannel = interaction.channel;
-
-    // The member who sent the command must be in a voice channel
-    if (!channelId) {
-      console.log('User not in a voice channel.');
-      return await interaction.reply('You must be in a voice channel to play music!');
-    }
-
-    // Create or retrieve the music player for this guild
-    let musicPlayer = client.musicPlayers.get(guildId);
-    if (!musicPlayer) {
-      console.log('Creating new MusicPlayer instance.');
-      musicPlayer = new MusicPlayer(guildId, channelId, textChannel);
-      client.musicPlayers.set(guildId, musicPlayer);
-      await musicPlayer.joinChannel();
-    }
-
-    const wasEmpty = musicPlayer.queue.length === 0; // Check if the queue was empty before adding the song
-    console.log('Queue was empty before adding song:', wasEmpty);
-    await musicPlayer.addSong(url);
-
-    if (wasEmpty && musicPlayer.queue.length === 1) {
-      console.log('Waiting for AudioPlayer to transition to "Playing" state...');
-      await entersState(musicPlayer.audioPlayer, AudioPlayerStatus.Playing, 5e3);
-      musicPlayer.sendNowPlaying();
-      console.log('Now playing message sent.');
-    }
-
-    // Notify the user
-    console.log('Sending reply message: Added to queue!');
-    await interaction.reply('Added to queue!');
+    await command.execute(interaction, client); // Execute the command
   } catch (error) {
-    console.error(`Error executing /play command: ${error.message}`);
-    await interaction.reply('There was an error while executing this command!');
+    console.error(error);
+    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
   }
-},
+});
 
+client.on('voiceStateUpdate', async (oldState, newState) => {
+  if (!oldState.channelId) return;
+  const botInOldChannel = oldState.channel.members.has(client.user.id);
+  if (!botInOldChannel) return;
+
+  if (oldState.channelId !== newState.channelId) {
+    const playCommand = client.commands.get('play');
+    if (playCommand) {
+      await playCommand.execute(oldState, client);
+    }
+  }
+});
+
+client.login(token);
