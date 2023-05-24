d4925344f6b7b8b0ea12b2620fa2e785cda01f36
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-24 10:36:44 -0400

    Update musicPlayer.js

--- features/musicPlayer.js
+++ features/musicPlayer.js
@@ -9,8 +9,7 @@ const {
 const ytdl = require('ytdl-core');
 
 class MusicPlayer {
-    constructor(client, guildId, channelId, textChannel) {
-        this.client = client;
+    constructor(guildId, channelId, textChannel) {
         this.guildId = guildId;
         this.channelId = channelId;
         this.textChannel = textChannel;
@@ -34,19 +33,17 @@ class MusicPlayer {
     }
 
     async joinChannel() {
-        const channel = await this.client.channels.fetch(this.channelId);
-        if (!channel || channel.type !== 'GUILD_VOICE') {
-            throw new Error('Invalid voice channel');
-        }
-
         this.connection = joinVoiceChannel({
-            channelId: channel.id,
+            channelId: this.channelId,
             guildId: this.guildId,
-            adapterCreator: channel.guild.voiceAdapterCreator,
+            adapterCreator: this.textChannel.guild.voiceAdapterCreator,
         });
 
         try {
-            await entersState(this.connection, VoiceConnectionStatus.Ready, 20e3);
+            await Promise.race([
+                entersState(this.connection, VoiceConnectionStatus.Ready, 30e3),
+                entersState(this.connection, VoiceConnectionStatus.Signalling, 30e3),
+            ]);
         } catch (error) {
             this.connection.destroy();
             throw error;
@@ -56,10 +53,6 @@ class MusicPlayer {
     }
 
     async addSong(url) {
-        if (!this.isValidYoutubeUrl(url)) {
-            throw new Error('Invalid YouTube URL');
-        }
-
         this.queue.push(url);
         if (this.audioPlayer.state.status !== AudioPlayerStatus.Playing) {
             await this.processQueue();
@@ -76,11 +69,6 @@ class MusicPlayer {
         const stream = ytdl(url, { filter: 'audioonly' });
         const resource = createAudioResource(stream);
         this.audioPlayer.play(resource);
-    }
-
-    isValidYoutubeUrl(url) {
-        const pattern = /^((http(s)?:\/\/)?((w){3}.)?youtube(.com)?|.*)((\?v=)|v\/|embed\/|watch\?|\&v=)?(\?v=)?([^#&?]*).*/;
-        return pattern.test(url);
     }
 
     sendNowPlaying() {
