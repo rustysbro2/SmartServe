ce645dca3860dfa7c37be0942adea40d3aed0554
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-24 15:13:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
+// index.js
 const { ShardingManager } = require('discord.js');
 const { token } = require('./config.js');
-const voiceStateHandler = require('./voiceStateHandler.js');
 
 const manager = new ShardingManager('./bot.js', { token: token });
 
-manager.on('shardCreate', shard => {
-  console.log(`Launched shard ${shard.id}`);
-  voiceStateHandler(shard.client); // Pass the client object to the handler
-});
-
+manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 manager.spawn();
