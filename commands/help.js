3deae4baca40c16912314a3331e874259a900331
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-24 16:48:42 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,23 +10,49 @@ module.exports = {
   async execute(interaction) {
     const commands = interaction.client.commands;
 
+    // Define command categories and their respective commands
+    const commandCategories = [
+      {
+        name: 'Music',
+        commands: [
+          { name: 'play', description: 'Play a song.' },
+          { name: 'skip', description: 'Skip the current song.' },
+        ],
+      },
+      {
+        name: 'Invite Tracker',
+        commands: [
+          { name: 'setinvitechannel', description: 'Set the invite tracking channel.' },
+        ],
+      },
+    ];
+
     // Create the help embed
     const helpEmbed = new MessageEmbed()
       .setColor('#0099ff')
       .setTitle('Help')
       .setDescription('Please select a category:');
     
-    // Get all unique categories from the commands
-    const categories = [...new Set(commands.map(command => command.category))];
-    
     // Create the select menu with category options
     const selectMenu = new MessageSelectMenu()
       .setCustomId('help_category')
-      .setPlaceholder('Select a category')
-      .addOptions(categories.map(category => ({
-        label: category,
-        value: category,
-      })));
+      .setPlaceholder('Select a category');
+    
+    // Add options to the select menu based on command categories
+    commandCategories.forEach(category => {
+      const options = category.commands.map(command => ({
+        label: command.name,
+        value: command.name,
+        description: command.description,
+      }));
+
+      selectMenu.addOptions({
+        label: category.name,
+        value: category.name,
+        description: `Commands: ${options.map(option => option.label).join(', ')}`,
+        options: options,
+      });
+    });
 
     // Create the action row with the select menu
     const actionRow = new MessageActionRow()
