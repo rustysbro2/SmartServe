7aa5f217f2fd32b806216fcaf192d9142622737c
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-24 13:41:46 -0400

    Update musicPlayer.js

--- features/musicPlayer.js
+++ features/musicPlayer.js
@@ -7,6 +7,7 @@ const {
   VoiceConnectionStatus,
 } = require('@discordjs/voice');
 const ytdl = require('ytdl-core');
+const { entersState } = require('@discordjs/voice');
 
 class MusicPlayer {
   constructor(guildId, channelId, textChannel) {
