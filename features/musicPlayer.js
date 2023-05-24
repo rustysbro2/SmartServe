5952e550ead84facd79a541298bbfa14b8f2dfba
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-23 20:22:06 -0400

    Update musicPlayer.js

--- features/musicPlayer.js
+++ features/musicPlayer.js
@@ -17,13 +17,17 @@ class MusicPlayer {
     }
 
     async join(channelId) {
+        console.log("Join method called"); // Add this line
         const channel = await this.client.channels.fetch(channelId);
+        console.log("Channel fetched", channel); // Add this line
         this.connection = joinVoiceChannel({
             channelId: channel.id,
             guildId: this.guildId,
             adapterCreator: channel.guild.voiceAdapterCreator,
         });
+        console.log("Join voice channel", this.connection); // Add this line
         this.connection.subscribe(this.player);
+        console.log("Subscribed player"); // Add this line
     }
 
     async play(url) {
