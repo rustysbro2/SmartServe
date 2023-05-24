0ea5a654b6ff5f3636ec2e70d1265411cb157847
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-24 13:00:44 -0400

    Update musicPlayer.js

--- features/musicPlayer.js
+++ features/musicPlayer.js
@@ -22,80 +22,85 @@ class MusicPlayer {
 
   setupListeners() {
     this.audioPlayer.on('stateChange', async (oldState, newState) => {
-    if (newState.status === AudioPlayerStatus.Idle && oldState.status !== AudioPlayerStatus.Idle) {
-      await this.processQueue();
-    } else if (newState.status === AudioPlayerStatus.Playing && oldState.status !== AudioPlayerStatus.Playing) {
-      this.sendNowPlaying();
-    }
-  });
+      console.log(`State change: ${oldState.status} -> ${newState.status}`);
+      if (newState.status === AudioPlayerStatus.Idle && oldState.status !== AudioPlayerStatus.Idle) {
+        await this.processQueue();
+      } else if (newState.status === AudioPlayerStatus.Playing && oldState.status !== AudioPlayerStatus.Playing) {
+        console.log('Sending Now Playing message...');
+        this.sendNowPlaying();
+      }
+    });
 
-  this.audioPlayer.on('error', (error) => {
-    console.error(`Error: ${error.message}`);
-  });
-}
-
-async joinChannel() {
-  this.connection = joinVoiceChannel({
-    channelId: this.channelId,
-    guildId: this.guildId,
-    adapterCreator: this.textChannel.guild.voiceAdapterCreator,
-  });
-
-  try {
-    await entersState(this.connection, VoiceConnectionStatus.Ready, 30e3);
-  } catch (error) {
-    console.error(`Failed to join voice channel: ${error.message}`);
-    this.connection.destroy();
-    throw error;
+    this.audioPlayer.on('error', (error) => {
+      console.error(`Error: ${error.message}`);
+    });
   }
 
-  this.connection.subscribe(this.audioPlayer);
-}
+  async joinChannel() {
+    console.log('Joining voice channel...');
+    this.connection = joinVoiceChannel({
+      channelId: this.channelId,
+      guildId: this.guildId,
+      adapterCreator: this.textChannel.guild.voiceAdapterCreator,
+    });
 
-isValidYoutubeUrl(url) {
-  const pattern = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/.+/;
-  return pattern.test(url);
-}
-
-async addSong(url) {
-  if (!this.isValidYoutubeUrl(url)) {
-    throw new Error('Invalid YouTube URL');
-  }
-
-  const wasEmpty = this.queue.length === 0;
-
-  this.queue.push(url);
-  if (wasEmpty && this.audioPlayer.state.status !== AudioPlayerStatus.Playing) {
-    await this.processQueue();
-  }
-}
-
-async processQueue() {
-  if (this.queue.length === 0) {
-    if (this.connection) {
+    try {
+      await entersState(this.connection, VoiceConnectionStatus.Ready, 30e3);
+      console.log('Voice connection established.');
+    } catch (error) {
+      console.error(`Failed to join voice channel: ${error.message}`);
       this.connection.destroy();
-      this.connection = null;
+      throw error;
     }
-    return;
+
+    this.connection.subscribe(this.audioPlayer);
   }
 
-  const url = this.queue.shift();
-  const stream = ytdl(url, { filter: 'audioonly' });
-  const resource = createAudioResource(stream);
-
-  this.audioPlayer.play(resource);
-
-  await entersState(this.audioPlayer, AudioPlayerStatus.Playing, 5e3);
-  this.sendNowPlaying();
-}
-
-sendNowPlaying() {
-  const currentSong = this.queue[0];
-  if (currentSong) {
-    const message = `Now playing: ${currentSong}`;
-    this.textChannel.send(message);
+  isValidYoutubeUrl(url) {
+    const pattern = /^(https?:\/\/)?(www\.)?(youtube\.com|youtu\.be)\/.+/;
+    return pattern.test(url);
   }
-}
+
+  async addSong(url) {
+    if (!this.isValidYoutubeUrl(url)) {
+      throw new Error('Invalid YouTube URL');
+    }
+
+    const wasEmpty = this.queue.length === 0;
+
+    this.queue.push(url);
+    if (wasEmpty && this.audioPlayer.state.status !== AudioPlayerStatus.Playing) {
+      await this.processQueue();
+    }
+  }
+
+  async processQueue() {
+    if (this.queue.length === 0) {
+      if (this.connection) {
+        this.connection.destroy();
+        this.connection = null;
+      }
+      return;
+    }
+
+    const url = this.queue.shift();
+    const stream = ytdl(url, { filter: 'audioonly' });
+    const resource = createAudioResource(stream);
+
+    this.audioPlayer.play(resource);
+
+    await entersState(this.audioPlayer, AudioPlayerStatus.Playing, 5e3);
+    this.sendNowPlaying();
+  }
+
+  sendNowPlaying() {
+    const currentSong = this.queue[0];
+    if (currentSong) {
+      console.log('Now playing:', currentSong);
+      const message = `Now playing: ${currentSong}`;
+      this.textChannel.send(message);
+    }
+  }
 }
 
 module.exports = MusicPlayer;
