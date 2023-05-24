c9da9a6967581447224d52bc50dd26f613ef21ca
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-24 11:22:46 -0400

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -2,37 +2,37 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const MusicPlayer = require('../features/musicPlayer.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('play')
-        .setDescription('Play a song from YouTube')
-        .addStringOption(option =>
-            option.setName('url')
-                .setDescription('The YouTube URL of the song to play')
-                .setRequired(true)),
-    async execute(interaction, client) {
-        const url = interaction.options.getString('url');
-        const guildId = interaction.guildId;
-        const channelId = interaction.member.voice.channelId;
-        const textChannel = interaction.channel;
+  data: new SlashCommandBuilder()
+    .setName('play')
+    .setDescription('Play a song from YouTube')
+    .addStringOption(option =>
+      option
+        .setName('url')
+        .setDescription('The YouTube URL of the song to play')
+        .setRequired(true)),
+  async execute(interaction, client) {
+    const url = interaction.options.getString('url');
+    const guildId = interaction.guildId;
+    const channelId = interaction.member.voice.channelId;
+    const textChannel = interaction.channel;
 
-        // The member who sent the command must be in a voice channel:
-        if (!channelId) {
-            return await interaction.reply('You must be in a voice channel to play music!');
-        }
+    // The member who sent the command must be in a voice channel:
+    if (!channelId) {
+      return await interaction.reply('You must be in a voice channel to play music!');
+    }
 
-        // Create or retrieve the music player for this guild
-        let musicPlayer = client.musicPlayers.get(guildId);
-        if (!musicPlayer) {
-            musicPlayer = new MusicPlayer(guildId, channelId, textChannel);
-            client.musicPlayers.set(guildId, musicPlayer);
-            await musicPlayer.joinChannel();
-        }
+    // Create or retrieve the music player for this guild
+    let musicPlayer = client.musicPlayers.get(guildId);
+    if (!musicPlayer) {
+      musicPlayer = new MusicPlayer(guildId, channelId, textChannel);
+      client.musicPlayers.set(guildId, musicPlayer);
+      await musicPlayer.joinChannel();
+    }
 
-        await musicPlayer.addSong(url);
-        await musicPlayer.sendNowPlaying();
+    await musicPlayer.addSong(url);
+    musicPlayer.sendNowPlaying(); // Call sendNowPlaying without await
 
-
-        // Notify the user
-        await interaction.reply(`Added to queue!`);
-    },
+    // Notify the user
+    await interaction.reply(`Added to queue!`);
+  },
 };
