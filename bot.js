fbeb1792dab51b0dce4941fb42d9e83705ebe528
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-24 09:55:18 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const { Client, Collection, Intents, ShardingManager } = require('discord.js');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.js');
 const inviteTracker = require('./features/inviteTracker.js');
 const fs = require('fs');
@@ -10,61 +10,52 @@ const intents = new Intents([
     Intents.FLAGS.GUILD_VOICE_STATES,
 ]);
 
-const manager = new ShardingManager('./bot.js', { token: token });
+const client = new Client({ shards: "auto", intents });
 
-manager.on('shardCreate', async shard => {
-    console.log(`Launched shard ${shard.id}`);
-    await shard.whenReady;
+client.commands = new Collection();
 
-    const client = shard.client;
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
 
-    client.commands = new Collection();
+client.once('ready', async () => {
+    console.log(`Shard ${client.shard.ids} logged in as ${client.user.tag}!`);
+    client.user.setActivity(`${client.guilds.cache.size} servers | Shard ${client.shard.ids[0]}`, { type: 'WATCHING' });
 
-    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
-        const command = require(`./commands/${file}`);
-        client.commands.set(command.data.name, command);
-    }
+    inviteTracker.execute(client);
 
-    client.once('ready', async () => {
-        console.log(`Shard ${shard.id} logged in as ${client.user.tag}!`);
-        client.user.setActivity(`${client.guilds.cache.size} servers | Shard ${shard.id}`, { type: 'WATCHING' });
-
-        inviteTracker.execute(client);
-
-        const slashCommands = require('./slashCommands.js');
-        await slashCommands(client);
-    });
-
-    client.on('interactionCreate', async interaction => {
-        if (!interaction.isCommand()) return;
-
-        const command = client.commands.get(interaction.commandName);
-
-        if (!command) return;
-
-        try {
-            await command.execute(interaction, client);
-        } catch (error) {
-            console.error(error);
-            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-        }
-    });
-
-    client.on('voiceStateUpdate', async (oldState, newState) => {
-        if (!oldState.channelId) return;
-        const botInOldChannel = oldState.channel.members.has(client.user.id);
-        if (!botInOldChannel) return;
-
-        if (oldState.channelId !== newState.channelId) {
-            const playCommand = client.commands.get('play');
-            if (playCommand) {
-                await playCommand.execute(oldState, client);
-            }
-        }
-    });
-
-    await client.login(token);
+    const slashCommands = require('./slashCommands.js');
+    await slashCommands(client);
 });
 
-manager.spawn();
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    try {
+        await command.execute(interaction, client);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+    }
+});
+
+client.on('voiceStateUpdate', async (oldState, newState) => {
+    if (!oldState.channelId) return;
+    const botInOldChannel = oldState.channel.members.has(client.user.id);
+    if (!botInOldChannel) return;
+
+    if (oldState.channelId !== newState.channelId) {
+        const playCommand = client.commands.get('play');
+        if (playCommand) {
+            await playCommand.execute(oldState, client);
+        }
+    }
+});
+
+client.login(token);
