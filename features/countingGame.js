63c0ad897150707c20eae996ecf13c49dccb8928
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-25 14:35:44 -0400

    Update countingGame.js

--- features/countingGame.js
+++ features/countingGame.js
@@ -1,4 +1,3 @@
-//features/countingGame.js
 const { Collection } = require('discord.js');
 const db = require('../database.js');
 
@@ -39,7 +38,77 @@ module.exports = {
       const currentNumber = parseInt(message.content);
       if (isNaN(currentNumber)) return;
 
-      // Your counting game logic here
+      const expectedNumber = await getNextExpectedNumber(message.guild.id);
+      if (currentNumber !== expectedNumber) {
+        await handleIncorrectNumber(message, expectedNumber);
+      } else {
+        await incrementCount(message.guild.id, currentNumber);
+      }
     });
   }
 };
+
+async function getNextExpectedNumber(guildId) {
+  return new Promise((resolve, reject) => {
+    db.query(
+      `
+      SELECT expectedNumber FROM countingGame WHERE guildId = ?
+      `,
+      [guildId],
+      function (error, results) {
+        if (error) return reject(error);
+        if (results.length > 0) {
+          resolve(results[0].expectedNumber + 1);
+        } else {
+          resolve(1);
+        }
+      }
+    );
+  });
+}
+
+async function incrementCount(guildId, currentNumber) {
+  return new Promise((resolve, reject) => {
+    db.query(
+      `
+      INSERT INTO countingGame (guildId, expectedNumber)
+      VALUES (?, ?)
+      ON DUPLICATE KEY UPDATE
+      expectedNumber = GREATEST(expectedNumber, ?)
+      `,
+      [guildId, currentNumber + 1, currentNumber + 1],
+      function (error) {
+        if (error) return reject(error);
+        resolve();
+      }
+    );
+  });
+}
+
+async function handleIncorrectNumber(message, expectedNumber) {
+  const guildId = message.guild.id;
+  const channelId = message.channel.id;
+  const countingChannel = message.guild.channels.cache.get(channelId);
+
+  // Reset counting game for the guild
+  db.query(
+    `
+    DELETE FROM countingGame WHERE guildId = ?
+    `,
+    [guildId],
+    function (error) {
+      if (error) console.error(`Failed to reset counting game for guild ${guildId}:`, error);
+    }
+  );
+
+  // Remove the counting channel from the collection
+  countingChannels.delete(guildId);
+
+  // Send error message and instructions
+  const errorMessage = `You broke the counting game! The expected number was ${expectedNumber}.`;
+  const instructions = 'Start over by sending the number 1.';
+  countingChannel.send(`${errorMessage}\n${instructions}`)
+    .catch((error) => {
+      console.error(`Failed to send counting game error message in guild ${guildId}:`, error);
+    });
+}
