5003050ae548dcbc2e5795ef1080745402b4571a
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-25 15:59:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Client } = require('@discordjs/rest');
 const { token } = require('./config.js');
 
-const client = Client.create({ token: token });
+const client = new Client({ token: token });
 
 client.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 client.spawn();
