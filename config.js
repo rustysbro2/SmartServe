d5f8ca91c15564899579c022c827096728294a82
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-25 12:29:55 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 // config.js
 module.exports = {
-    token: 'MTEwNzAyNTU3ODA0NzA1ODAzMA.Gu5arZ.UR86k56q8IJpYRgGz0yS6JbODVvyuDKWgcYaZs',
-    clientId: '1107025578047058030'	
+    token: '',
+    clientId: ''
 };
-
