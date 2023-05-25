3cfde7b272822c5934f04422d5c85578e25d0b14
rustysbro2 <rustysbro2016@techie.com>
2023-05-25 08:40:30 -0700

    Everything

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 // config.js
 module.exports = {
-    token: process.env.BOT_TOKEN,
-    // you can also store other configuration data here
+    token: 'MTEwNzAyNTU3ODA0NzA1ODAzMA.Gu5arZ.UR86k56q8IJpYRgGz0yS6JbODVvyuDKWgcYaZs',
+    clientId: '1107025578047058030'	
 };
+
