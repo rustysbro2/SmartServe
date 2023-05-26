8d678a8b720a417a22e8983d72e3fbef8822f8b2
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-26 10:40:33 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,130 +1,24 @@
-// bot.js
-const { Client, Collection, Intents } = require('discord.js');
-const { token } = require('./config.js');
-const inviteTracker = require('./features/inviteTracker.js');
-const fs = require('fs');
-const { joinVoiceChannel, entersState, VoiceConnectionStatus } = require('@discordjs/voice');
-const { AudioPlayerStatus, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
-const ytdl = require('ytdl-core');
+const Discord = require('discord.js');
 
-const intents = new Intents([
-  Intents.FLAGS.GUILDS,
-  Intents.FLAGS.GUILD_MESSAGES,
-  Intents.FLAGS.GUILD_MEMBERS,
-  Intents.FLAGS.GUILD_VOICE_STATES,
-  Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-  Intents.FLAGS.GUILD_MESSAGE_TYPING,
-  Intents.FLAGS.GUILD_EMOJIS,
-  Intents.FLAGS.GUILD_INTEGRATIONS,
-  Intents.FLAGS.GUILD_WEBHOOKS,
-  Intents.FLAGS.GUILD_INVITES,
-  Intents.FLAGS.GUILD_BANNED_MEMBERS,
-  Intents.FLAGS.GUILD_WIDGETS,
-  Intents.FLAGS.GUILD_STAGE_CHANNELS,
-  Intents.FLAGS.GUILD_PARTS,
-  Intents.FLAGS.MESSAGE_CONTENT,
-  Intents.FLAGS.MESSAGE_EMBEDS,
-  Intents.FLAGS.MESSAGE_REACTIONS,
-  Intents.FLAGS.MESSAGE_TYPING,
-  Intents.FLAGS.GUILD_VOICE_STATES,
-  Intents.FLAGS.GUILD_MESSAGES_EPHEMERAL,
-  Intents.FLAGS.GUILD_MESSAGE_DELETES,
-  Intents.FLAGS.GUILD_EMOJIS_UPDATE,
-  Intents.FLAGS.GUILD_WEBHOOKS_UPDATE,
-  Intents.FLAGS.GUILD_INVITES_UPDATE,
-  Intents.FLAGS.GUILD_BANNED_MEMBERS_UPDATE,
-  Intents.FLAGS.GUILD_WIDGETS_UPDATE,
-  Intents.FLAGS.GUILD_STAGE_CHANNELS_UPDATE,
-  Intents.FLAGS.GUILD_PARTS_UPDATE
-]);
+const ShardingManager = require('discord.js').ShardingManager;
 
-const client = new Client({ shards: "auto", intents });
+const { TOKEN } = require('./.env');
 
-client.commands = new Collection();
-client.musicPlayers = new Map();
-
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.data.name, command);
-}
-
-client.once('ready', async () => {
-  console.log(`Shard ${client.shard.ids} logged in as <span class="math-inline">\{client\.user\.tag\}\!\`\);
-client\.user\.setActivity\(\`</span>{client.guilds.cache.size} servers | Shard ${client.shard.ids[0]}`, { type: 'WATCHING' });
-
-  inviteTracker.execute(client);
-
-  const slashCommands = require('./slashCommands.js');
-  await slashCommands(client);
-
-  // Start checking voice channels every second
-  setInterval(() => {
-    checkVoiceChannels();
-  }, 1000); // Check every 1 second
+const bot = new ShardingManager({
+  clientOptions: {
+    token: TOKEN,
+  },
+  shardCount: 10, // The number of shards to create.
 });
 
-async function checkVoiceChannels() {
-  const botId = client.user.id;
-  const guilds = client.guilds.cache;
+bot.on('ready', () => {
+  console.log('Bot is ready!');
+});
 
-  for (const guild of guilds) {
-    const guildId = guild[1].id;
-    const musicPlayer = client.musicPlayers.players.get(guildId);
-    const voiceChannels = guild[1].channels.cache.filter(channel => channel.type === 'GUILD_VOICE');
-
-    for (const [channelId, channel] of voiceChannels) {
-      if (channel.members.size === 1 && channel.members.has(botId)) {
-        // Bot is the only member in the voice channel
-        console.log(`Bot is the only member in the voice channel: ${channel.name}`);
-        console.log(`Channel Members: ${channel.members.size}`);
-        
-        if (musicPlayer && musicPlayer.connection) {
-          console.log("Destroying connection and leaving voice channel.");
-          musicPlayer.connection.destroy();
-client.musicPlayers.players.delete(guildId);
-      }
-    }
-  }
-}
-
-client.on('command', async (interaction) => {
-  if (!interaction.isCommand()) return;
-
-  const command = client.commands.commands.get(interaction.commandName);
-
-  if (!command) return;
-
-  try {
-    await command.execute(interaction, client); // Execute the command
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+bot.on('message', (message) => {
+  if (message.content === 'ping') {
+    message.channel.send('pong');
   }
 });
 
-client.login(token);
-
-connection.destroy();
-client.musicPlayers.players.delete(guildId);
-      }
-    }
-  }
-}
-
-client.on('command', async (interaction) => {
-  if (!interaction.isCommand()) return;
-
-  const command = client.commands.commands.get(interaction.commandName);
-
-  if (!command) return;
-
-  try {
-    await command.execute(interaction, client); // Execute the command
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-  }
-});
-
-client.login(token);
+bot.login(); // This will automatically spawn the shards.
