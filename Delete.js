c50818d84bb8760d48542787adf537b7d356b67a
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-26 19:13:24 -0400

    Update Delete.js

--- Delete.js
+++ Delete.js
@@ -1,16 +1,30 @@
+const { Client } = require('discord.js');
 const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const { Routes } = require('discord-api-types/v10');
 const { clientId, token } = require('./config.js');
 
-const rest = new REST({ version: '9' }).setToken(token);
+const client = new Client();
+const rest = new REST({ version: '10' }).setToken(token);
 
 (async () => {
-  const commands = await rest.get(Routes.applicationCommands(clientId));
+  try {
+    console.log('Deleting all commands...');
 
-  for (let command of commands) {
-    console.log(`Deleting command ${command.name}`);
-    await rest.delete(Routes.applicationCommand(clientId, command.id));
+    await client.login(token);
+    const guilds = client.guilds.cache.map((guild) => guild.id);
+
+    for (const guildId of guilds) {
+      await rest.put(
+        Routes.applicationGuildCommands(clientId, guildId),
+        { body: [] }
+      );
+
+      console.log(`All commands deleted for guild ${guildId}.`);
+    }
+
+    console.log('All commands deleted successfully.');
+    client.destroy();
+  } catch (error) {
+    console.error('Error deleting commands:', error);
   }
-  
-  console.log('All commands deleted successfully.');
 })();
