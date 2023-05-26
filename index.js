972a4b8eaa62af82d06a407bb36d2babf4b48d4c
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-26 10:14:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,22 @@
-const { Client } = require('./module.js');
+const Discord = require("discord.js");
 
-const client = new Client({
-  intents: [
-    GatewayIntentBits.GUILDS,
-    GatewayIntentBits.GUILD_MESSAGES,
-    GatewayIntentBits.GUILD_MEMBERS,
-    GatewayIntentBits.GUILD_VOICE_STATES,
-  ],
+const client = new Discord.Client();
+
+const shardingManager = new Discord.ShardingManager(client);
+
+shardingManager.options = {
+  shardCount: 10,
+  shardThreshold: 1000,
+};
+
+shardingManager.start();
+
+client.on("ready", () => {
+  console.log("Bot is ready!");
 });
 
-client.on('ready', async () => {
-  console.log(`Shard ${client.shard.ids} logged in as ${client.user.tag}!`);
-  client.user.setActivity(`${client.guilds.cache.size} servers | Shard ${client.shard.ids[0]}`, { type: 'WATCHING' });
+client.on("message", (message) => {
+  // Do something with the message.
 });
 
-client.on('message', async (message) => {
-  if (message.content.startsWith(config.prefix)) {
-    const command = await client.commands.find(command => command.name === message.content.slice(config.prefix.length));
-
-    if (command) {
-      try {
-        await command.execute(message);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  }
-});
-
-client.login(process.env.TOKEN);
+client.login("YOUR_BOT_TOKEN");
