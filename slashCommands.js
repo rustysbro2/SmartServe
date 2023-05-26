71506c26dae96a883abe76e36d133314cb526b53
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-26 18:57:49 -0400

    Update slashCommands.js

--- slashCommands.js
+++ slashCommands.js
@@ -1,65 +1,30 @@
-// slashCommands.js
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v10');
 const { clientId, token } = require('./config.js');
-const fs = require('fs');
-
-function commandHasChanged(oldCommand, newCommand) {
-    return oldCommand.name !== newCommand.name ||
-        oldCommand.description !== newCommand.description ||
-        (oldCommand.options && newCommand.options && 
-        JSON.stringify(oldCommand.options) !== JSON.stringify(newCommand.options));
-}
+const { readdirSync } = require('fs');
+const { join } = require('path');
 
 module.exports = async function(client) {
-    const commands = [];
-    const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+  const commands = [];
+  const commandFiles = readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-    for (const file of commandFiles) {
-        const command = require(`./commands/${file}`);
-        commands.push(command.data);
-    }
+  for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+  }
 
-    const rest = new REST({ version: '10' }).setToken(token);
+  const rest = new REST({ version: '10' }).setToken(token);
 
-    // Loop through each guild the bot is in and register the slash commands
-    client.guilds.cache.forEach(async (guild) => {
-        try {
-            console.log(`Started refreshing application (/) commands for guild ${guild.id}.`);
+  try {
+    console.log('Started refreshing application (/) commands.');
 
-            // Get existing slash commands in the guild
-            const existingCommands = await rest.get(
-                Routes.applicationGuildCommands(clientId, guild.id)
-            );
+    await rest.put(
+      Routes.applicationCommands(clientId),
+      { body: commands }
+    );
 
-            // Remove old commands and update changed commands
-            for (const command of existingCommands) {
-                const newCommand = commands.find(cmd => cmd.name === command.name);
-                if (!newCommand) {
-                    await rest.delete(
-                        Routes.applicationGuildCommand(clientId, guild.id, command.id)
-                    );
-                } else if (commandHasChanged(command, newCommand)) {
-                    await rest.patch(
-                        Routes.applicationGuildCommand(clientId, guild.id, command.id),
-                        { body: newCommand }
-                    );
-                }
-            }
-
-            // Add new commands
-            for (const command of commands) {
-                if (!existingCommands.find(cmd => cmd.name === command.name)) {
-                    await rest.post(
-                        Routes.applicationGuildCommands(clientId, guild.id),
-                        { body: command },
-                    );
-                }
-            }
-
-            console.log(`Successfully reloaded application (/) commands for guild ${guild.id}.`);
-        } catch (error) {
-            console.error(`Error while refreshing application (/) commands for guild ${guild.id}.`, error);
-        }
-    });
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error('Error while refreshing application (/) commands:', error);
+  }
 };
