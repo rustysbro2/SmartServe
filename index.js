282e3483c26a7f8770b6520808795fc0ee8348bf
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-26 10:58:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 const { ShardingManager } = require('discord.js');
-const dotenv = require("dotenv");
+const { token } = require('./config.js');
 
-// Load environment variables from .env file
-dotenv.config();
+const manager = new ShardingManager('./bot.js', { token: token });
 
-// Create a new ShardingManager object
-const manager = new ShardingManager('./bot.js', {
-  token: process.env.BOT_TOKEN,
-});
-
-// Listen for the shardCreate event
 manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 
-// Start the ShardingManager
 manager.spawn();
