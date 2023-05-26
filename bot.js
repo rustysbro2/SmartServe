bce14f01fe1487ee074793f0e80c8e88418869b3
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-26 10:58:48 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,83 @@
-const Discord = require('discord.js');
+// bot.js
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./config.js');
+const inviteTracker = require('./features/inviteTracker.js');
+const fs = require('fs');
+const { joinVoiceChannel, entersState, VoiceConnectionStatus } = require('@discordjs/voice');
+const { AudioPlayerStatus, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
+const ytdl = require('ytdl-core');
 
-// Create a new ShardManager instance.
-const shardManager = new Discord.ShardManager({
-  clientId: 'my-bot-id',
-  clientToken: 'my-bot-token',
-  shards: 10, // The number of shards to create.
+const intents = new Intents([
+  Intents.FLAGS.GUILDS,
+  Intents.FLAGS.GUILD_MESSAGES,
+  Intents.FLAGS.GUILD_MEMBERS,
+  Intents.FLAGS.GUILD_VOICE_STATES,
+]);
+
+const client = new Client({ shards: "auto", intents });
+
+client.commands = new Collection();
+client.musicPlayers = new Map();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
+
+client.once('ready', async () => {
+  console.log(`Shard ${client.shard.ids} logged in as ${client.user.tag}!`);
+  client.user.setActivity(`${client.guilds.cache.size} servers | Shard ${client.shard.ids[0]}`, { type: 'WATCHING' });
+
+  inviteTracker.execute(client);
+
+  const slashCommands = require('./slashCommands.js');
+  await slashCommands(client);
+
+  // Start checking voice channels every second
+  setInterval(() => {
+    checkVoiceChannels();
+  }, 1000); // Check every 1 second
 });
 
-// Create a new Discord client instance for each shard.
-shardManager.on('shardReady', (shard) => {
-  const client = new Discord.Client({
-    id: shard.clientId,
-    token: shard.clientToken,
-  });
+async function checkVoiceChannels() {
+  const botId = client.user.id;
+  const guilds = client.guilds.cache;
 
-  // Add your bot's commands to the client.
-  client.commands.add(yourCommands);
+  for (const guild of guilds) {
+    const guildId = guild[1].id;
+    const musicPlayer = client.musicPlayers.get(guildId);
+    const voiceChannels = guild[1].channels.cache.filter(channel => channel.type === 'GUILD_VOICE');
 
-  // Start the client.
-  client.login();
+    for (const [channelId, channel] of voiceChannels) {
+      if (channel.members.size === 1 && channel.members.has(botId)) {
+        // Bot is the only member in the voice channel
+        console.log(`Bot is the only member in the voice channel: ${channel.name}`);
+        console.log(`Channel Members: ${channel.members.size}`);
+        
+        if (musicPlayer && musicPlayer.connection) {
+          console.log("Destroying connection and leaving voice channel.");
+          musicPlayer.connection.destroy();
+          client.musicPlayers.delete(guildId);
+        }
+      }
+    }
+  }
+}
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction, client); // Execute the command
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+  }
 });
 
-// Start the sharding manager.
-shardManager.start();
+client.login(token);
