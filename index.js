ab48499b713f55cdc40e9c009c4b25889813caaa
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-26 10:27:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
-const { Client, Intents } = require('discord.js');
+const { ShardingManager } = require('discord.js');
 const dotenv = require("dotenv");
 
 // Load environment variables from .env file
 dotenv.config();
 
-// Create a new Discord client object
-const client = new Discord.Client({
-  intents: ["guilds", "channels", "messages", "presences"],
-  shardCount: 10,
-  shardThreshold: 1000,
+// Create a new ShardingManager object
+const manager = new ShardingManager('./bot.js', {
+  token: process.env.BOT_TOKEN,
 });
 
-// Create a new ShardingManager object
-const shardingManager = new Discord.ShardingManager(client);
+// Listen for the shardCreate event
+manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 
 // Start the ShardingManager
-shardingManager.start();
-
-// Listen for the ready event
-client.on("ready", () => {
-  console.log("Bot is ready!");
-});
-
-// Listen for the message event
-client.on("message", (message) => {
-  // Do something with the message
-});
-
-// Login with the bot's token
-client.login(process.env.BOT_TOKEN);
+manager.spawn();
