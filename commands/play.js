6eb8b31377119248164fbe8d40c81549dac7f0f8
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-26 21:53:43 -0400

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,26 +1,60 @@
+// commands/play.js
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MusicPlayer } = require('../features/musicPlayer');
+const MusicPlayer = require('../features/musicPlayer.js');
+const { AudioPlayerStatus, entersState } = require('@discordjs/voice');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('play')
-    .setDescription('Play a song')
-    .addStringOption((option) =>
-      option.setName('url').setDescription('YouTube URL of the song').setRequired(true)
-    ),
-  async execute(interaction) {
-    const guildId = interaction.guildId;
-    const channelId = interaction.member.voice.channelId;
-    const url = interaction.options.getString('url');
-
-    const musicPlayer = new MusicPlayer(guildId, channelId, interaction.channel);
-
+    .setDescription('Play a song from YouTube')
+    .addStringOption(option =>
+      option
+        .setName('url')
+        .setDescription('The YouTube URL of the song to play')
+        .setRequired(true)),
+  async execute(interaction, client) {
     try {
+      console.log('Executing /play command...');
+      const url = interaction.options.getString('url');
+      console.log('URL:', url);
+      const guildId = interaction.guild.id;
+      console.log('Guild ID:', guildId);
+      const channelId = interaction.member.voice.channelId;
+      console.log('Channel ID:', channelId);
+      const textChannel = interaction.channel;
+
+      if (!channelId) {
+        console.log('User not in a voice channel.');
+        return await interaction.reply('You must be in a voice channel to play music!');
+      }
+
+      // Respond to the interaction right away to avoid a timeout
+      await interaction.reply('Processing your request...');
+
+      let musicPlayer = client.musicPlayers.get(guildId);
+      if (!musicPlayer) {
+        console.log('Creating new MusicPlayer instance.');
+        musicPlayer = new MusicPlayer(guildId, channelId, textChannel);
+        client.musicPlayers.set(guildId, musicPlayer);
+        await musicPlayer.joinChannel();
+      }
+
+      const wasEmpty = musicPlayer.queue.length === 0;
+      console.log('Queue was empty before adding song:', wasEmpty);
       await musicPlayer.addSong(url);
-      await interaction.reply(`Added song to the queue: ${url}`);
+
+      if (wasEmpty && musicPlayer.queue.length === 1) {
+        console.log('Waiting for AudioPlayer to transition to "Playing" state...');
+        await entersState(musicPlayer.audioPlayer, AudioPlayerStatus.Playing, 5e3);
+        musicPlayer.sendNowPlaying(textChannel);
+        console.log('Now playing message sent.');
+      }
+
+      // Update the interaction with the completion status
+      await interaction.editReply('Added to queue!');
     } catch (error) {
-      console.error('Error executing /play command:', error.message);
-      await interaction.reply('Failed to add the song to the queue.');
+      console.error(`Error executing /play command: ${error.message}`);
+      await interaction.editReply({ content: `There was an error while executing this command! Error details: ${error.message}`, ephemeral: true });
     }
   },
 };
