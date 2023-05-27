9838485f7469ddd0080b52ce8bb2a7ac99d20f04
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-27 18:03:30 -0400

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,41 +1,42 @@
-const { SlashCommandBuilder, ActionRowBuilder, SelectMenuBuilder } = require('discord.js');
+const { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
+  data: new SlashCommandBuilder()
     .setName('status')
     .setDescription('Set status'),
-    async execute(interaction, client) {
-        const actionRow = new ActionRowBuilder()
-        .addComponents(
-            new SelectMenuBuilder()
-            .setCustomId('set-status')
-            .setPlaceholder('Nothing is selected.')
-            .setMinValues(1)
-            .setMaxValues(1)
-            .addOptions([
-                {
-                    label: `online`,
-                    description: `Online status.`,
-                    value: `online`
-                },
-                {
-                    label: `idle`,
-                    description: `Idle status.`,
-                    value: `idle`
-                },
-                {
-                    label: `dnd`,
-                    description: `Do Not Disturb status.`,
-                    value: `dnd`
-                },
-                {
-                    label: `invisible`,
-                    description: `Invisible status.`,
-                    value: `invisible`
-                },
-            ])
-        );
+  
+  async execute(interaction, client) {
+    const actionRow = new ActionRowBuilder()
+      .addComponents(
+        new StringSelectMenuBuilder()
+          .setCustomId('set-status')
+          .setPlaceholder('Nothing is selected.')
+          .setMinValues(1)
+          .setMaxValues(1)
+          .addOptions([
+            {
+              label: 'Online',
+              description: 'Online status.',
+              value: 'online'
+            },
+            {
+              label: 'Idle',
+              description: 'Idle status.',
+              value: 'idle'
+            },
+            {
+              label: 'Do Not Disturb',
+              description: 'Do Not Disturb status.',
+              value: 'dnd'
+            },
+            {
+              label: 'Invisible',
+              description: 'Invisible status.',
+              value: 'invisible'
+            },
+          ])
+      );
 
-        await interaction.reply({ content: `Status? `, components: [actionRow] });
-    },
+    await interaction.reply({ content: 'Status?', components: [actionRow] });
+  },
 };
