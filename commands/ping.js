477a4ff4de6865a88de74c141d71a5d04f1a296f
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-27 10:19:02 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,17 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
-    data: new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!'),
-    async execute(interaction) {
-        await interaction.reply('Pong!');
-    },
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Replies with Pong!')
+    .addStringOption(option =>
+      option.setName('category')
+        .setDescription('Command category')
+        .setRequired(true)),
+
+  async execute(interaction) {
+    await interaction.reply('Pong!');
+  },
+  category: 'Main Menu', // Set the command category
+  categoryDescription: 'Main menu commands' // Set the category description
 };
