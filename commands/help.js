b612b95a7d6c8a6c28ec79c70878d4d608bdaa3d
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-27 15:26:48 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,7 @@ module.exports = {
 
   async execute(interaction) {
     const commandCategories = [];
-    const defaultCategory = 'Uncategorized'; // Specify the default category name
+    const defaultCategoryName = 'Uncategorized'; // Specify the default category name
 
     // Get the absolute path to the commands directory (same directory as help.js)
     const commandsDirectory = path.join(__dirname);
@@ -18,7 +18,7 @@ module.exports = {
     const commandFiles = fs.readdirSync(commandsDirectory).filter((file) => file.endsWith('.js'));
 
     // Loop through each command module
-    for (for (const file of commandFiles) {
+    for (const file of commandFiles) {
       if (file === 'help.js') continue; // Skip the help command file
 
       const command = require(path.join(commandsDirectory, file));
@@ -66,51 +66,34 @@ module.exports = {
       }
     }
 
-
-          // Check if the category has a description property and it is not empty
-          if (command.hasOwnProperty('categoryDescription') && command.categoryDescription.length > 0) {
-            newCategory.description = command.categoryDescription;
-          }
-
-          commandCategories.push(newCategory);
-        }
-      } else {
-        // Assign the command to the default category
-        commandCategories.push({
-          name: defaultCategory,
-          description: 'Commands that do not belong to any specific category',
-          commands: [
-            {
-              name: command.data.name,
-              description: command.data.description,
-            },
-          ],
-        });
-      }
-    }
-
     // Create the string select menu and add options for each command category
     const selectMenu = new StringSelectMenuBuilder()
       .setCustomId('help_category')
       .setPlaceholder('Select a category');
 
-    let optionIndex = 0; // Variable to keep track of the option index
+    // Create a set to keep track of used option values
+    const usedOptionValues = new Set();
 
     commandCategories.forEach((category) => {
       const optionBuilder = new StringSelectMenuOptionBuilder()
         .setLabel(category.name)
-        .setValue(`category_${optionIndex}`); // Use a unique value for each category option
+        .setValue(category.name);
+
+      // Generate a unique option value if it is already used
+      while (usedOptionValues.has(optionBuilder.value)) {
+        optionBuilder.setValue(optionBuilder.value + '_'); // Append an underscore to the value
+      }
+
+      // Add the option value to the set
+      usedOptionValues.add(optionBuilder.value);
 
       // Set the description only if it exists and is not empty
-      if (category.hasOwnProperty('description') && category.description.length > 0) {
+      if (category.description && category.description.length > 0) {
         optionBuilder.setDescription(category.description);
       }
 
       selectMenu.addOptions(optionBuilder);
-
-      optionIndex++; // Increment the option index for the next category
     });
-
 
     // Create the action row with the select menu
     const actionRow = new ActionRowBuilder().addComponents(selectMenu);
@@ -122,12 +105,6 @@ module.exports = {
       .setColor('#0099ff');
 
     // Send the initial embed with the action row and select menu
-    try {
-      await interaction.reply({ embeds: [initialEmbed], components: [actionRow] });
-    } catch (error) {
-      console.error('An error occurred while executing the help command:');
-      console.error(error);
-      await interaction.reply('An error occurred while executing the help command.');
-    }
+    await interaction.reply({ embeds: [initialEmbed], components: [actionRow] });
   },
 };
