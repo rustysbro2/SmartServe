9f010ad13e279840bf4a1b311435183b0ee84068
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-28 14:24:40 -0400

    Update musicPlayer.js

--- features/musicPlayer.js
+++ features/musicPlayer.js
@@ -248,8 +248,8 @@ isBotAlone() {
 
     const otherMembers = members.filter(member => !member.user.bot && member.id !== botId);
 
-    if (otherMembers.size === 0 && botMember.voice?.selfDeaf) {
-      console.log('Bot is alone in the voice channel and self-deafened. Leaving the channel.');
+    if (otherMembers.size === 0 && !botMember.voice?.selfDeaf) {
+      console.log('Bot is alone in the voice channel. Leaving the channel.');
       this.audioPlayer.stop();
       this.connection.destroy();
       this.connection = null;
