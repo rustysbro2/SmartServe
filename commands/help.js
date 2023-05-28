b59dbbf9b3ea41c555d4f20eb5f651bb9c41c80f
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-27 20:05:10 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,33 +2,32 @@ const { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder, StringSe
 const fs = require('fs');
 const path = require('path');
 
-// Function to handle select menu interaction
-// Function to handle select menu interaction
 async function handleSelectMenu(interaction, commandCategories) {
   console.log('Select menu interaction received:', interaction);
 
   const selectedCategory = interaction.values[0];
 
-  // Find the category based on the selected value
+  console.log('Selected category:', selectedCategory);
+
   const category = commandCategories.find(
     (category) =>
       category.name.toLowerCase().replace(/\s/g, '_') === selectedCategory
   );
 
   if (category) {
-    // Create the embed with the category's commands
+    console.log('Category found:', category.name);
+
     const categoryEmbed = new EmbedBuilder()
       .setTitle(`Commands - ${category.name}`)
       .setDescription(category.description || 'No description available');
 
-    // Add the commands as fields in the embed
     category.commands.forEach((command) => {
+      console.log('Adding command to embed:', command.name);
       categoryEmbed.addFields({ name: command.name, value: command.description });
     });
 
     try {
       if (interaction.message) {
-        // If the interaction has a message, update the original embed
         await interaction.message.edit({ embeds: [categoryEmbed] });
         console.log('Interaction message updated.');
       } else {
@@ -42,7 +41,6 @@ async function handleSelectMenu(interaction, commandCategories) {
   }
 }
 
-
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('help')
@@ -52,52 +50,46 @@ module.exports = {
     console.log('Help command interaction received:', interaction);
 
     if (interaction.deferred || interaction.replied) {
-      // Interaction has already been replied to or deferred
+      console.log('Interaction already deferred or replied to.');
       return;
     }
 
     const commandCategories = [];
-    const defaultCategoryName = 'Uncategorized'; // Specify the default category name
+    const defaultCategoryName = 'Uncategorized';
 
-    // Get the absolute path to the commands directory (same directory as help.js)
     const commandsDirectory = path.join(__dirname, '../commands');
+    console.log('Commands directory:', commandsDirectory);
 
-    // Read all command modules from the commands directory
     const commandFiles = fs.readdirSync(commandsDirectory).filter((file) => file.endsWith('.js'));
+    console.log('Command files:', commandFiles);
 
-    // Loop through each command module
     for (const file of commandFiles) {
-      if (file === 'help.js') continue; // Skip the help command file
+      if (file === 'help.js') continue;
 
       const command = require(path.join(commandsDirectory, file));
+      console.log('Command module:', command);
 
-      // Check if the command module has a category property
       if (command.category) {
-        // Check if the category already exists in the commandCategories array
         let category = commandCategories.find((category) => category.name === command.category);
 
         if (!category) {
-          // Create a new category if it doesn't exist
           category = {
             name: command.category,
-            description: '', // Initialize the description as an empty string
+            description: '',
             commands: [],
           };
 
           commandCategories.push(category);
         }
 
-        // Add the command to the category
         category.commands.push({
           name: command.data.name,
           description: command.data.description,
         });
       } else {
-        // Assign the command to the default category
         let defaultCategory = commandCategories.find((category) => category.name === defaultCategoryName);
 
         if (!defaultCategory) {
-          // Create the default category if it doesn't exist
           defaultCategory = {
             name: defaultCategoryName,
             description: 'Commands that do not belong to any specific category',
@@ -107,7 +99,6 @@ module.exports = {
           commandCategories.push(defaultCategory);
         }
 
-        // Add the command to the default category
         defaultCategory.commands.push({
           name: command.data.name,
           description: command.data.description,
@@ -115,9 +106,10 @@ module.exports = {
       }
     }
 
-    const usedOptionValues = new Set(); // Track used option values
+    console.log('Command categories:', commandCategories);
 
-    // Create the string select menu and add options for each command category
+    const usedOptionValues = new Set();
+
     const selectMenu = new StringSelectMenuBuilder()
       .setCustomId('help_category')
       .setPlaceholder('Select a category');
@@ -125,9 +117,8 @@ module.exports = {
     commandCategories.forEach((category) => {
       const optionBuilder = new StringSelectMenuOptionBuilder()
         .setLabel(category.name)
-        .setValue(generateUniqueOptionValue(category.name)); // Generate a unique option value
+        .setValue(generateUniqueOptionValue(category.name));
 
-      // Set the description only if it exists and is not empty
       if (category.description && category.description.length > 0) {
         optionBuilder.setDescription(category.description);
       }
@@ -135,14 +126,12 @@ module.exports = {
       selectMenu.addOptions(optionBuilder);
     });
 
-    // Function to generate a unique option value
     function generateUniqueOptionValue(categoryName) {
       const sanitizedCategoryName = categoryName.toLowerCase().replace(/\s/g, '_');
 
       let optionValue = sanitizedCategoryName;
       let index = 1;
 
-      // Append a number to the option value until it becomes unique
       while (usedOptionValues.has(optionValue)) {
         optionValue = `${sanitizedCategoryName}_${index}`;
         index++;
@@ -152,17 +141,14 @@ module.exports = {
       return optionValue;
     }
 
-    // Create the action row with the select menu
     const actionRow = new ActionRowBuilder().addComponents(selectMenu);
 
-    // Create the initial embed with the category information
     const initialEmbed = new EmbedBuilder()
       .setTitle('Command Categories')
       .setDescription('Please select a category from the dropdown menu.')
       .setColor('#0099ff');
 
     try {
-      // Send the initial embed with the action row and select menu
       await interaction.reply({ embeds: [initialEmbed], components: [actionRow] });
       console.log('Initial embed sent.');
     } catch (error) {
