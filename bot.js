ce2eeac1059bea18c0c2638b8d358a866d55b908
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-27 20:34:01 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -77,28 +77,21 @@ async function checkVoiceChannels() {
   const botId = client.user.id;
   const guilds = client.guilds.cache;
 
+  console.log('Checking voice channels...');
+
   for (const guild of guilds) {
     const guildId = guild[1].id;
     const musicPlayer = client.musicPlayers.get(guildId);
     const voiceChannels = guild[1].channels.cache.filter(channel => channel.type === 'GUILD_VOICE');
 
-    for (const [channelId, channel] of voiceChannels) {
-      const membersInChannel = channel.members.filter(member => !member.user.bot);
-      
-      if (membersInChannel.size === 0) {
-        // No non-bot members in the voice channel
-        console.log(`No members in the voice channel: ${channel.name}`);
-        console.log(`Channel Members: ${channel.members.size}`);
+    console.log(`Guild: ${guildId}, Voice Channels: ${voiceChannels.size}`);
 
-        if (musicPlayer && musicPlayer.connection) {
-          console.log("Destroying connection and leaving voice channel.");
-          musicPlayer.connection.destroy();
-          client.musicPlayers.delete(guildId);
-        }
-      } else if (membersInChannel.size === 1 && membersInChannel.has(botId)) {
-        // Bot is the only non-bot member in the voice channel
+    for (const [channelId, channel] of voiceChannels) {
+      console.log(`Channel: ${channelId}, Members: ${channel.members.size}`);
+
+      if (channel.members.size === 1 && channel.members.has(botId)) {
+        // Bot is the only member in the voice channel
         console.log(`Bot is the only member in the voice channel: ${channel.name}`);
-        console.log(`Channel Members: ${channel.members.size}`);
 
         if (musicPlayer && musicPlayer.connection) {
           console.log("Destroying connection and leaving voice channel.");
@@ -109,6 +102,7 @@ async function checkVoiceChannels() {
     }
   }
 }
+
 
 
 
