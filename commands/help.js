f2b6c82b5a030ef617bd497287ff10d1370d13bc
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-29 18:16:14 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, EmbedBuilder } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
-const { guildId } = require('../config.js');
+const { guildId } = require('../config.js');//tt
 
 async function handleSelectMenu(interaction, commandCategories) {
   console.log('Select menu interaction received:', interaction);
