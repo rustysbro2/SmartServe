21d1cdfce8523307dde561eb65f0b253989dd7a6
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-29 09:34:18 -0400

    Update slashCommands.js

--- slashCommands.js
+++ slashCommands.js
@@ -39,45 +39,39 @@ module.exports = async function (client) {
     console.log('Started refreshing application (/) commands.');
 
     // Get existing global slash commands
-    console.log('Fetching existing global commands...');
     const existingGlobalCommands = await rest.get(
       Routes.applicationCommands(clientId)
     );
-    console.log('Existing global commands fetched:', existingGlobalCommands);
 
-    // Remove old global commands
-    console.log('Deleting old global commands...');
-    const deleteGlobalPromises = existingGlobalCommands.map((command) =>
-      rest.delete(Routes.applicationCommand(clientId, command.id))
-    );
+    // Remove old global commands that are not present in the updated commands
+    const deleteGlobalPromises = existingGlobalCommands.map((command) => {
+      if (!commands.find((updatedCommand) => updatedCommand.name === command.name)) {
+        console.log(`Deleting old global command: ${command.name}`);
+        return rest.delete(Routes.applicationCommand(clientId, command.id));
+      }
+    });
     await Promise.all(deleteGlobalPromises);
-    console.log('Old global commands deleted.');
 
     // Register updated global commands
-    console.log('Registering updated global commands...');
     const registerGlobalPromises = [rest.post(
       Routes.applicationCommands(clientId),
       { body: commands },
     )];
     await Promise.all(registerGlobalPromises);
-    console.log('Updated global commands registered.');
 
     console.log('Successfully reloaded global application (/) commands.');
 
     // Register guild-specific commands
     for (const { guildId, command, category } of guildSpecificCommands) {
-      console.log(`Registering guild-specific command for guild ID ${guildId}...`);
       await rest.post(
         Routes.applicationGuildCommand(clientId, guildId),
         { body: command }
       );
-      console.log(`Guild-specific command registered for guild ID ${guildId}.`);
     }
 
     console.log('Successfully added guild-specific commands.');
 
   } catch (error) {
     console.error('Error while refreshing application (/) commands.', error);
-    console.error('Command that caused the error:', commands.find(cmd => cmd.name === error.rawError.errors.name[0].name));
   }
 };
