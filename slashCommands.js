06bdb5c353f1b09887f0d5d76ac1fdc74ee95691
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-29 11:26:29 -0400

    Update slashCommands.js

--- slashCommands.js
+++ slashCommands.js
@@ -23,7 +23,7 @@ module.exports = async function (client) {
     if (command.global !== false) {
       globalCommands.push(command.data.toJSON());
       console.log(`Refreshing global command: ./commands/${file}`);
-    } else if (command.guildId === guildId) {
+    } else {
       const guildCommand = command.data.toJSON();
       guildCommand.guildId = guildId; // Add guildId property
       guildCommands.push(guildCommand);
@@ -88,11 +88,14 @@ module.exports = async function (client) {
     );
     console.log('All global commands:', allGlobalCommands);
 
-    // Fetch and display guild-specific commands for the specified guild
-    const allGuildCommands = await rest.get(
-      Routes.applicationGuildCommands(clientId, guildId)
-    );
-    console.log(`All guild-specific commands for guild ${guildId}:`, allGuildCommands);
+    // Fetch and display guild-specific commands for all guilds
+    for (const command of guildCommands) {
+      const guildId = command.guildId;
+      const allGuildCommands = await rest.get(
+        Routes.applicationGuildCommands(clientId, guildId)
+      );
+      console.log(`All guild-specific commands for guild ${guildId}:`, allGuildCommands);
+    }
   } catch (error) {
     console.error('Error while refreshing application (/) commands:', error);
   }
