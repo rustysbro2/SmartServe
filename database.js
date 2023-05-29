d11178d15b60c4b76d7ce1b212c4e7927bd05af5
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-29 14:44:24 -0400

    Update database.js

--- database.js
+++ database.js
@@ -1,15 +1,14 @@
-// database.js
 const mysql = require('mysql2');
 const { promisify } = require('util');
 
 const pool = mysql.createPool({
-    connectionLimit: 10, // Important: set limits to avoid full table scans
-    host: "localhost",
-    user: "rustysbro",
-    password: "Dincas2029@/",
-    database: "SmartServe"
+  connectionLimit: 10,
+  host: 'localhost',
+  user: 'your_username',
+  password: 'your_password',
+  database: 'your_database_name',
 });
 
-pool.query = promisify(pool.query);
+const query = promisify(pool.query).bind(pool);
 
-module.exports = pool;
+module.exports = query;
