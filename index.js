3103970ddeeb20b14fa935876da986ab8fc8a941
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-30 13:38:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const { ShardingManager } = require('discord.js');
 const { token } = require('./config.js');
 
-const shardCount = 4; // Specify the desired number of shards
-
-const manager = new ShardingManager('./bot.js', { token: token, totalShards: shardCount });
+const manager = new ShardingManager('./bot.js', { token: token });
 
 manager.on('shardCreate', shard => {
   console.log(`Launched shard ${shard.id}`);
