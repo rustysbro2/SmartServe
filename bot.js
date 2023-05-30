91e62055a405fa4dc05ab5598f59d7a8be7841ee
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-30 12:27:20 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,21 +23,45 @@ for (const file of commandFiles) {
   const command = require(`./commands/${file.endsWith('.js') ? file : file + '.js'}`);
   client.commands.set(command.data.name, command);
 
-  let category = commandCategories.find(category => category.name === command.category);
-  if (!category) {
-    category = {
-      name: command.category || 'Uncategorized',
-      description: '',
-      commands: []
-    };
-    commandCategories.push(category);
+  if (command.category) {
+    let category = commandCategories.find(category => category.name === command.category);
+    if (!category) {
+      category = {
+        name: command.category,
+        description: '',
+        commands: []
+      };
+      commandCategories.push(category);
+    }
+    category.commands.push({
+      name: command.data.name,
+      description: command.data.description
+    });
+  } else {
+    let commandFound = false;
+    for (const category of commandCategories) {
+      if (category.name === 'Uncategorized') {
+        category.commands.push({
+          name: command.data.name,
+          description: command.data.description
+        });
+        commandFound = true;
+        break;
+      }
+    }
+    if (!commandFound) {
+      const defaultCategory = {
+        name: 'Uncategorized',
+        description: 'Commands that do not belong to any specific category',
+        commands: [{
+          name: command.data.name,
+          description: command.data.description
+        }]
+      };
+      commandCategories.push(defaultCategory);
+    }
   }
-  category.commands.push({
-    name: command.data.name,
-    description: command.data.description
-  });
 }
-
 
 client.once('ready', async () => {
   console.log(`Shard ${client.shard.ids} logged in as ${client.user.tag}!`);
@@ -63,22 +87,4 @@ client.on('interactionCreate', async (interaction) => {
     if (commandName === 'help') {
       console.log('Executing help command...');
       await client.commands.get('help').execute(interaction, client);
-      console.log('Help command executed.');
-    } else {
-      const command = client.commands.get(commandName);
-
-      if (!command) return;
-
-      try {
-        console.log(`Executing ${commandName} command...`);
-        await command.execute(interaction, client);
-        console.log(`${commandName} command executed.`);
-      } catch (error) {
-        console.error(error);
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-      }
-    }
-  }
-});
-
-client.login(token);
+     
