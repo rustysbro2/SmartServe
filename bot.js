973b4e5a35dc1caf7f36c0e060524c5931d33819
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-30 15:58:26 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -69,7 +69,7 @@ client.on('interactionCreate', async (interaction) => {
 
   if (interaction.isStringSelectMenu() && interaction.customId === 'help_category') {
     console.log('Select menu interaction received:', interaction);
-    await helpCommand.handleSelectMenu(interaction, client, commandCategories);
+    await helpCommand.handleSelectMenu(interaction, commandCategories.commands);
   } else if (interaction.isCommand()) {
     const { commandName, guildId: interactionGuildId } = interaction;
 
@@ -77,7 +77,7 @@ client.on('interactionCreate', async (interaction) => {
 
     if (commandName === 'help') {
       console.log('Executing help command...');
-      await client.commands.get('help').execute(interaction, client, commandCategories, interaction.guildId);
+      await client.commands.get('help').execute(interaction, client, commandCategories.commands, interaction.guildId);
       console.log('Help command executed.');
     } else {
       const command = client.commands.get(commandName);
