315b2576066b296211407c86fcf1ffe2985dfb01
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-29 21:11:16 -0400

    Update slashCommands.js

--- slashCommands.js
+++ slashCommands.js
@@ -3,45 +3,25 @@ const { Routes } = require('discord-api-types/v10');
 const { clientId, token } = require('./config.js');
 const fs = require('fs');
 
-const commandsFolder = './commands';
-const rest = new REST({ version: '10' }).setToken(token);
+module.exports = async function (client) {
+  const commands = [];
+  const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-async function registerSlashCommands() {
+  for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+  }
+
+  const rest = new REST({ version: '10' }).setToken(token);
+
   try {
     console.log('Started refreshing application (/) commands.');
 
-    // Read command files from the commands folder
-    const commandFiles = fs.readdirSync(commandsFolder).filter((file) => file.endsWith('.js'));
-
-    // Fetch existing global commands
-    const existingGlobalCommands = await rest.get(Routes.applicationCommands(clientId));
-
-    // Filter out existing commands that are not in the command files
-    const globalCommandsToDelete = existingGlobalCommands.filter(
-      (existingCommand) => !commandFiles.includes(`${existingCommand.name}.js`)
-    );
-
-    // Create the new global commands
-    const globalCommandsToUpdate = commandFiles
-      .filter((file) => !existingGlobalCommands.some((command) => command.name === file.replace('.js', '')))
-      .map((file) => require(`./commands/${file}`).data.toJSON());
-
-    // Delete the unnecessary global commands
-    for (const command of globalCommandsToDelete) {
-      await rest.delete(Routes.applicationCommand(clientId, command.id));
-      console.log(`Deleted global command: ${command.name}`);
-    }
-
-    // Register or update the global commands
-    for (const command of globalCommandsToUpdate) {
-      await rest.post(Routes.applicationCommands(clientId), { body: command });
-      console.log(`Registered or updated global command: ${command.name}`);
-    }
+    // Register the commands globally
+    await rest.put(Routes.applicationCommands(clientId), { body: commands });
 
     console.log('Successfully refreshed application (/) commands.');
   } catch (error) {
     console.error('Error refreshing application (/) commands:', error);
   }
-}
-
-registerSlashCommands();
+};
