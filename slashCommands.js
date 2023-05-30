f51073468ca010632c0f60d912856b9ac5e3686b
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-29 21:39:27 -0400

    Update slashCommands.js

--- slashCommands.js
+++ slashCommands.js
@@ -1,14 +1,40 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v10');
-const { clientId, token } = require('./config.js');
+const { clientId, guildId, token } = require('./config.js');
 const fs = require('fs');
+const { pool } = require('./database.js');
+
+async function createCommandIdsTable() {
+  const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS commandIds (
+      commandName VARCHAR(255),
+      commandId VARCHAR(255),
+      lastModified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+      PRIMARY KEY (commandName)
+    )
+  `;
+
+  try {
+    await pool.promise().query(createTableQuery);
+    console.log('CommandIds table created or already exists.');
+  } catch (error) {
+    console.error('Error creating commandIds table:', error);
+  }
+}
 
 async function updateCommandData(commands) {
   try {
-    // Update the command objects with null commandId
-    commands.forEach((command) => {
-      command.commandId = null;
-    });
+    for (const command of commands) {
+      const { commandName, commandId, lastModified } = command;
+
+      const insertUpdateQuery = `
+        INSERT INTO commandIds (commandName, commandId, lastModified)
+        VALUES (?, ?, ?)
+        ON DUPLICATE KEY UPDATE commandId = ?, lastModified = ?
+      `;
+
+      await pool.promise().query(insertUpdateQuery, [commandName, commandId, lastModified, commandId, lastModified]);
+    }
 
     console.log('Command data updated successfully.');
   } catch (error) {
@@ -17,6 +43,9 @@ async function updateCommandData(commands) {
 }
 
 module.exports = async function (client) {
+  // Create the commandIds table if it doesn't exist
+  await createCommandIdsTable();
+
   const commands = [];
 
   // Read command files from the commands directory
@@ -26,9 +55,9 @@ module.exports = async function (client) {
   for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     const commandData = {
-      name: command.data.name,
-      description: command.data.description,
-      options: command.data.options ? command.data.options.toJSON() : [],
+      commandName: command.data.name,
+      commandId: null,
+      lastModified: fs.statSync(`./commands/${file}`).mtime,
     };
 
     // Add the command data to the commands array
@@ -40,7 +69,7 @@ module.exports = async function (client) {
   try {
     console.log('Started refreshing application (/) commands.');
 
-    // Update the command data
+    // Update the command data in the table
     await updateCommandData(commands);
 
     // Register the global slash commands
