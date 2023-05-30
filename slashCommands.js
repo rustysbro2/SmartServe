822688ca6defa92fca5e4886d70194745b417119
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-29 21:13:11 -0400

    Update slashCommands.js

--- slashCommands.js
+++ slashCommands.js
@@ -1,15 +1,43 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v10');
-const { clientId, token } = require('./config.js');
+const { clientId, guildId, token } = require('./config.js');
 const fs = require('fs');
+const { query } = require('./database.js');
+
+// Function to insert or update a command in the database
+async function upsertCommand(commandName, commandId, options) {
+  const queryStr = `
+    INSERT INTO commandIds (commandName, commandId, options)
+    VALUES (?, ?, ?)
+    ON DUPLICATE KEY UPDATE
+    commandId = VALUES(commandId),
+    options = VALUES(options)
+  `;
+
+  try {
+    await query(queryStr, [commandName, commandId, JSON.stringify(options)]);
+    console.log(`Command '${commandName}' upserted successfully.`);
+  } catch (error) {
+    console.error(`Error upserting command '${commandName}':`, error);
+  }
+}
 
 module.exports = async function (client) {
   const commands = [];
-  const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+  // Read command files from the commands directory
+  const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
+
+  // Loop through command files and register slash commands
   for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
-    commands.push(command.data.toJSON());
+    const commandData = command.data.toJSON();
+
+    // Add the command data to the commands array
+    commands.push(commandData);
+
+    // Upsert the command in the database
+    await upsertCommand(commandData.name, '', commandData.options || []);
   }
 
   const rest = new REST({ version: '10' }).setToken(token);
@@ -17,8 +45,10 @@ module.exports = async function (client) {
   try {
     console.log('Started refreshing application (/) commands.');
 
-    // Register the commands globally
-    await rest.put(Routes.applicationCommands(clientId), { body: commands });
+    // Register the global slash commands
+    await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+      body: commands,
+    });
 
     console.log('Successfully refreshed application (/) commands.');
   } catch (error) {
