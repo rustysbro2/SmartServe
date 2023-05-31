02010bafcf6810978c4033597b086fb469cadfc6
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-31 15:38:05 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,12 +5,17 @@ module.exports = {
     .setName('ping')
     .setDescription('Replies with the current ping of the bot'),
 
-  execute(interaction) {
-    interaction.reply(`Pong! Ping: ${interaction.client.ws.ping}ms`);
+  async execute(interaction) {
+    try {
+      const reply = await interaction.deferReply({ ephemeral: true });
+      const ping = reply.createdTimestamp - interaction.createdTimestamp;
+      await interaction.editReply(`Pong! Bot Latency: ${ping}ms, API Latency: ${Math.round(interaction.client.ws.ping)}ms`);
+    } catch (error) {
+      console.error('Error replying to ping command:', error);
+    }
   },
 
   global: false, // Command is not global (optional, as it defaults to false)
   category: 'General', // Specify the category
   categoryDescription: 'Commands related to general functionality', // Specify the category description
-
 };
