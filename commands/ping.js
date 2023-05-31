7b657c5c1c811d0999236eb465e1fc9b2577ffb0
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-31 16:56:34 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,21 +1,122 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, EmbedBuilder } = require('discord.js');
+const { guildId } = require('../config.js');
+
+async function handleSelectMenu(interaction, commandCategories) {
+  console.log('Interaction Guild ID:', interaction.guildId);
+
+  const selectedCategory = interaction.values[0];
+  const category = commandCategories.find(
+    (category) =>
+      category.name.toLowerCase().replace(/\s/g, '_') === selectedCategory
+  );
+
+  console.log('Command Categories:');
+  commandCategories.forEach((category) => {
+    console.log(`Category: ${category.name}`);
+    console.log(`Guild ID: ${category.guildId}`);
+    console.log('Commands:', category.commands);
+  });
+
+  let categoryEmbed;
+
+  if (category) {
+    categoryEmbed = new EmbedBuilder();
+    categoryEmbed.setTitle(`Commands - ${category.name}`);
+
+    if (category.categoryDescription && category.categoryDescription.length > 0) {
+      categoryEmbed.setDescription(category.categoryDescription);
+    }
+
+    const guildSpecificCommands = category.commands.filter(
+      (command) => command.guildId === interaction.guildId
+    );
+    const globalCommands = category.commands.filter(
+      (command) => command.global !== false
+    );
+
+    console.log('Guild Specific Commands:');
+    guildSpecificCommands.forEach((command) => {
+      console.log(`Command: ${command.name}`);
+      console.log(`Category: ${category.name}`);
+      console.log(`Global: ${command.global}`);
+    });
+
+    console.log('Global Commands:');
+    globalCommands.forEach((command) => {
+      console.log(`Command: ${command.name}`);
+      console.log(`Category: ${category.name}`);
+      console.log(`Global: ${command.global}`);
+    });
+
+    const commandsToShow = guildSpecificCommands.length > 0 ? guildSpecificCommands : globalCommands;
+
+    commandsToShow.forEach((command) => {
+      categoryEmbed.addFields([{ name: command.name, value: command.description }]);
+    });
+
+    console.log('Category Embed:', categoryEmbed);
+
+    try {
+      if (interaction.message) {
+        const actionRow = new ActionRowBuilder().addComponents(...interaction.message.components[0].components);
+        await interaction.deferUpdate();
+        await interaction.message.edit({ embeds: [categoryEmbed], components: [actionRow] });
+      } else {
+        console.error('Interaction does not have a message.');
+      }
+    } catch (error) {
+      console.error('Error deferring or editing interaction:', error);
+    }
+  } else {
+    console.error(`Category '${selectedCategory}' not found.`);
+    return;
+  }
+}
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('ping')
-    .setDescription('Replies with the current ping of the bot'),
+    .setName('help')
+    .setDescription('List all commands or info about a specific command'),
 
-  async execute(interaction) {
+  async execute(interaction, client, commandCategories) {
+    console.log('Guild ID from config.js:', guildId);
+    if (interaction.deferred || interaction.replied) {
+      console.log('Interaction already deferred or replied to.');
+      return;
+    }
+
+    const isGlobal = !guildId || (interaction.guildId && interaction.guildId === guildId);
+
+    const filteredCommandCategories = commandCategories
+      .filter((category) => isGlobal ? !category.guildId : category.guildId === interaction.guildId)
+      .slice(0, 10);
+
+    const usedOptionValues = new Set();
+
+    const selectMenu = new StringSelectMenuBuilder()
+      .setCustomId('help_category')
+      .setPlaceholder('Select a category');
+
+    filteredCommandCategories.forEach((category) => {
+      if (category.commands.some((command) => command.global !== false || (isGlobal && command.guildId === guildId))) {
+        const categoryName = category.name.toLowerCase().replace(/\s/g, '_');
+        selectMenu.addOptions(
+          new StringSelectMenuOptionBuilder()
+            .setLabel(category.name)
+            .setValue(categoryName)
+            .setDescription(category.description)
+        );
+        usedOptionValues.add(categoryName);
+      }
+    });
+
+    const actionRow = new ActionRowBuilder().addComponents(selectMenu);
+
     try {
-      const reply = await interaction.deferReply({ ephemeral: true });
-      const ping = reply.createdTimestamp - interaction.createdTimestamp;
-      await interaction.editReply(`Pong! Bot Latency: ${ping}ms, API Latency: ${Math.round(interaction.client.ws.ping)}ms`);
+      await interaction.reply({ content: 'Please select a category:', components: [actionRow] });
     } catch (error) {
-      console.error('Error replying to ping command:', error);
+      console.error('Error replying to interaction:', error);
     }
   },
-
-  global: false, // Command is not global (optional, as it defaults to false)
-  category: 'General', // Specify the category
-  categoryDescription: 'Commands related to general functionality', // Specify the category description
+  handleSelectMenu,
 };
