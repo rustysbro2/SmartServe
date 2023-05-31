598bc0d7bc05188c0bd468d6c88fd5942d2dc91d
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-31 15:00:19 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -3,14 +3,12 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('ping')
-    .setDescription('Replies with the current ping of the bot'),
+    .setDescription('Replies with the current ping of the bot')
+    .addGlobalOption() // Set the command as global
+    .addCategory('General') // Set the category name
+    .addCategoryDescription('Commands related to general functionality'), // Set the category description
 
   execute(interaction) {
     interaction.reply(`Pong! Ping: ${interaction.client.ws.ping}ms`);
-  },
-
-  global: false, // Command is not global (optional, as it defaults to false)
-  category: 'General', // Specify the category
-  categoryDescription: 'Commands related to general functionality', // Specify the category description
-
+  }
 };
