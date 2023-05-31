305339420a48d258192f13ca35394160b5c702b8
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-31 08:31:33 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,8 +2,16 @@ const { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder, StringSe
 const { guildId } = require('../config.js');
 
 async function handleSelectMenu(interaction, commandCategories) {
+  console.log('Select menu interaction received:', interaction);
+
   const selectedCategory = interaction.values[0];
-  const category = commandCategories.find((category) => category.name.toLowerCase().replace(/\s/g, '_') === selectedCategory);
+
+  console.log('Selected category:', selectedCategory);
+
+  const category = commandCategories.find(
+    (category) =>
+      category.name.toLowerCase().replace(/\s/g, '_') === selectedCategory
+  );
 
   if (category) {
     console.log('Category found:', category.name);
@@ -42,6 +50,8 @@ module.exports = {
     .setDescription('List all commands or info about a specific command'),
 
   async execute(interaction, client, commandCategories, guildId) {
+    console.log('Help command interaction received:', interaction);
+
     if (interaction.deferred || interaction.replied) {
       console.log('Interaction already deferred or replied to.');
       return;
@@ -59,30 +69,27 @@ module.exports = {
       .setCustomId('help_category')
       .setPlaceholder('Select a category');
 
+    // Add options to the select menu
     filteredCommandCategories.forEach((category) => {
-      category.commands.forEach((command) => {
-        if (command.global !== false) {
-          const optionBuilder = new StringSelectMenuOptionBuilder()
-            .setLabel(command.name)
-            .setValue(generateUniqueOptionValue(command.name));
+      const optionBuilder = new StringSelectMenuOptionBuilder()
+        .setLabel(category.name)
+        .setValue(generateUniqueOptionValue(category.name));
 
-          if (command.description && command.description.length > 0) {
-            optionBuilder.setDescription(command.description);
-          }
+      if (category.description && category.description.length > 0) {
+        optionBuilder.setDescription(category.description);
+      }
 
-          selectMenu.addOptions(optionBuilder);
-        }
-      });
+      selectMenu.addOptions(optionBuilder);
     });
 
-    function generateUniqueOptionValue(commandName) {
-      const sanitizedCommandName = commandName.toLowerCase().replace(/\s/g, '_');
+    function generateUniqueOptionValue(categoryName) {
+      const sanitizedCategoryName = categoryName.toLowerCase().replace(/\s/g, '_');
 
-      let optionValue = sanitizedCommandName;
+      let optionValue = sanitizedCategoryName;
       let index = 1;
 
       while (usedOptionValues.has(optionValue)) {
-        optionValue = `${sanitizedCommandName}_${index}`;
+        optionValue = `${sanitizedCategoryName}_${index}`;
         index++;
       }
 
