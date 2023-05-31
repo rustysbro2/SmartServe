4c828a6e64b92b8cd8ab83f7133c51d2e84e2df2
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-31 17:22:45 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,9 @@
 const { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, EmbedBuilder } = require('discord.js');
 const { guildId } = require('../config.js');
 
+const { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, EmbedBuilder } = require('discord.js');
+const { guildId } = require('../config.js');
+
 async function handleSelectMenu(interaction, commandCategories, guildId) {
   console.log('Interaction Guild ID:', interaction.guildId);
 
@@ -61,6 +64,56 @@ async function handleSelectMenu(interaction, commandCategories, guildId) {
     return;
   }
 }
+
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('help')
+    .setDescription('List all commands or info about a specific command'),
+
+  async execute(interaction, client, commandCategories, guildId) {
+    console.log('Guild ID from config.js:', guildId);
+    if (interaction.deferred || interaction.replied) {
+      console.log('Interaction already deferred or replied to.');
+      return;
+    }
+
+    const isGlobal = !guildId || (interaction.guildId && interaction.guildId === guildId);
+
+    const filteredCommandCategories = commandCategories
+      .filter((category) => isGlobal ? !category.guildId : category.guildId === interaction.guildId)
+      .slice(0, 10);
+
+    const usedOptionValues = new Set();
+
+    const selectMenu = new StringSelectMenuBuilder()
+      .setCustomId('help_category')
+      .setPlaceholder('Select a category');
+
+    filteredCommandCategories.forEach((category) => {
+      if (category.commands.some((command) => command.global !== false || command.guildId === undefined)) {
+        const categoryName = category.name.toLowerCase().replace(/\s/g, '_');
+        selectMenu.addOptions(
+          new StringSelectMenuOptionBuilder()
+            .setLabel(category.name)
+            .setValue(categoryName)
+            .setDescription(category.categoryDescription) // Updated line
+        );
+        usedOptionValues.add(categoryName);
+      }
+    });
+
+    const actionRow = new ActionRowBuilder().addComponents(selectMenu);
+
+    try {
+      await interaction.reply({ content: 'Please select a category:', components: [actionRow] });
+    } catch (error) {
+      console.error('Error replying to interaction:', error);
+    }
+  },
+  handleSelectMenu,
+};
+
 
 
 module.exports = {
