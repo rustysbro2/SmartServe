a47d58c443ce165785b7258e261013ad371b7b5a
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-05-31 15:02:06 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,6 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('ping')
     .setDescription('Replies with the current ping of the bot')
-    .setGlobal(true) // Command is global
     .setCategory('General') // Specify the category
     .setDescription('Commands related to general functionality'), // Specify the category description
 
