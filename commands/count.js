c6224d8f68a5d23810afb4cbf737d59497b7d98c
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-01 11:28:37 -0400

    Create count.js

---
+++ commands/count.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('discord.js');
+const countingGame = require('../features/countingGame');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('count')
+    .setDescription('Increment the count by one'),
+
+  async execute(interaction) {
+    try {
+      countingGame.incrementCount();
+      const count = countingGame.getCount();
+      await interaction.reply(`Count: ${count}`);
+    } catch (error) {
+      console.error('Error executing count command:', error);
+    }
+  }
+};
