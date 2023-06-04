4fe4e959f45e0ffcb6f6346089a30d549dba0bcd
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-04 18:17:18 -0400

    Update setStrikeChannel.js

--- commands/setStrikeChannel.js
+++ commands/setStrikeChannel.js
@@ -1,23 +1,22 @@
-// commands/setStrikeChannel.js
-
 const { SlashCommandBuilder } = require('discord.js');
 const { setStrikeChannel } = require('../features/strikeFeature');
+const pool = require('../database');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('setstrikechannel')
-    .setDescription('Set the strike channel for strikes logging')
-    .addChannelOption(option => option.setName('channel').setDescription('Select a channel').setRequired(true)),
+    .setDescription('Set the strike channel')
+    .addChannelOption(option =>
+      option.setName('channel')
+        .setDescription('Select the strike channel')
+        .setRequired(true)
+    ),
   async execute(interaction) {
-    const channel = interaction.options.getChannel('channel');
+    const channelId = interaction.options.getChannel('channel').id;
     const guildId = interaction.guild.id;
 
-    try {
-      await setStrikeChannel(guildId, channel.id);
-      await interaction.reply(`Strike channel set to ${channel} successfully.`);
-    } catch (error) {
-      console.error('Error setting strike channel:', error);
-      await interaction.reply('Failed to set the strike channel.');
-    }
+    setStrikeChannel(pool, guildId, channelId);
+
+    await interaction.reply(`Strike channel has been set to <#${channelId}>.`);
   },
 };
