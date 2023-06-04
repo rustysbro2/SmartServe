c984823a7ce7054ca078540d22ba36fda53da580
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-04 18:07:34 -0400

    Update strike.js

--- commands/strike.js
+++ commands/strike.js
@@ -1,29 +1,26 @@
-// commands/strike.js
-
 const { SlashCommandBuilder } = require('discord.js');
-const strikeFeature = require('../features/strikeFeature');
+const { addStrike } = require('../features/strikeFeature');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('strike')
-    .setDescription('Manage strikes for a user')
-    .addSubcommand(subcommand =>
-      subcommand
-        .setName('manage')
-        .setDescription('Manage strikes for a user')
-        .addUserOption(option =>
-          option.setName('user').setDescription('Select the user').setRequired(true)
-        )
-        .addStringOption(option =>
-          option.setName('action').setDescription('Select the action').setRequired(true)
-            .addChoice('Add', 'add')
-            .addChoice('Remove', 'remove')
-        )
-        .addStringOption(option =>
-          option.setName('reason').setDescription('Enter the reason for the strike').setRequired(true)
-        )
+    .setDescription('Strike a user')
+    .addUserOption(option =>
+      option.setName('user').setDescription('Select a user').setRequired(true)
+    )
+    .addStringOption(option =>
+      option
+        .setName('reason')
+        .setDescription('Enter the reason for the strike')
+        .setRequired(true)
     ),
   async execute(interaction) {
-    await strikeFeature.execute(interaction.client, interaction);
+    const user = interaction.options.getUser('user');
+    const reason = interaction.options.getString('reason');
+
+    // Call the addStrike function from the strikeFeature
+    await addStrike(user.id, reason);
+
+    await interaction.reply(`User ${user.tag} has been struck with reason: ${reason}`);
   },
 };
