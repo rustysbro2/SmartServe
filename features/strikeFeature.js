59b4e6301b9f83e6a71471b1eea9c1169ee9162a
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-04 18:03:07 -0400

    Update strikeFeature.js

--- features/strikeFeature.js
+++ features/strikeFeature.js
@@ -1,88 +1,42 @@
 // features/strikeFeature.js
 
-const { db } = require('../database');
+const database = require('../database');
 
-module.exports = {
-  execute(client, interaction) {
-    const subcommand = interaction.options.getSubcommand();
+module.exports.execute = async (client, interaction) => {
+  const subcommand = interaction.options.getSubcommand();
+  const user = interaction.options.getUser('user');
+  const action = interaction.options.getString('action');
+  const reason = interaction.options.getString('reason');
 
-    if (subcommand === 'add') {
-      const user = interaction.options.getUser('user');
-      const reason = interaction.options.getString('reason');
-      addStrike(user.id, reason)
-        .then(() => {
-          interaction.reply(`Strike added for user ${user.username} with reason: ${reason}`);
-        })
-        .catch((error) => {
-          console.error('Error adding strike:', error);
-          interaction.reply('Failed to add strike. Please try again.');
-        });
-    } else if (subcommand === 'remove') {
-      const user = interaction.options.getUser('user');
-      removeStrike(user.id)
-        .then(() => {
-          interaction.reply(`Strike removed for user ${user.username}`);
-        })
-        .catch((error) => {
-          console.error('Error removing strike:', error);
-          interaction.reply('Failed to remove strike. Please try again.');
-        });
-    } else if (subcommand === 'list') {
-      const user = interaction.options.getUser('user');
-      listStrikes(user.id)
-        .then((strikes) => {
-          interaction.reply(`Strikes for user ${user.username}: ${strikes.length}`);
-        })
-        .catch((error) => {
-          console.error('Error listing strikes:', error);
-          interaction.reply('Failed to list strikes. Please try again.');
-        });
+  if (subcommand === 'manage') {
+    if (action === 'add') {
+      try {
+        // Add a strike for the user
+        await addStrike(user.id, reason);
+        await interaction.reply(`${user.username} has been given a strike for: ${reason}`);
+      } catch (error) {
+        console.error('Error adding strike:', error);
+        await interaction.reply('An error occurred while adding a strike.');
+      }
+    } else if (action === 'remove') {
+      try {
+        // Remove a strike for the user
+        await removeStrike(user.id);
+        await interaction.reply(`${user.username}'s strike has been removed.`);
+      } catch (error) {
+        console.error('Error removing strike:', error);
+        await interaction.reply('An error occurred while removing a strike.');
+      }
     }
-  },
+  }
 };
 
 async function addStrike(userId, reason) {
-  try {
-    await createStrikesTableIfNotExists();
-
-    const query = 'INSERT INTO strikes (user_id, reason) VALUES (?, ?)';
-    await db.query(query, [userId, reason]);
-  } catch (error) {
-    throw error;
-  }
+  const query = 'INSERT INTO strikes (user_id, reason) VALUES (?, ?)';
+  await database.query(query, [userId, reason]);
 }
 
 async function removeStrike(userId) {
-  try {
-    await createStrikesTableIfNotExists();
-
-    const query = 'DELETE FROM strikes WHERE user_id = ? ORDER BY id DESC LIMIT 1';
-    await db.query(query, [userId]);
-  } catch (error) {
-    throw error;
-  }
-}
-
-async function listStrikes(userId) {
-  try {
-    await createStrikesTableIfNotExists();
-
-    const query = 'SELECT * FROM strikes WHERE user_id = ?';
-    const [rows] = await db.query(query, [userId]);
-    return rows;
-  } catch (error) {
-    throw error;
-  }
-}
-
-async function createStrikesTableIfNotExists() {
-  const query = `
-    CREATE TABLE IF NOT EXISTS strikes (
-      id INT AUTO_INCREMENT PRIMARY KEY,
-      user_id VARCHAR(255) NOT NULL,
-      reason VARCHAR(255) NOT NULL,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    )
-  `;
-  await db.query(query);
+  const query = 'DELETE FROM strikes WHERE user_id = ? LIMIT 1';
+  await database.query(query, [userId]);
 }
