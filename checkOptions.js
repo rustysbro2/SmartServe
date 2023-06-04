a057ec0f32ebe5b3fbcb05ab4ef4cabec231e037
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-04 14:14:56 -0400

    Update checkOptions.js

--- checkOptions.js
+++ checkOptions.js
@@ -1,24 +1,21 @@
-require('dotenv').config();
 const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
+const { Routes } = require('discord-api-types/v10');
 const { clientId, guildId, token } = require('./config.js');
 
-const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({ version: '10' }).setToken(token);
 
 (async () => {
   try {
     console.log('Fetching slash commands...');
-    const commands = await rest.get(
-      Routes.applicationGuildCommands(clientId, guildId)
-    );
+    const commands = await rest.get(Routes.applicationGuildCommands(clientId, guildId));
     console.log('Slash commands:', commands);
 
     console.log('Command Options:');
-    commands.forEach((command) => {
+    for (const command of commands) {
       console.log('Command:', command.name);
       console.log('Options:', command.options);
-    });
+    }
   } catch (error) {
-    console.error('Error fetching slash commands:', error);
+    console.error('Error fetching slash commands:', error.message);
   }
 })();
