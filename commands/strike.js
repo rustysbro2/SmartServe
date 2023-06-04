bf6d0f52fe4f1ba459939f993ec6bf74bd347481
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-04 18:20:49 -0400

    Update strike.js

--- commands/strike.js
+++ commands/strike.js
@@ -1,25 +1,33 @@
-// commands/strike.js
-
-const { SlashCommandBuilder } = require('discord.js');
-const db = require('../database');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { logStrike, getStrikes } = require('../features/strikeFeature');
+const pool = require('../database');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('strike')
     .setDescription('Strike a user')
-    .addUserOption(option => option.setName('user').setDescription('Select a user').setRequired(true))
-    .addStringOption(option => option.setName('reason').setDescription('Enter the reason for the strike').setRequired(true)),
+    .addUserOption(option =>
+      option.setName('user').setDescription('Select a user').setRequired(true)
+    )
+    .addStringOption(option =>
+      option.setName('reason').setDescription('Enter the reason for the strike').setRequired(true)
+    ),
   async execute(interaction) {
     const user = interaction.options.getUser('user');
     const reason = interaction.options.getString('reason');
     const guildId = interaction.guild.id;
 
-    try {
-      await db.query('INSERT INTO strikes (guild_id, user_id, reason) VALUES (?, ?, ?)', [guildId, user.id, reason]);
-      await interaction.reply(`User ${user.tag} has been struck with reason: ${reason}`);
-    } catch (error) {
-      console.error('Error striking user:', error);
-      await interaction.reply('Failed to strike the user.');
-    }
+    logStrike(pool, guildId, user.id, reason);
+
+    const strikeCount = await getStrikes(pool, guildId, user.id);
+
+    const embed = new EmbedBuilder()
+      .setColor('#FF0000')
+      .setTitle('Strike Added')
+      .setDescription(`User: ${user.tag}\nReason: ${reason}\nTotal Strikes: ${strikeCount}`)
+      .setTimestamp()
+      .build();
+
+    await interaction.reply({ embeds: [embed] });
   },
 };
