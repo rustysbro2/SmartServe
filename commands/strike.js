4892c01afd9ac27618fac3877ca75b715b2ef77c
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-04 18:02:33 -0400

    Update strike.js

--- commands/strike.js
+++ commands/strike.js
@@ -9,29 +9,18 @@ module.exports = {
     .setDescription('Manage strikes for a user')
     .addSubcommand(subcommand =>
       subcommand
-        .setName('add')
-        .setDescription('Add a strike to a user')
+        .setName('manage')
+        .setDescription('Manage strikes for a user')
         .addUserOption(option =>
           option.setName('user').setDescription('Select the user').setRequired(true)
         )
         .addStringOption(option =>
+          option.setName('action').setDescription('Select the action').setRequired(true)
+            .addChoice('Add', 'add')
+            .addChoice('Remove', 'remove')
+        )
+        .addStringOption(option =>
           option.setName('reason').setDescription('Enter the reason for the strike').setRequired(true)
-        )
-    )
-    .addSubcommand(subcommand =>
-      subcommand
-        .setName('remove')
-        .setDescription('Remove the latest strike from a user')
-        .addUserOption(option =>
-          option.setName('user').setDescription('Select the user').setRequired(true)
-        )
-    )
-    .addSubcommand(subcommand =>
-      subcommand
-        .setName('list')
-        .setDescription('List all strikes of a user')
-        .addUserOption(option =>
-          option.setName('user').setDescription('Select the user').setRequired(true)
         )
     ),
   async execute(interaction) {
