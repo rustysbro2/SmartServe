bfd49e3a57d785729fde77b7083b010d1d6e718f
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-04 18:09:29 -0400

    Update strikeFeature.js

--- features/strikeFeature.js
+++ features/strikeFeature.js
@@ -1,25 +1,36 @@
-const pool = require('../database');
-
-async function addStrike(userId, reason) {
-  try {
-    const createTableQuery = `
-      CREATE TABLE IF NOT EXISTS strikes (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        user_id VARCHAR(255) NOT NULL,
-        reason VARCHAR(255) NOT NULL,
-        timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-      )
-    `;
-    await pool.query(createTableQuery);
-
-    const insertQuery = 'INSERT INTO strikes (user_id, reason) VALUES (?, ?)';
-    await pool.query(insertQuery, [userId, reason]);
-    console.log(`Added strike for user with ID: ${userId}`);
-  } catch (error) {
-    console.error('Error adding strike:', error);
-  }
-}
+const { SlashCommandBuilder } = require('discord.js');
+const { setStrikeChannel, getStrikes } = require('../features/strikeFeature');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-  addStrike,
+  data: new SlashCommandBuilder()
+    .setName('setstrikechannel')
+    .setDescription('Set the strike channel')
+    .addChannelOption(option =>
+      option.setName('channel')
+        .setDescription('Select the strike channel')
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const channel = interaction.options.getChannel('channel');
+    const guildId = interaction.guild.id;
+
+    // Set the strike channel
+    setStrikeChannel(guildId, channel.id);
+
+    // Fetch the strikes and update the embed
+    const strikes = await getStrikes(guildId);
+
+    const embed = new MessageEmbed()
+      .setTitle('Strike List')
+      .setDescription('List of players with their strike counts');
+
+    strikes.forEach(strike => {
+      embed.addField(strike.userTag, `Strikes: ${strike.count}`, true);
+    });
+
+    channel.send({ embeds: [embed] });
+
+    await interaction.reply('Strike channel has been set successfully.');
+  },
 };
