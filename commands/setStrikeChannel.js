8288dc56bdec0817fbbf729b849e33fbe89c3bbf
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-04 19:15:28 -0400

    Update setStrikeChannel.js

--- commands/setStrikeChannel.js
+++ commands/setStrikeChannel.js
@@ -1,40 +1,30 @@
-// commands/setStrikeChannel.js
+const { setStrikeChannel } = require('../strikeFeature');
 
-const { SlashCommandBuilder } = require('discord.js');
-const pool = require('../database.js');
-const { EmbedBuilder } = require('discord.js');
+async function execute(interaction, pool) {
+  const guildId = interaction.guildId;
+  const channelId = interaction.options.getChannel('channel').id;
+
+  try {
+    await setStrikeChannel(pool, guildId, channelId);
+    await interaction.reply('Strike channel has been set successfully.');
+  } catch (error) {
+    console.error('Error setting strike channel:', error);
+    await interaction.reply('An error occurred while setting the strike channel.');
+  }
+}
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('setstrikechannel')
-    .setDescription('Set the strike channel')
-    .addChannelOption(option =>
-      option.setName('channel')
-        .setDescription('Select a channel')
-        .setRequired(true)
-    ),
-  async execute(interaction) {
-    const channelId = interaction.options.getChannel('channel').id;
-    const guildId = interaction.guild.id;
-
-    try {
-      // Update the strike channel in the database
-      await pool.query(
-        'INSERT INTO strike_channels (guild_id, channel_id) VALUES (?, ?) ON DUPLICATE KEY UPDATE channel_id = ?',
-        [guildId, channelId, channelId]
-      );
-
-      // Create and send the success embed
-      const embed = new EmbedBuilder()
-        .setColor(0x00FF00)
-        .setTitle('Strike Channel Set')
-        .setDescription(`Strike channel has been set to <#${channelId}>.`)
-        .setTimestamp();
-
-      await interaction.reply({ embeds: [embed] });
-    } catch (error) {
-      console.error('Error setting strike channel:', error);
-      await interaction.reply('Error setting strike channel. Please try again later.');
-    }
+  data: {
+    name: 'setstrikechannel',
+    description: 'Set the channel where strikes will be logged',
+    options: [
+      {
+        name: 'channel',
+        description: 'The channel to set as the strike channel',
+        type: 'CHANNEL',
+        required: true,
+      },
+    ],
   },
+  execute,
 };
