6b5343296b2035899465e6fbb30b5cafb96f9acf
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-04 19:57:57 -0400

    Update strike.js

--- commands/strike.js
+++ commands/strike.js
@@ -1,41 +1,34 @@
+const { SlashCommandBuilder } = require('discord.js');
 const { logStrike } = require('../features/strikeFeature');
 const pool = require('../database');
 
-async function execute(interaction, pool) {
-  const guildId = interaction.guildId;
-  const userId = interaction.options.getUser('user').id;
-  const reason = interaction.options.getString('reason');
-
-  console.log('Logging strike for user:', userId);
-
-  try {
-    await logStrike(pool, guildId, userId, reason);
-    await interaction.reply(`Strike logged for user <@${userId}>. Reason: ${reason}`);
-  } catch (error) {
-    console.error('Error logging strike:', error);
-    await interaction.reply('An error occurred while logging the strike.');
-  }
-}
-
-
 module.exports = {
-  data: {
-    name: 'strike',
-    description: 'Log a strike for a user',
-    options: [
-      {
-        name: 'user',
-        description: 'The user to strike',
-        type: 6,
-        required: true,
-      },
-      {
-        name: 'reason',
-        description: 'The reason for the strike',
-        type: 3,
-        required: true,
-      },
-    ],
+  data: new SlashCommandBuilder()
+    .setName('strike')
+    .setDescription('Log a strike for a user')
+    .addUserOption((option) =>
+      option
+        .setName('user')
+        .setDescription('The user to strike')
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName('reason')
+        .setDescription('The reason for the strike')
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const guildId = interaction.guildId;
+    const userId = interaction.options.getUser('user').id;
+    const reason = interaction.options.getString('reason');
+
+    try {
+      await logStrike(pool, guildId, userId, reason);
+      await interaction.reply(`Strike logged for user <@${userId}>. Reason: ${reason}`);
+    } catch (error) {
+      console.error('Error logging strike:', error);
+      await interaction.reply('An error occurred while logging the strike.');
+    }
   },
-  execute,
 };
