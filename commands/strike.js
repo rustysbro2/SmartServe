afcd87e283908cb3ff923cd81d492cb2274cafe9
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-04 19:53:27 -0400

    Update strike.js

--- commands/strike.js
+++ commands/strike.js
@@ -1,21 +1,22 @@
 const { logStrike } = require('../features/strikeFeature');
 const pool = require('../database');
 
-async function execute(interaction) {
+async function execute(interaction, pool) {
   const guildId = interaction.guildId;
   const userId = interaction.options.getUser('user').id;
   const reason = interaction.options.getString('reason');
 
+  console.log('Logging strike for user:', userId);
+
   try {
-    console.log(`Logging strike for user: ${userId}`);
     await logStrike(pool, guildId, userId, reason);
-    console.log(`Strike logged for user: ${userId}`);
     await interaction.reply(`Strike logged for user <@${userId}>. Reason: ${reason}`);
   } catch (error) {
     console.error('Error logging strike:', error);
     await interaction.reply('An error occurred while logging the strike.');
   }
 }
+
 
 module.exports = {
   data: {
