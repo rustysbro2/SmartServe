bf1f4768161fb49a271d779e9c3ec0ce8d78b5bf
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-04 14:03:01 -0400

    Update checkOptions.js

--- checkOptions.js
+++ checkOptions.js
@@ -5,17 +5,20 @@ const rest = new REST({ version: '10' }).setToken(token);
 
 const fetchCommands = async () => {
   try {
-    const guildCommands = await rest.get(
-      Routes.applicationGuildCommands(clientId, guildId)
-    );
-    console.log('Guild Commands:');
-    console.log(guildCommands);
-
     const globalCommands = await rest.get(
       Routes.applicationCommands(clientId)
     );
     console.log('Global Commands:');
     console.log(globalCommands);
+
+    console.log('Global Command Options:');
+    for (const command of globalCommands) {
+      const commandDetails = await rest.get(
+        Routes.applicationCommand(clientId, command.id)
+      );
+      console.log('Command:', command.name);
+      console.log('Options:', commandDetails.options);
+    }
   } catch (error) {
     console.log(`There was an error: ${error}`);
   }
