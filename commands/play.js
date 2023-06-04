f4842517b38b13b3002af995c01e868ded07790b
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-04 14:04:34 -0400

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require('@discordjs');
 const MusicPlayer = require('../features/musicPlayer.js');
 const { AudioPlayerStatus, entersState } = require('@discordjs/voice');
 
