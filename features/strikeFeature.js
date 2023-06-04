74ae5a478b530268a4dc8018096fd99c2679185b
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-04 18:15:30 -0400

    Update strikeFeature.js

--- features/strikeFeature.js
+++ features/strikeFeature.js
@@ -1,61 +1,56 @@
-// features/strikeFeature.js
+const database = require('../database.js');
 
-const db = require('../database');
-const { EmbedBuilder } = require('discord.js');
+function initializeDatabase() {
+  const createTableQuery = `
+    CREATE TABLE IF NOT EXISTS strike_channels (
+      guild_id VARCHAR(20) NOT NULL,
+      channel_id VARCHAR(20) NOT NULL,
+      PRIMARY KEY (guild_id)
+    );
+  `;
+  database.query(createTableQuery)
+    .then(() => {
+      console.log('strike_channels table created');
+    })
+    .catch((error) => {
+      console.error('Error creating strike_channels table:', error);
+    });
+}
 
-// Set the strike channel for a guild
 async function setStrikeChannel(guildId, channelId) {
+  const query = `
+    INSERT INTO strike_channels (guild_id, channel_id)
+    VALUES (?, ?)
+    ON DUPLICATE KEY UPDATE channel_id = ?;
+  `;
   try {
-    await db.query('INSERT INTO strike_channels (guild_id, channel_id) VALUES (?, ?) ON DUPLICATE KEY UPDATE channel_id = ?', [guildId, channelId, channelId]);
-
-    // Get the strikes for all users in the guild
-    const strikeData = await getStrikesForGuild(guildId);
-
-    // Create the initial embed with strike data
-    const embed = createStrikeEmbed(strikeData);
-
-    // Find the strike channel
-    const channel = client.channels.cache.get(channelId);
-
-    // Send the initial embed to the strike channel
-    await channel.send({ embeds: [embed] });
+    await database.query(query, [guildId, channelId, channelId]);
+    console.log('Strike channel set successfully');
   } catch (error) {
     console.error('Error setting strike channel:', error);
   }
 }
 
-// Get the strikes for all users in the guild
-async function getStrikesForGuild(guildId) {
+async function getStrikeChannel(guildId) {
+  const query = `
+    SELECT channel_id
+    FROM strike_channels
+    WHERE guild_id = ?;
+  `;
   try {
-    const query = 'SELECT user_id, COUNT(*) AS strikes FROM strikes WHERE guild_id = ? GROUP BY user_id';
-    const strikes = await db.query(query, [guildId]);
-    return strikes;
+    const [rows] = await database.query(query, [guildId]);
+    if (rows.length > 0) {
+      return rows[0].channel_id;
+    }
+    return null;
   } catch (error) {
-    console.error('Error getting strikes for guild:', error);
-    return [];
+    console.error('Error getting strike channel:', error);
+    return null;
   }
-}
-
-// Create an embed with strike data
-function createStrikeEmbed(strikeData) {
-  const embed = new EmbedBuilder()
-    .setTitle('Strikes')
-    .setDescription('List of users with strikes:')
-    .setColor('#FF0000');
-
-  if (strikeData.length === 0) {
-    embed.addField('No strikes', 'No users have been striked yet.');
-  } else {
-    strikeData.forEach((strike) => {
-      const userTag = `<@${strike.user_id}>`;
-      embed.addField(userTag, `Strikes: ${strike.strikes}`);
-    });
-  }
-
-  return embed;
 }
 
 module.exports = {
+  initializeDatabase,
   setStrikeChannel,
-  // Other functions...
+  getStrikeChannel,
 };
