f4e4dd8bdbe20c1b33f5c5af188fc88ff026716c
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-04 21:28:37 -0400

    Update strikeFeature.js

--- features/strikeFeature.js
+++ features/strikeFeature.js
@@ -46,22 +46,32 @@ async function logStrike(guildId, userId, reason) {
     const [rows] = await pool.query(selectQuery, [guildId, userId]);
     
     console.log('Rows:', rows);
-    
-    const existingReasons = rows[0]?.strike_reasons ?? '';
 
-    const updatedReasons = existingReasons ? `${existingReasons}, ${reason}` : reason;
+    if (!rows || rows.length === 0) {
+      console.log('No existing strikes found for the user.');
+      const insertQuery = `
+        INSERT INTO strikes (guild_id, user_id, strike_reasons)
+        VALUES (?, ?, ?)
+      `;
+      await pool.query(insertQuery, [guildId, userId, reason]);
+    } else {
+      const existingReasons = rows[0].strike_reasons;
+      const updatedReasons = existingReasons ? `${existingReasons}, ${reason}` : reason;
 
-    const updateQuery = `
-      INSERT INTO strikes (guild_id, user_id, strike_reasons)
-      VALUES (?, ?, ?)
-      ON DUPLICATE KEY UPDATE strike_reasons = ?
-    `;
-    await pool.query(updateQuery, [guildId, userId, updatedReasons, updatedReasons]);
+      const updateQuery = `
+        UPDATE strikes
+        SET strike_reasons = ?
+        WHERE guild_id = ? AND user_id = ?
+      `;
+      await pool.query(updateQuery, [updatedReasons, guildId, userId]);
+    }
 
     console.log('Strike logged successfully.');
   } catch (error) {
     console.error('Error logging strike:', error);
   }
+}
+
 }
 
 async function getStrikes(guildId, userId) {
