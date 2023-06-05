1c391a0b4f3ec860527770bc934e2da16ae93489
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-04 21:29:39 -0400

    Update setStrikeChannel.js

--- commands/setStrikeChannel.js
+++ commands/setStrikeChannel.js
@@ -1,33 +1,27 @@
+const { SlashCommandBuilder } = require('discord.js');
 const { setStrikeChannel } = require('../features/strikeFeature');
-const pool = require('../database');
-
-async function execute(interaction) {
-  const guildId = interaction.guildId;
-  const channelId = interaction.options.getChannel('channel').id;
-
-  try {
-    console.log(`Setting strike channel for guild: ${guildId}`);
-    await setStrikeChannel(pool, guildId, channelId);
-    console.log(`Strike channel set for guild: ${guildId}`);
-    await interaction.reply(`Strike channel has been set successfully.`);
-  } catch (error) {
-    console.error('Error setting strike channel:', error);
-    await interaction.reply('An error occurred while setting the strike channel.');
-  }
-}
 
 module.exports = {
-  data: {
-    name: 'setstrikechannel',
-    description: 'Set the channel for strike notifications',
-    options: [
-      {
-        name: 'channel',
-        description: 'The channel to set for strike notifications',
-        type: 7, // Channel type: 7
-        required: true,
-      },
-    ],
+  data: new SlashCommandBuilder()
+    .setName('setstrikechannel')
+    .setDescription('Set the channel to log strike messages')
+    .addChannelOption((option) =>
+      option
+        .setName('channel')
+        .setDescription('The channel to log strike messages')
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+    const guildId = interaction.guildId;
+    const channelId = interaction.options.getChannel('channel').id;
+
+    try {
+      await setStrikeChannel(guildId, channelId);
+      await interaction.reply(`Strike channel has been set to <#${channelId}>.`);
+    } catch (error) {
+      console.error('Error setting strike channel:', error);
+      await interaction.reply('An error occurred while setting the strike channel.');
+    }
   },
-  execute,
 };
