3f623eaf3467837af6d0160445f590ae6d105532
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-06 13:30:54 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -159,23 +159,18 @@ client.login(token);
 
 async function getJoinMessageChannelFromDatabase(guildId) {
   try {
-    console.log('Retrieving join message channel from the database for guild ID:', guildId);
-
-    const [rows] = await pool.promise().query('SELECT join_message_channel, target_guild_id FROM guilds WHERE target_guild_id = ?', [guildId]);
-
-    console.log('Rows:', rows);
-
+    const [rows] = await pool.promise().query('SELECT join_message_channel FROM guilds WHERE target_guild_id = ?', [guildId]);
     if (rows.length > 0) {
-      const joinMessageChannel = rows[0];
+      const joinMessageChannel = rows[0].join_message_channel;
       console.log('Retrieved join message channel:', joinMessageChannel);
       return joinMessageChannel;
     }
-
     return null;
   } catch (error) {
     console.error('Error retrieving join message channel from the database:', error);
     throw error;
   }
 }
+
 
 
