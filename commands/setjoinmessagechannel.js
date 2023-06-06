49196be37cb06657587aadb26d015bf67709937b
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-06 09:36:33 -0400

    Update setjoinmessagechannel.js

--- commands/setjoinmessagechannel.js
+++ commands/setjoinmessagechannel.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { pool } = require('../database.js');
+const pool = require('../database.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
