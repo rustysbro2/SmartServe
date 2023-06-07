f9234752e28b10f4583c23db009f5ee2dd72ef2b
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-07 11:33:38 -0400

    Update setjoinmessagechannel.js

--- commands/setjoinmessagechannel.js
+++ commands/setjoinmessagechannel.js
@@ -25,56 +25,44 @@ async function saveJoinMessageChannelToDatabase(channelId, guildId) {
   }
 }
 
-async function saveLeaveMessageChannelToDatabase(channelId, guildId) {
-  try {
-    await pool.promise().query('INSERT INTO guilds (leave_message_channel, target_guild_id) VALUES (?, ?) ON DUPLICATE KEY UPDATE leave_message_channel = ?, target_guild_id = ?', [channelId, guildId, channelId, guildId]);
-  } catch (error) {
-    console.error('Error saving leave message channel to the database:', error);
-    throw error;
-  }
-}
-
 module.exports = {
-  setJoinMessageChannelCommand: {
-    data: new SlashCommandBuilder()
-      .setName('setjoinmessagechannel')
-      .setDescription('Set the channel for the bot to send a join message when added to a new guild')
-      .addChannelOption(option =>
-        option
-          .setName('channel')
-          .setDescription('The channel to send the join message')
-          .setRequired(true)),
+  data: new SlashCommandBuilder()
+    .setName('setjoinmessagechannel')
+    .setDescription('Set the channel for the bot to send a join message when added to a new guild')
+    .addChannelOption(option =>
+      option.setName('channel')
+        .setDescription('The channel to send the join message')
+        .setRequired(true)),
 
-    async execute(interaction) {
-      const channel = interaction.options.getChannel('channel');
-      const guildId = interaction.guild.id;
+  async execute(interaction) {
+    const channel = interaction.options.getChannel('channel');
+    const guildId = interaction.guild.id;
 
-      console.log('Channel ID:', channel.id);
-      console.log('Guild ID:', guildId);
+    console.log('Channel ID:', channel.id);
+    console.log('Guild ID:', guildId);
 
-      try {
-        await createGuildsTable();
+    try {
+      await createGuildsTable();
 
-        await saveJoinMessageChannelToDatabase(channel.id, guildId);
+      await saveJoinMessageChannelToDatabase(channel.id, guildId);
 
-        const joinMessage = `The bot has been added to a new guild!\nGuild ID: ${guildId}`;
+      const joinMessage = `The bot has been added to a new guild!\nGuild ID: ${guildId}`;
 
-        if (channel && channel.type === 'GUILD_TEXT') {
-          console.log('Join message channel:', channel.name);
-          await channel.send(joinMessage);
-        } else {
-          console.log('Channel not found or invalid channel type:', channel);
-        }
-
-        interaction.reply(`Join message channel set to ${channel} for all new guilds.`);
-      } catch (error) {
-        console.error('Error setting join message channel:', error);
-        interaction.reply('Failed to set the join message channel. Please try again.');
+      if (channel && channel.type === 'GUILD_TEXT') {
+        console.log('Join message channel:', channel.name);
+        await channel.send(joinMessage);
+      } else {
+        console.log('Channel not found or invalid channel type:', channel);
       }
-    },
 
-    category: 'Administration',
-    categoryDescription: 'Commands for server administration',
-    global: false,
-  }
+      interaction.reply(`Join message channel set to ${channel} for all new guilds.`);
+    } catch (error) {
+      console.error('Error setting join message channel:', error);
+      interaction.reply('Failed to set the join message channel. Please try again.');
+    }
+  },
+
+  category: 'Administration',
+  categoryDescription: 'Commands for server administration',
+  global: false,
 };
