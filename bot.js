3f94fd3230a66bf8aa1b0e483900bc00d9476c17
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-07 13:30:33 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -150,7 +150,7 @@ client.on('guildCreate', async (guild) => {
     console.log('Target Channel:', channel);
     console.log('Channel Type:', channel?.type);
 
-    if (!channel || channel.type !== CHANNEL_TYPES.GUILD_TEXT) {
+    if (!channel || channel.type !== 0) {
       console.log('Text channel not found in the target guild.');
       return;
     }
@@ -194,7 +194,7 @@ client.on('guildDelete', async (guild) => {
     console.log('Target Channel:', channel);
     console.log('Channel Type:', channel?.type);
 
-    if (!channel || channel.type !== CHANNEL_TYPES.GUILD_TEXT) {
+    if (!channel || channel.type !== 0) {
       console.log('Text channel not found in the target guild.');
       return;
     }
