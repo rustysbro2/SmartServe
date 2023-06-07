3538cc44734de76e653db50c0de3244384e4582b
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-07 11:05:23 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,96 +1,276 @@
-const { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, EmbedBuilder } = require('discord.js');
-const { guildId } = require('../config.js');
+const { Client, Collection, GatewayIntentBits, Presence, ActivityType } = require('discord.js');
+const { token } = require('./config.js');
+const inviteTracker = require('./features/inviteTracker.js');
+const fs = require('fs');
+const helpCommand = require('./commands/help');
+const countingCommand = require('./commands/count');
+const setJoinMessageChannelCommand = require('./commands/setjoinmessagechannel');
+const setLeaveMessageChannelCommand = require('./commands/setleavemessagechannel');
+const slashCommands = require('./slashCommands.js');
+const pool = require('./database.js');
+const { CHANNEL_TYPES } = require('discord.js');
 
-async function handleSelectMenu(interaction, commandCategories) {
-  console.log('Interaction guild ID:', interaction.guildId);
-  console.log('Stored guild ID:', guildId);
-  const selectedCategory = interaction.values[0];
-  const category = commandCategories.find(
-    (category) =>
-      category.name.toLowerCase().replace(/\s/g, '_') === selectedCategory
-  );
+const intents = [
+  GatewayIntentBits.Guilds,
+  GatewayIntentBits.GuildMessages,
+  GatewayIntentBits.GuildMembers,
+  GatewayIntentBits.GuildVoiceStates,
+  GatewayIntentBits.GuildPresences
+];
 
-  let categoryEmbed;
+const client = new Client({ shards: "auto", intents });
 
-  if (category) {
-    categoryEmbed = new EmbedBuilder();
-    categoryEmbed.setTitle(`Commands - ${category.name}`);
-    categoryEmbed.setDescription(category.categoryDescription || 'No description available.');
+client.commands = new Collection();
+client.musicPlayers = new Map();
 
-    const commandsToShow = category.commands.filter((command) => {
-      console.log('Command:', command.name);
-      console.log('Global:', command.global);
-      console.log('Interaction guild ID:', interaction.guildId);
-      console.log('Stored guild ID:', guildId);
-      const shouldShow = command.global === true || (command.global === false && interaction.guildId === '1106643216125665350');
-      console.log('Should Show:', shouldShow);
-      return shouldShow;
-    });
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandCategories = [];
 
-    console.log('Commands to show:', commandsToShow); // Debugging line
+for (const file of commandFiles) {
+  const command = require(`./commands/${file.endsWith('.js') ? file : file + '.js'}`);
+  client.commands.set(command.data.name, command);
 
-    commandsToShow.forEach((command) => {
-      categoryEmbed.addFields({ name: command.name, value: command.description });
-    });
-
-    try {
-      if (interaction.message) {
-        const actionRow = new ActionRowBuilder().addComponents(...interaction.message.components[0].components);
-        await interaction.deferUpdate();
-        await interaction.message.edit({ embeds: [categoryEmbed], components: [actionRow] });
-      } else {
-        console.error('Interaction does not have a message.');
-      }
-    } catch (error) {
-      console.error('Error deferring or editing interaction:', error);
+  if (command.category) {
+    let category = commandCategories.find(category => category.name === command.category);
+    if (!category) {
+      category = {
+        name: command.category,
+        description: '',
+        commands: [],
+        guildId: command.guildId,
+        categoryDescription: command.categoryDescription // Assign category description here
+      };
+      commandCategories.push(category);
     }
+    category.commands.push({
+      name: command.data.name,
+      description: command.data.description,
+      global: command.global !== false,
+      categoryDescription: command.categoryDescription // Include the categoryDescription property
+    });
   } else {
-    console.error(`Category '${selectedCategory}' not found.`);
-    return;
+    let defaultCategory = commandCategories.find(category => category.name === 'Uncategorized');
+    if (!defaultCategory) {
+      defaultCategory = {
+        name: 'Uncategorized',
+        description: 'Commands that do not belong to any specific category',
+        commands: [],
+        guildId: undefined
+      };
+      commandCategories.push(defaultCategory);
+    }
+    defaultCategory.commands.push({
+      name: command.data.name,
+      description: command.data.description,
+      global: command.global !== false
+    });
   }
 }
 
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('help')
-    .setDescription('List all commands or info about a specific command'),
+// Remove empty categories
+commandCategories.forEach((category) => {
+  if (category.commands.length === 0) {
+    const index = commandCategories.indexOf(category);
+    commandCategories.splice(index, 1);
+  }
+});
 
-  async execute(interaction, client, commandCategories) {
-    if (interaction.deferred || interaction.replied) {
-      console.log('Interaction already deferred or replied to.');
+client.once('ready', async () => {
+  try {
+    console.log(`Shard ${client.shard.ids} logged in as ${client.user.tag}!`);
+    client.user.setPresence({
+      activities: [
+        {
+          name: `${client.guilds.cache.size} servers | Shard ${client.shard.ids[0]}`,
+          type: ActivityType.WATCHING,
+        },
+      ],
+      status: "online",
+    });
+
+    inviteTracker.execute(client);
+
+    await slashCommands(client);
+
+    console.log('Command Categories:');
+    commandCategories.forEach((category) => {
+      console.log(`Category: ${category.name}`);
+      console.log(`Guild ID: ${category.guildId}`);
+      console.log('Commands:', category.commands);
+    });
+  } catch (error) {
+    console.error('Error during bot initialization:', error);
+  }
+});
+
+client.on('interactionCreate', async (interaction) => {
+  try {
+    if (interaction.isStringSelectMenu() && interaction.customId === 'help_category') {
+      helpCommand.handleSelectMenu(interaction, commandCategories);
+    } else if (interaction.isCommand()) {
+      const command = client.commands.get(interaction.commandName);
+      if (command) {
+        await command.execute(interaction, client, commandCategories);
+      }
+    }
+  } catch (error) {
+    console.error('Error handling interaction:', error);
+  }
+});
+
+client.on('guildCreate', async (guild) => {
+  try {
+    console.log(`Bot joined a new guild: ${guild.name} (${guild.id})`);
+
+    const joinMessageChannel = await getJoinMessageChannelFromDatabase(guild.id);
+
+    if (!joinMessageChannel) {
+      console.log('Join message channel not set in the database.');
       return;
     }
 
-    const filteredCommandCategories = commandCategories
-      .filter((category) => 
-        category.commands.some((command) => command.global === true || (command.global === false && interaction.guildId === '1106643216125665350'))
-      )
-      .slice(0, 10);
+    console.log('Retrieved join message channel:', joinMessageChannel);
 
-    const selectMenu = new StringSelectMenuBuilder()
-      .setCustomId('help_category')
-      .setPlaceholder('Select a category');
+    const joinMessage = `The bot has been added to a new guild!\nGuild ID: ${guild.id}`;
 
-    filteredCommandCategories.forEach((category) => {
-      if (category.commands.some((command) => command.global !== false || command.guildId === undefined)) {
-        const categoryName = category.name.toLowerCase().replace(/\s/g, '_');
-        selectMenu.addOptions(
-          new StringSelectMenuOptionBuilder()
-            .setLabel(category.name)
-            .setValue(categoryName)
-            .setDescription(category.categoryDescription || 'No description available.')
-        );
-      }
+    const targetGuild = client.guilds.cache.get(joinMessageChannel.target_guild_id);
+    if (!targetGuild) {
+      console.log('Target guild not found.');
+      return;
+    }
+
+    console.log('Target Guild:', targetGuild);
+
+    console.log('Target Guild Channels:');
+    targetGuild.channels.cache.forEach((channel) => {
+      console.log(`Channel ID: ${channel.id}, Name: ${channel.name}, Type: ${channel.type}`);
     });
 
-    const actionRow = new ActionRowBuilder().addComponents(selectMenu);
+    const channel = targetGuild.channels.cache.get(joinMessageChannel.join_message_channel);
+    console.log('Target Channel:', channel);
+    console.log('Channel Type:', channel?.type);
 
-    try {
-      await interaction.reply({ content: 'Please select a category:', components: [actionRow] });
-    } catch (error) {
-      console.error('Error replying to interaction:', error);
+    if (!channel || channel.type !== 0) {
+      console.log('Text channel not found in the target guild.');
+      return;
     }
-  },
 
-  handleSelectMenu,
-};
+    await channel.send(joinMessage);
+    console.log('Join message sent successfully.');
+  } catch (error) {
+    console.error('Error handling guildCreate event:', error);
+  }
+});
+
+client.on('guildDelete', async (guild) => {
+  try {
+    console.log(`Bot left a guild: ${guild.name} (${guild.id})`);
+
+    const leaveMessageChannel = await getLeaveMessageChannelFromDatabase(guild.id);
+
+    if (!leaveMessageChannel) {
+      console.log('Leave message channel not set in the database.');
+      return;
+    }
+
+    console.log('Retrieved leave message channel:', leaveMessageChannel);
+
+    const leaveMessage = `The bot has been removed from a guild!\nGuild ID: ${guild.id}`;
+
+    const targetGuild = client.guilds.cache.get(leaveMessageChannel.target_guild_id);
+    if (!targetGuild) {
+      console.log('Target guild not found.');
+      return;
+    }
+
+    console.log('Target Guild:', targetGuild);
+
+    console.log('Target Guild Channels:');
+    targetGuild.channels.cache.forEach((channel) => {
+      console.log(`Channel ID: ${channel.id}, Name: ${channel.name}, Type: ${channel.type}`);
+    });
+
+    const channel = targetGuild.channels.cache.get(leaveMessageChannel.leave_message_channel);
+    console.log('Target Channel:', channel);
+    console.log('Channel Type:', channel?.type);
+
+    if (!channel || channel.type !== 0) {
+      console.log('Text channel not found in the target guild.');
+      return;
+    }
+
+    await channel.send(leaveMessage);
+    console.log('Leave message sent successfully.');
+  } catch (error) {
+    console.error('Error handling guildDelete event:', error);
+  }
+});
+
+client.on('error', (error) => {
+  console.error('Discord client error:', error);
+});
+
+client.login(token);
+
+async function getJoinMessageChannelFromDatabase(guildId) {
+  try {
+    const [rows] = await pool.promise().query('SELECT join_message_channel, target_guild_id FROM guilds WHERE target_guild_id = ? LIMIT 1', [guildId]);
+    if (rows.length > 0) {
+      const joinMessageChannel = rows[0];
+      console.log('Retrieved join message channel:', joinMessageChannel);
+      return joinMessageChannel;
+    }
+    return null;
+  } catch (error) {
+    console.error('Error retrieving join message channel from the database:', error);
+    throw error;
+  }
+}
+
+async function getLeaveMessageChannelFromDatabase(guildId) {
+  try {
+    const [rows] = await pool.promise().query('SELECT leave_message_channel, target_guild_id FROM guilds WHERE target_guild_id = ? LIMIT 1', [guildId]);
+    if (rows.length > 0) {
+      const leaveMessageChannel = rows[0];
+      console.log('Retrieved leave message channel:', leaveMessageChannel);
+      return leaveMessageChannel;
+    }
+    return null;
+  } catch (error) {
+    console.error('Error retrieving leave message channel from the database:', error);
+    throw error;
+  }
+}
+
+async function createGuildsTable() {
+  try {
+    await pool.promise().query(`
+      CREATE TABLE IF NOT EXISTS guilds (
+        join_message_channel VARCHAR(255) NOT NULL,
+        leave_message_channel VARCHAR(255) NOT NULL,
+        target_guild_id VARCHAR(255) NOT NULL
+      )
+    `);
+  } catch (error) {
+    console.error('Error creating guilds table:', error);
+    throw error;
+  }
+}
+
+async function saveJoinMessageChannelToDatabase(channelId, guildId) {
+  try {
+    await pool.promise().query('INSERT INTO guilds (join_message_channel, target_guild_id) VALUES (?, ?) ON DUPLICATE KEY UPDATE join_message_channel = ?, target_guild_id = ?', [channelId, guildId, channelId, guildId]);
+  } catch (error) {
+    console.error('Error saving join message channel to the database:', error);
+    throw error;
+  }
+}
+
+async function saveLeaveMessageChannelToDatabase(channelId, guildId) {
+  try {
+    await pool.promise().query('INSERT INTO guilds (leave_message_channel, target_guild_id) VALUES (?, ?) ON DUPLICATE KEY UPDATE leave_message_channel = ?, target_guild_id = ?', [channelId, guildId, channelId, guildId]);
+  } catch (error) {
+    console.error('Error saving leave message channel to the database:', error);
+    throw error;
+  }
+}
