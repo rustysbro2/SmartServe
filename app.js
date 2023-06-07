2c227fa75270b54a76c5600445338fd720635184
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-07 14:25:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,81 @@
 const express = require('express');
-const { Client } = require('discord.js');
+const session = require('express-session');
+const passport = require('passport');
+const DiscordStrategy = require('passport-discord').Strategy;
+const crypto = require('crypto');
 
 const app = express();
-const client = new Client();
 
-// Website Setup
+// Generate a random session secret
+const sessionSecret = crypto.randomBytes(32).toString('hex');
 
-app.set('view engine', 'ejs');
+// Configure session middleware
+app.use(session({
+  secret: sessionSecret,
+  resave: false,
+  saveUninitialized: false
+}));
 
-// Define website routes
-app.get('/', (req, res) => {
-  res.render('index');
+// Passport configuration
+passport.use(new DiscordStrategy({
+  clientID: 'YOUR_DISCORD_CLIENT_ID',
+  clientSecret: 'YOUR_DISCORD_CLIENT_SECRET',
+  callbackURL: 'YOUR_CALLBACK_URL',
+  scope: ['identify']
+}, (accessToken, refreshToken, profile, done) => {
+  // Verify and retrieve user data
+  // Replace this with your own implementation
+  const user = {
+    id: profile.id,
+    username: profile.username,
+    discriminator: profile.discriminator,
+    accessToken: accessToken
+  };
+
+  return done(null, user);
+}));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
 });
 
-app.get('/about', (req, res) => {
-  res.render('about');
+passport.deserializeUser((id, done) => {
+  // Retrieve user data from database or cache
+  // Replace this with your own implementation
+  const user = {
+    id: id,
+    username: 'exampleUser' // Replace with actual username retrieval
+  };
+
+  done(null, user);
+});
+
+// Initialize Passport and restore authentication state, if any
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Define routes
+app.get('/', (req, res) => {
+  res.send('Welcome to the homepage');
+});
+
+app.get('/login', passport.authenticate('discord'));
+
+app.get('/callback', passport.authenticate('discord', {
+  successRedirect: '/dashboard',
+  failureRedirect: '/login'
+}));
+
+app.get('/dashboard', (req, res) => {
+  if (req.isAuthenticated()) {
+    res.send(`Welcome, ${req.user.username}!`);
+  } else {
+    res.redirect('/login');
+  }
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Website running on port 3000');
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
 });
