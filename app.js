7a17ae6d3935931ee4b4ca7f9d73ce450a69525f
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-08 16:07:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -140,7 +140,7 @@ app.get('/dashboard', (req, res) => {
 });
 
 // Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 // HTTPS and SSL configuration
 const options = {
