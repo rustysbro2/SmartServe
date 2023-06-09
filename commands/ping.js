93d9b368c19cf89bfa27297d89bfe9dfadfcf936
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-09 18:57:32 -0400

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -7,19 +7,11 @@ module.exports = {
 
   async execute(interaction) {
     try {
-      // Check if the user has the required permissions
-      if (!interaction.channel.permissionsFor(interaction.member).has('SEND_MESSAGES')) {
-        await interaction.reply('You do not have permission to use this command.');
-        return;
-      }
-
       const sent = await interaction.reply('Pinging...');
       const latency = sent.createdTimestamp - interaction.createdTimestamp;
-      await interaction.editReply(`Pong! Bot latency: ${latency}ms, API latency: ${Math.round(interaction.client.ws.ping)}ms`);
+      interaction.editReply(`Pong! Bot latency: ${latency}ms, API latency: ${Math.round(interaction.client.ws.ping)}ms`);
     } catch (error) {
       console.error('Error replying to ping command:', error);
-      // Only send a message indicating an error occurred without specifying the reason
-      await interaction.reply('An error occurred while executing the command.');
     }
   },
 
