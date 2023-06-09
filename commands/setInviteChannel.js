bebae68930ca5d969a0cf0b73e0aff4caaa9d6bf
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-09 15:48:20 -0400

    Update setInviteChannel.js

--- commands/setInviteChannel.js
+++ commands/setInviteChannel.js
@@ -1,20 +1,21 @@
-const { SlashCommandBuilder, PermissionBitField, PermissionFlagBits } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagBits } = require('discord.js');
 const inviteTracker = require('../features/inviteTracker.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('setinvitechannel')
     .setDescription('Set the channel for invite tracking messages')
-    .setDefaultMemberPermissions(PermissionFlagBits.KickMembers)
     .addChannelOption(option =>
       option.setName('channel')
         .setDescription('The channel to send messages in')
-        .setRequired(true)),
+        .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagBits.KickMembers);
+
   async execute(interaction) {
 
       // Permission checks for the user
       const member = interaction.member;
-      if (!member.permissions.has(PermissionsBitField.Flags.KickMembers)) {
+      if (!member.permissions.has(.Flags.KickMembers)) {
         await interaction.reply("You must be an administrator to perform this action.");
         return;
       }
