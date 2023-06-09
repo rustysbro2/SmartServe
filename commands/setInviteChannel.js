8bc2c96415bf96fc7376addc0f1a42a64e219bd4
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-09 12:35:44 -0400

    Update setInviteChannel.js

--- commands/setInviteChannel.js
+++ commands/setInviteChannel.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, Permissions } = require('discord.js');
 const inviteTracker = require('../features/inviteTracker.js');
 
 module.exports = {
@@ -19,31 +19,20 @@ module.exports = {
 
       // Permission checks for the bot
       const botMember = interaction.guild.members.cache.get(interaction.client.user.id);
-      if (!botMember.permissions.has('SEND_MESSAGES') || !botMember.permissions.has('EMBED_LINKS')) {
-        await interaction.reply('The bot does not have the required permissions to execute this command.');
+      const requiredPermissions = new Permissions(['SEND_MESSAGES', 'EMBED_LINKS']);
+      if (!botMember.permissions.has(requiredPermissions)) {
+        const missingPermissions = requiredPermissions.missing(botMember.permissions);
+        await interaction.reply(`The bot does not have the required permissions to execute this command. Missing permissions: ${missingPermissions.join(', ')}`);
         return;
       }
 
       const channel = interaction.options.getChannel('channel');
       inviteTracker.setInviteChannel(interaction.guildId, channel.id);
 
-      const embed = new EmbedBuilder()
-        .setColor(0x00ff00)
-        .setTitle('Invite Channel Set')
-        .setDescription(`Invite tracking messages will be sent in ${channel}`)
-        .setThumbnail(interaction.client.user.displayAvatarURL());
-
-      await interaction.reply({ embeds: [embed], ephemeral: true });
+      await interaction.reply('Invite channel has been set successfully.');
     } catch (error) {
       console.error('Error handling setinvitechannel command:', error);
-
-      // Determine the reason for the error and provide an appropriate error message
-      let errorMessage = 'An error occurred while executing the command. Please try again later.';
-      if (error.code === 'BitFieldInvalid') {
-        errorMessage = 'Invalid permissions. Make sure the bot has the required permissions.';
-      }
-
-      await interaction.reply(`Command failed: ${errorMessage}`);
+      await interaction.reply('An error occurred while executing the command. Please try again later.');
     }
   },
 
