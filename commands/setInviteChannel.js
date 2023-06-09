9af52e96b25a063aaabec028ffa1412a7cd41822
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-09 15:40:53 -0400

    Update setInviteChannel.js

--- commands/setInviteChannel.js
+++ commands/setInviteChannel.js
@@ -10,38 +10,13 @@ module.exports = {
         .setDescription('The channel to send messages in')
         .setRequired(true)),
   async execute(interaction) {
-    try {
-      // Check if the interaction has guild data
-      if (!interaction.guild) {
-        await interaction.reply('This command can only be used in a server (guild).');
-        return;
-      }
 
       // Permission checks for the user
-      if (!interaction.member.permissions.has(PermissionBitField.Flags.MANAGE_GUILD)) {
+      if (!member.permissions.has(PermissionsBitField.Flags.KickMembers)) {
         await interaction.reply("You must be an administrator to perform this action.");
         return;
       }
 
-      // Permission checks for the bot
-      const botMember = interaction.guild.members.cache.get(interaction.client.user.id);
-      if (
-        !interaction.channel.permissionsFor(botMember).has(PermissionBitField.SEND_MESSAGES) ||
-        !interaction.channel.permissionsFor(botMember).has(PermissionBitField.EMBED_LINKS)
-      ) {
-        await interaction.reply('The bot does not have the required permissions to execute this command.');
-        return;
-      }
-
-      const channel = interaction.options.getChannel('channel');
-      inviteTracker.setInviteChannel(interaction.guildId, channel.id);
-
-      await interaction.reply('Invite channel has been set successfully.');
-    } catch (error) {
-      console.error('Error handling setinvitechannel command:', error);
-      await interaction.reply('An error occurred while executing the command. Please try again later.');
-    }
-  },
 
   category: 'Invite Tracker',
   categoryDescription: 'Commands related to invite tracking functionality',
