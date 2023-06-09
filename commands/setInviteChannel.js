30042de25f7b45ff1af414ead8cd11e7905fa508
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-09 12:32:53 -0400

    Update setInviteChannel.js

--- commands/setInviteChannel.js
+++ commands/setInviteChannel.js
@@ -10,29 +10,34 @@ module.exports = {
         .setDescription('The channel to send messages in')
         .setRequired(true)),
   async execute(interaction) {
-    // Permission checks for the user
-    if (!interaction.member.permissions.has('MANAGE_GUILD')) {
-      await interaction.reply('You do not have permission to use this command.');
-      return;
+    try {
+      // Permission checks for the user
+      if (!interaction.member.permissions.has('MANAGE_GUILD')) {
+        await interaction.reply('You do not have permission to use this command.');
+        return;
+      }
+
+      // Permission checks for the bot
+      const botMember = interaction.guild.members.cache.get(interaction.client.user.id);
+      if (!botMember.permissions.has('SEND_MESSAGES') || !botMember.permissions.has('EMBED_LINKS')) {
+        await interaction.reply('The bot does not have the required permissions to execute this command.');
+        return;
+      }
+
+      const channel = interaction.options.getChannel('channel');
+      inviteTracker.setInviteChannel(interaction.guildId, channel.id);
+
+      const embed = new EmbedBuilder()
+        .setColor(0x00ff00)
+        .setTitle('Invite Channel Set')
+        .setDescription(`Invite tracking messages will be sent in ${channel}`)
+        .setThumbnail(interaction.client.user.displayAvatarURL());
+
+      await interaction.reply({ embeds: [embed], ephemeral: true });
+    } catch (error) {
+      console.error('Error handling setinvitechannel command:', error);
+      await interaction.reply('An error occurred while executing the command. Please try again later.');
     }
-
-    // Permission checks for the bot
-    const botMember = interaction.guild.members.cache.get(interaction.client.user.id);
-    if (!botMember.permissions.has('SEND_MESSAGES') || !botMember.permissions.has('EMBED_LINKS')) {
-      await interaction.reply('The bot does not have the required permissions to execute this command.');
-      return;
-    }
-
-    const channel = interaction.options.getChannel('channel');
-    inviteTracker.setInviteChannel(interaction.guildId, channel.id);
-
-    const embed = new EmbedBuilder()
-      .setColor(0x00ff00)
-      .setTitle('Invite Channel Set')
-      .setDescription(`Invite tracking messages will be sent in ${channel}`)
-      .setThumbnail(interaction.client.user.displayAvatarURL());
-
-    await interaction.reply({ embeds: [embed], ephemeral: true });
   },
 
   category: 'Invite Tracker',
