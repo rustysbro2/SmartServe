cf60634b5b4d518769f2ace452035cb0ae906b2b
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-09 15:59:00 -0400

    Update setInviteChannel.js

--- commands/setInviteChannel.js
+++ commands/setInviteChannel.js
@@ -1,34 +1,12 @@
-const { SlashCommandBuilder, PermissionFlagBits } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName('kick')
-    .setDescription('Select a member and kick them.')
-    .addUserOption(option =>
-      option
-        .setName('target')
-        .setDescription('The member to kick')
-        .setRequired(true))
-    .setDefaultMemberPermissions(PermissionFlagBits.KickMembers),
-
-  async execute(interaction) {
-    // Permission checks for the user
-    const member = interaction.member;
-    if (!member.permissions.has(PermissionFlagBits.KickMembers)) {
-      await interaction.reply("You must have the Kick Members permission to use this command.");
-      return;
-    }
-
-    // Retrieve the target user from the command's options
-    const targetUser = interaction.options.getUser('target');
-
-    // Kick the target user
-    try {
-      await interaction.guild.members.kick(targetUser.id);
-      await interaction.reply(`Successfully kicked ${targetUser.tag}.`);
-    } catch (error) {
-      console.error(`Failed to kick ${targetUser.tag}:`, error);
-      await interaction.reply(`Failed to kick ${targetUser.tag}.`);
-    }
-  },
-};
+const data = new SlashCommandBuilder()
+	.setName('ban')
+	.setDescription('Select a member and ban them.')
+	.addUserOption(option =>
+		option
+			.setName('target')
+			.setDescription('The member to ban')
+			.setRequired(true))
+	.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+	.setDMPermission(false);
