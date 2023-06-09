df91e595ae8104b33e45f0a6a389310ed77c242e
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-09 19:06:27 -0400

    Update count.js

--- commands/count.js
+++ commands/count.js
@@ -8,17 +8,11 @@ module.exports = {
 
   async execute(interaction) {
     try {
-      // Check if the bot has permission to send messages in the interaction channel
-      if (!interaction.channel.permissionsFor(interaction.client.user).has('SEND_MESSAGES')) {
-        return interaction.reply('I do not have permission to send messages in this channel.');
-      }
-
       countingGame.incrementCount();
       const count = countingGame.getCount();
       await interaction.reply(`Count: ${count}`);
     } catch (error) {
       console.error('Error executing count command:', error);
-      await interaction.reply('An error occurred while executing the command. Please try again later.');
     }
   }
 };
