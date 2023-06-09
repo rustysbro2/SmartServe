0183e29ea86beb5ae539d6bd4f3d5ca2da844c00
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-09 11:40:15 -0400

    Update setInviteChannel.js

--- commands/setInviteChannel.js
+++ commands/setInviteChannel.js
@@ -8,8 +8,16 @@ module.exports = {
     .addChannelOption(option =>
       option.setName('channel')
         .setDescription('The channel to send messages in')
-        .setRequired(true)),
+        .setRequired(true))
+    .setPermissions(['MANAGE_GUILD', 'SEND_MESSAGES', 'EMBED_LINKS']), // Require "Manage Server", "Send Messages", and "Embed Links" permissions
+
   async execute(interaction) {
+    // Check if the user has the necessary permissions
+    if (!interaction.member.permissions.has('MANAGE_GUILD') || !interaction.member.permissions.has('SEND_MESSAGES') || !interaction.member.permissions.has('EMBED_LINKS')) {
+      await interaction.reply('You do not have the required permissions to use this command!');
+      return;
+    }
+
     const channel = interaction.options.getChannel('channel');
     inviteTracker.setInviteChannel(interaction.guildId, channel.id);
 
