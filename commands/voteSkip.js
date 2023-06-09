e1f1fd3d7ec1cbdbebc2c236c67971baade67f6a
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-09 18:48:35 -0400

    Update voteSkip.js

--- commands/voteSkip.js
+++ commands/voteSkip.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder, PermissionFlagsBits, PermissionsBitField  } = require('@discordjs/builders');
 
 module.exports = {
   data: new SlashCommandBuilder()
