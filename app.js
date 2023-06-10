de2b0a790e0852b014820a97499a503d9a11c6b2
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-10 10:33:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const https = require('https');
 const fs = require('fs');
 const path = require('path');
+const DiscordOAuth2 = require('discord-oauth2');
 
 const options = {
   key: fs.readFileSync('/root/Certs/private-key.key'), // Replace with the path to your private key file
@@ -11,12 +12,47 @@ const options = {
 
 const app = express();
 const port = 443;
+const oauth = new DiscordOAuth2();
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/login', (req, res) => {
+  const redirectUri = 'https://your-domain.com/callback'; // Replace with your redirect URI
+
+  const authorizationUrl = oauth.generateAuthUrl({
+    scope: ['identify', 'email'], // Specify the required scopes
+    redirectUri,
+  });
+  res.redirect(authorizationUrl);
+});
+
+app.get('/callback', async (req, res) => {
+  const { code } = req.query;
+
+  try {
+    const tokenData = await oauth.tokenRequest({
+      code,
+      scope: ['identify', 'email'], // Specify the required scopes
+    });
+
+    const user = await oauth.getUser(tokenData.access_token);
+
+    // Store the user's data or perform additional logic as needed
+    // For demonstration purposes, we'll simply display the user's information
+    res.send(user);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('An error occurred.');
+  }
 });
 
 https.createServer(options, app).listen(port, () => {
