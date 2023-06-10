4ef9f2496c1596dbe84b21f63bd19db3258d25c6
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-10 10:51:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -55,7 +55,6 @@ app.get('/callback', async (req, res) => {
     console.error('Error:', error);
     res.status(500).send('An error occurred.');
   }
-});
 
 https.createServer(options, app).listen(port, () => {
   console.log(`Server is running on port ${port}`);
