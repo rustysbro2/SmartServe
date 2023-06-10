66ab7d839e44a269a87a915ad98a0b045bcfbc96
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-09 22:08:58 -0400

    Update Delete.js

--- Delete.js
+++ Delete.js
@@ -7,18 +7,35 @@ client.once('ready', async () => {
   console.log(`Ready! Logged in as ${client.user.tag}`);
 
   try {
-    const commands = await client.application.commands.fetch();
-    let deletedCount = 0;
+    // Deleting global commands
+    const globalCommands = await client.application.commands.fetch();
+    let deletedGlobalCount = 0;
 
-    for (const command of commands.values()) {
+    for (const command of globalCommands.values()) {
       await command.delete();
-      console.log(`Deleted command "${command.name}"`);
-      deletedCount++;
+      console.log(`Deleted global command "${command.name}"`);
+      deletedGlobalCount++;
     }
 
-    console.log(`Successfully deleted ${deletedCount} global commands.`);
+    console.log(`Successfully deleted ${deletedGlobalCount} global commands.`);
+
+    // Deleting guild-specific commands
+    const guilds = client.guilds.cache; // Fetches all the guilds the bot is a member of
+
+    for (const guild of guilds.values()) {
+      const guildCommands = await guild.commands.fetch();
+      let deletedGuildCount = 0;
+
+      for (const command of guildCommands.values()) {
+        await command.delete();
+        console.log(`Deleted guild-specific command "${command.name}" in guild "${guild.name}"`);
+        deletedGuildCount++;
+      }
+
+      console.log(`Successfully deleted ${deletedGuildCount} guild-specific commands in guild "${guild.name}".`);
+    }
   } catch (error) {
-    console.error('Error deleting global commands:', error);
+    console.error('Error deleting commands:', error);
   }
 
   client.destroy();
