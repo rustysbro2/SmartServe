27e09a8e61e9996a1de868dfc65918073512c24a
Richard Mazzocco <rustysbro2016@techie.com>
2023-06-10 21:48:10 -0400

    Modify Delete

--- delete.js
+++ delete.js
@@ -1,5 +1,7 @@
+require('dotenv').config({ path: './src/.env' });
+
 const { Client, GatewayIntentBits } = require('discord.js');
-const { clientId, token } = require('./config.js');
+const { CLIENT_ID, TOKEN, guildId } = process.env;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -7,21 +9,35 @@ client.once('ready', async () => {
   console.log(`Ready! Logged in as ${client.user.tag}`);
 
   try {
-    const commands = await client.application.commands.fetch();
-    let deletedCount = 0;
+    const globalCommands = await client.application.commands.fetch();
+    let deletedGlobalCount = 0;
 
-    for (const command of commands.values()) {
+    for (const command of globalCommands.values()) {
       await command.delete();
-      console.log(`Deleted command "${command.name}"`);
-      deletedCount++;
+      console.log(`Deleted global command "${command.name}"`);
+      deletedGlobalCount++;
     }
 
-    console.log(`Successfully deleted ${deletedCount} global commands.`);
+    console.log(`Successfully deleted ${deletedGlobalCount} global commands.`);
+
+    if (guildId) {
+      const guild = await client.guilds.fetch(guildId);
+      const guildCommands = await guild.commands.fetch();
+      let deletedGuildCount = 0;
+
+      for (const command of guildCommands.values()) {
+        await command.delete();
+        console.log(`Deleted guild-specific command "${command.name}" in guild ${guild.name}`);
+        deletedGuildCount++;
+      }
+
+      console.log(`Successfully deleted ${deletedGuildCount} guild-specific commands in guild ${guild.name}.`);
+    }
   } catch (error) {
-    console.error('Error deleting global commands:', error);
+    console.error('Error deleting commands:', error);
   }
 
   client.destroy();
 });
 
-client.login(token);
+client.login(TOKEN);
