e5540b7f6337b7f9d0d1229b21405c4efab1699e
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-12 12:04:50 -0400

    Update bot.js

--- src/Bot/bot.js
+++ src/Bot/bot.js
@@ -11,8 +11,6 @@ const pool = require('../database.js');
 const { CHANNEL_TYPES } = require('discord.js');
 const cron = require('node-cron');
 dotenv.config(); // Load environment variables from .env file
-const userId = '385324994533654530';
-const botId = '1105598736551387247';
 const { startVoteReminderLoop, addPreviouslyVotedUsers } = require('./voteReminder');
 
 const intents = [
