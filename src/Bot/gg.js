74bd5abbd0daab57d65241e4dcdf2c73504c9300
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-12 16:47:52 -0400

    Update gg.js

--- src/Bot/gg.js
+++ src/Bot/gg.js
@@ -1,3 +1,25 @@
+const pool = require('../database.js');
+const client = require('./bot.js'); // Replace with the actual path to your client file
+
+async function sendVoteReminder(userId) {
+    try {
+        // Retrieve user information for the vote reminder
+        const userResult = await pool.query('SELECT * FROM users WHERE discord_id = ?', [userId]);
+        const user = userResult[0];
+
+        // Use the existing client instance to send the vote reminder
+        const userDM = await client.users.fetch(userId);
+        
+        // Send the vote reminder as a DM
+        await userDM.send('Reminder: Don\'t forget to vote!');
+        
+        console.log(`Vote reminder sent to user with Discord ID ${userId}`);
+    } catch (error) {
+        console.error(`Error sending vote reminder to user with Discord ID ${userId}:`, error);
+    }
+}
+
+
 let fetch;
 
 import('node-fetch').then(nodeFetch => {
@@ -21,6 +43,26 @@ async function main() {
         if (response.ok) {
             const data = await response.json();
             console.log(data);
+
+            if (data.voted === 0) {
+                // User has not voted
+                console.log('User has not voted.');
+
+                // Add the user to the vote reminder system if not opted out
+                const userOptedOut = await checkUserOptOut(data.userId);
+                if (!userOptedOut) {
+                    await addUserToReminder(data.userId);
+                    console.log('Added user to the vote reminder system.');
+
+                    // Send the vote reminder to the user
+                    await sendVoteReminder(data.userId);
+                    console.log('Vote reminder sent to user.');
+                } else {
+                    console.log('User has opted out of the vote reminder.');
+                }
+            } else {
+                console.log('User has voted.');
+            }
         } else {
             // If the request failed, throw an error
             const error = new Error(`HTTP Error: ${response.statusText}`);
@@ -33,3 +75,29 @@ async function main() {
         console.error(`Response Headers: ${JSON.stringify(Array.from(error.response.headers.entries()))}`);
     }
 }
+
+async function checkUserOptOut(userId) {
+    try {
+        // Check if the user has opted out of the vote reminder
+        const userResult = await pool.query('SELECT vote_reminder_status FROM users WHERE discord_id = ?', [userId]);
+        return userResult.length > 0 && userResult[0].vote_reminder_status === false;
+    } catch (error) {
+        console.error(`Error checking if user with Discord ID ${userId} has opted out of the vote reminder:`, error);
+        return false;
+    }
+}
+
+async function addUserToReminder(userId) {
+    try {
+        // Add the user to the vote reminder system
+        await pool.query('INSERT INTO users (discord_id, vote_reminder_status) VALUES (?, true)', [userId]);
+    } catch (error) {
+        console.error(`Error adding user with Discord ID ${userId} to the vote reminder system:`, error);
+    }
+}
+
+// ... (Other code)
+
+module.exports = {
+    sendVoteReminder
+};
