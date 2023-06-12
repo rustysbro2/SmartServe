79fcf9fc4abf1fc6e86d01bc103fbac61d745787
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-12 17:12:18 -0400

    Update opt.js

--- src/Bot/commands/TopG/opt.js
+++ src/Bot/commands/TopG/opt.js
@@ -3,8 +3,8 @@ const pool = require('../../../database.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('optout')
-    .setDescription('Opt out of the vote reminder'),
+    .setName('opt')
+    .setDescription('Toggle the vote reminder opt-in/opt-out status'),
 
   async execute(interaction) {
     const discordId = interaction.user.id;
@@ -13,17 +13,24 @@ module.exports = {
       // Check if the user already has a row in the table
       const [rows] = await pool.promise().query('SELECT * FROM users WHERE discord_id = ?', [discordId]);
       if (rows.length === 0) {
-        // Insert a new row for the user
+        // Insert a new row for the user and set opt_out_status to 1
         await pool.promise().query('INSERT INTO users (discord_id, opt_out_status) VALUES (?, 1)', [discordId]);
+        await interaction.reply('You have successfully opted out of the vote reminder.');
       } else {
-        // Update the opt_out_status for the user
-        await pool.promise().query('UPDATE users SET opt_out_status = 1 WHERE discord_id = ?', [discordId]);
+        // Toggle the opt_out_status for the user
+        const optOutStatus = rows[0].opt_out_status;
+        const newOptOutStatus = optOutStatus === 1 ? 0 : 1;
+        await pool.promise().query('UPDATE users SET opt_out_status = ? WHERE discord_id = ?', [newOptOutStatus, discordId]);
+        
+        if (newOptOutStatus === 1) {
+          await interaction.reply('You have successfully opted out of the vote reminder.');
+        } else {
+          await interaction.reply('You have successfully opted back in to the vote reminder.');
+        }
       }
-
-      await interaction.reply('You have successfully opted out of the vote reminder.');
     } catch (error) {
-      console.error(`Error opting out user with Discord ID ${discordId}:`, error);
-      await interaction.reply('An error occurred while opting out. Please try again later.');
+      console.error(`Error updating opt-out status for user with Discord ID ${discordId}:`, error);
+      await interaction.reply('An error occurred while updating the opt-out status. Please try again later.');
     }
   },
 };
