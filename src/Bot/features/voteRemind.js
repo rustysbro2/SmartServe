f8bb435dd82969b3bd6b1f13583527e6c8480215
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-16 18:40:58 -0400

    Update voteRemind.js

--- src/Bot/features/voteRemind.js
+++ src/Bot/features/voteRemind.js
@@ -1,14 +1,13 @@
 const mysql = require('mysql2/promise');
 const axios = require('axios');
-require('dotenv').config(); // Import dotenv and load environment variables
+require('dotenv').config();
 
 const botId = process.env.BOT_ID;
 const topGGToken = process.env.TOPGG_TOKEN;
 const supportServerLink = 'https://discord.gg/wtzp28pHRK'; // Replace with your support server link
 const topGGVoteLink = `https://top.gg/bot/${botId}/vote`; // Top.gg vote link
-const ownerUserId = 'OWNER_USER_ID'; // Replace with the actual owner's user ID
+const ownerUserId = '385324994533654530'; // Replace with the actual owner's user ID
 
-// MySQL connection settings
 const connection = mysql.createPool({
   host: 'localhost',
   user: 'rustysbro',
@@ -25,18 +24,16 @@ async function sendDM(user, message) {
 }
 
 async function sendRecurringReminders(client) {
-  // Select users who have never voted and 12 hours have passed since the initial reminder
   const [neverVotedRows] = await connection.query(
     'SELECT user_id, initial_reminder_time, recurring_remind_time FROM users WHERE voted = 0 AND initial_reminder_sent = 1'
   );
 
   const currentTime = Date.now();
 
-  const neverVotedPromises = neverVotedRows.map(async row => {
+  const neverVotedPromises = neverVotedRows.map(async (row) => {
     const initialReminderTime = new Date(row.initial_reminder_time).getTime();
     const recurringReminderTime = row.recurring_remind_time ? new Date(row.recurring_remind_time).getTime() : null;
 
-    // Check if 12 hours have passed since the initial reminder or the recurring reminder time
     if (
       currentTime - initialReminderTime >= 12 * 60 * 60 * 1000 ||
       (recurringReminderTime !== null && currentTime - recurringReminderTime >= 12 * 60 * 60 * 1000)
@@ -45,7 +42,6 @@ async function sendRecurringReminders(client) {
       if (row.user_id) {
         const user = await client.users.fetch(row.user_id);
 
-        // Check if the user has opted out
         const [[userData]] = await connection.query('SELECT * FROM users WHERE user_id = ?', [row.user_id]);
         if (userData.opt_out !== 1) {
           const userHasReceivedReminder =
@@ -55,12 +51,10 @@ async function sendRecurringReminders(client) {
             const voteLink = `https://top.gg/bot/${botId}/vote`;
             let message = `Hello! It seems you haven't voted yet. Please consider voting for our bot by visiting the vote link: ${voteLink}\n\nJoin our support server for any assistance or questions: ${supportServerLink}`;
 
-            // Mention the owner (e.g., @cmdr_ricky#0)
             const owner = await client.users.fetch(ownerUserId);
             message += ` The owner of the bot is ${owner}.`;
 
             sendDM(user, message);
-            // Update the recurring_remind_time in the database to the current time
             await connection.query('UPDATE users SET recurring_remind_time = ? WHERE user_id = ?', [
               new Date(),
               row.user_id
@@ -80,7 +74,6 @@ async function checkAndRecordUserVote(member) {
   console.log(`Checking vote status for user: ${member.user.tag}`);
 
   try {
-    // Check if the user has voted
     const response = await axios.get(`https://top.gg/api/bots/${botId}/check`, {
       params: {
         userId: member.user.id,
@@ -92,7 +85,6 @@ async function checkAndRecordUserVote(member) {
 
     const voteStatus = response.data.voted;
 
-    // Update the vote status in the database
     const [results] = await connection.query(
       'INSERT INTO users (user_id, voted, initial_reminder_sent, opt_out) VALUES (?, ?, 0, 0) ON DUPLICATE KEY UPDATE voted = VALUES(voted), initial_reminder_sent = initial_reminder_sent',
       [member.user.id, voteStatus]
@@ -100,18 +92,14 @@ async function checkAndRecordUserVote(member) {
 
     console.log(`User ${member.user.tag} has ${voteStatus === 1 ? '' : 'not '}voted.`);
 
-    // If the user hasn't voted and the initial reminder hasn't been sent yet, send it.
     const [[user]] = await connection.query('SELECT * FROM users WHERE user_id = ?', [member.user.id]);
     if (user.voted === 0 && user.initial_reminder_sent === 0 && user.opt_out === 0) {
-      // Send an initial reminder DM
       let message = `Hello, ${member.user}! It seems you haven't voted yet. Please consider voting for our bot by visiting the vote link: ${topGGVoteLink}\n\nYou won't receive further reminders unless you opt in to reminders. The owner of the bot is <@${ownerUserId}>.`;
 
-      // Add the support server link
       message += `\n\nJoin our support server for any assistance or questions: ${supportServerLink}`;
 
       sendDM(member.user, message);
 
-      // Update the initial_reminder_sent flag in the database
       await connection.query('UPDATE users SET initial_reminder_sent = 1 WHERE user_id = ?', [member.user.id]);
     }
   } catch (error) {
@@ -127,34 +115,29 @@ async function checkAllGuildMembers(client) {
 
     guild.members.fetch().then(async (members) => {
       members.forEach(async (member) => {
-        // Skip if the member is a bot
         if (member.user.bot) {
           return;
         }
 
-        // Check and record vote status
         await checkAndRecordUserVote(member);
       });
     });
   });
 
-  // Check vote status for users who have already voted every minute
   setInterval(() => {
-    console.log('Checking vote status for all guild members (every minute)...');
+    console.log('Checking vote status for all guild members (every 30 minutes)...');
     client.guilds.cache.forEach(async (guild) => {
       guild.members.fetch().then(async (members) => {
         members.forEach(async (member) => {
-          // Skip if the member is a bot
           if (member.user.bot) {
             return;
           }
 
-          // Check and record vote status
           await checkAndRecordUserVote(member);
         });
       });
     });
-  }, 60 * 1000);
+  }, 30 * 60 * 1000);
 }
 
 module.exports = {
