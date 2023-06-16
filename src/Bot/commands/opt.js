109637cf972d6c2c71e71abb621b1306a3cb495a
Ricky <103372389+rustysbro2@users.noreply.github.com>
2023-06-16 13:48:22 -0400

    Update opt.js

--- src/Bot/commands/opt.js
+++ src/Bot/commands/opt.js
@@ -1,22 +1,34 @@
 const { SlashCommandBuilder } = require('discord.js');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+// MySQL connection settings
+const connection = mysql.createPool({
+  host: 'localhost',
+  user: 'rustysbro',
+  password: 'Dincas50@/',
+  database: 'SmartBeta',
+});
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('optout')
-    .setDescription('Opt out of recurring reminders'),
+    .setName('opt')
+    .setDescription('Opt out or opt back in to receive recurring reminders')
+    .addBooleanOption(option => option.setName('opt_out').setDescription('Opt out or opt back in').setRequired(true)),
 
   async execute(interaction) {
-    const guild = interaction.guild;
-    const member = guild.members.cache.get(interaction.user.id);
+    const optOut = interaction.options.getBoolean('opt_out');
+    const userId = interaction.user.id;
 
     try {
-      // Update the database to mark the user as opted out
-      await connection.query('UPDATE users SET opt_out = 1 WHERE user_id = ?', [member.id]);
+      // Update the opt_out column in the database
+      await connection.query('UPDATE users SET opt_out = ? WHERE user_id = ?', [optOut ? 1 : 0, userId]);
 
-      await interaction.reply("You have opted out of recurring reminders. You will no longer receive them.");
+      const response = optOut ? 'You have successfully opted out of recurring reminders.' : 'You have successfully opted back in to receive recurring reminders.';
+      await interaction.reply(response);
     } catch (error) {
-      console.error('Error updating opt-out status:', error);
-      await interaction.reply("An error occurred while updating your opt-out status. Please try again later.");
+      console.error('Error updating opt_out status:', error);
+      await interaction.reply('An error occurred while updating your opt-out status. Please try again later.');
     }
   },
 };
