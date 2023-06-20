9f84dd2d4162e6825415985bd3f542cde0effe33
Richard Mazzocco <rustysbro2016@techie.com>
2023-06-20 12:47:17 -0400

    Website 1.3

--- src/Website/routes/dashboard.js
+++ src/Website/routes/dashboard.js
@@ -1,12 +1,21 @@
 const express = require('express');
-const path = require('path');
 const router = express.Router();
 
 // Define dashboard route
 router.get('/', async (req, res) => {
   if (req.isAuthenticated()) {
-    const filePath = path.join(__dirname, '../views/dashboard.html');
-    res.sendFile(filePath);
+    try {
+      // Fetch user data based on the authenticated user
+      const user = req.user.username;
+      const email = req.user.email;
+      const avatarURL = req.user.avatarURL;
+
+      // Render the dashboard view with the user data
+      res.render('dashboard', { user, email, avatarURL });
+    } catch (error) {
+      // Handle any errors that occur during data retrieval
+      res.status(500).send('Error fetching user data');
+    }
   } else {
     res.redirect('/login');
   }
--- src/Website/routes/index.js
+++ src/Website/routes/index.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const path = require('path');
 const router = express.Router();
 
-// Define root route
+// Define the index route
 router.get('/', (req, res) => {
-  const pageTitle = 'My Website';
-  const currentYear = new Date().getFullYear();
-  res.render('index', { pageTitle, currentYear });
+  res.render('index');
 });
 
 module.exports = router;
